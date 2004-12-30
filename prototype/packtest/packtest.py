e60747ee2d7bc0f4292496d7c0d7559fdbf0ff21
David Moore <djm@exelearning.org>
2004-12-30 01:34:44 +0000

    can now select port from command line
    git-svn-id: file:///home/jwt/exe/trunk@24 38b22f21-9aea-0310-abfc-843a9883df58

--- prototype/packtest/packtest.py
+++ prototype/packtest/packtest.py
@@ -7,19 +7,28 @@ import os
 import sys
 
 def main():
+    if len(sys.argv) > 1:
+        try:
+            port = int(sys.argv[1])
+        except ValueError:
+            print "Usage:",sys.argv[0],"[port]"
+            sys.exit(1)
+    else:
+        port = 8080
+
     root = static.File("web") 
     root.processors = {".rpy": script.ResourceScript}
     root.indexNames = ['packtest.rpy']
 
-    reactor.listenTCP(8080, server.Site(root))
-    reactor.callWhenRunning(launchBrowser)
+    reactor.listenTCP(port, server.Site(root))
+    reactor.callWhenRunning(launchBrowser, port)
     reactor.run()
 
-def launchBrowser():
+def launchBrowser(port):
     if sys.platform[:3] == "win":
-        os.system("start http://localhost:8080")
+        os.system("start http://localhost:%d"%port)
     else:
-        os.system("htmlview http://localhost:8080&")
+        os.system("htmlview http://localhost:%d&"%port)
     print "Welcome to the eXe: packtest"
 
 if __name__ == "__main__":
