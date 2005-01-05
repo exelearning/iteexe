0cb8178cbc4e05cb36d0ec24125db60983160c27
David Moore <djm@exelearning.org>
2005-01-05 04:18:58 +0000

    Initial checkin
    git-svn-id: file:///home/jwt/exe/trunk@46 38b22f21-9aea-0310-abfc-843a9883df58

---
+++ exe/webui/authoringpane.py
@@ -0,0 +1,66 @@
+# ===========================================================================
+# eXe 
+# Copyright 2004-2005, University of Auckland
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# ===========================================================================
+
+import sys
+import logging
+import gettext
+from exe.webui.blockfactory import g_blockFactory
+
+log = logging.getLogger(__name__)
+_   = gettext.gettext
+
+# ===========================================================================
+class AuthoringPane(object):
+    """
+    AuthoringPane is responsible for creating the XHTML for the authoring
+    area of the eXe web user interface.  
+    """
+    def __init__(self, topNode, levelLimit=None):
+        self.topNode   = topNode
+        self.blocks    = []
+        self.addBlocks(topNode, levelLimit)
+
+    def addBlocks(self, node, levelLimit):
+        for idevice in node.idevices:
+            block = g_blockFactory.createBlock(idevice)
+            assert(block)
+            self.blocks.append(block)
+
+        if self.levelLimit is None or len(node.id) < self.levelLimit:
+		for child in node.children:
+		    self.addBlocks(child)
+
+    def render(self):
+        """
+        Returns an XHTML string for viewing this pane
+        """
+        html = "" 
+        for block in self.blocks:
+            html += block.render()
+            
+        return html
+        
+    def process(self, args):
+        for block in self.blocks:
+            block.process(args)
+        
+        
+
+
+# ===========================================================================
