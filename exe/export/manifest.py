419896a5744541dd4a1a1837c3622ebec41befbc
Jenny Lin <jenny@exelearning.org>
2005-01-28 03:25:13 +0000

    modified manifest file
    git-svn-id: file:///home/jwt/exe/trunk@176 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/export/manifest.py
+++ exe/export/manifest.py
@@ -59,22 +59,34 @@ class Manifest(object):
         """
         
         self.xmlStr += """<?xml version="1.0" encoding="UTF-8"?>
-<!--This is a Reload version 2.0.1 SCORM 1.2 Content Package document-->
-<!--Spawned from the Reload Content Package Generator - http://www.reload.ac.uk-->
-<manifest xmlns="http://www.imsproject.org/xsd/imscp_rootv1p1p2" xmlns:imsmd="http://www.imsglobal.org/xsd/imsmd_rootv1p2p1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:adlcp="http://www.adlnet.org/xsd/adlcp_rootv1p2" identifier="MANIFEST-942DE505-084F-5979-5843-B76BFE75364E" xsi:schemaLocation="http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd">
-            <organizations default="Toc1"> 
-                <organization identifier="Toc1" structure="hierarchical"> 
-                 <title>%s</title> 
-        """ % (self.title)
+<manifest identifier="MANIFEST1" xmlns="http://www.imsglobal.org/xsd/imscp_v1p1" 
+    xmlns:imsmd="http://www.imsglobal.org/xsd/imsmd_v1p2" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+    xsi:schemaLocation="http://www.imsglobal.org/xsd/imscp_v1p1 imscp_v1p1.xsd http://www.imsglobal.org/xsd/imsmd_v1p2 imsmd_v1p2p2.xsd">
+       <metadata>
+           <schema>IMS Content</schema>
+           <schemaversion>1.1.3</schemaversion>
+            <imsmd:lom>
+            <imsmd:general>
+            <imsmd:title>
+            <imsmd:langstring xml:lang="en-US">%s</imsmd:langstring>
+            </imsmd:title>
+            </imsmd:general>
+            </imsmd:lom>
+        </metadata>
+        <organizations default="Toc1"> 
+            <organization identifier="Toc1" structure="hierarchical"> 
+             <title>%s</title> 
+        """ % (self.title, self.title)
         
         self.xmlStr += self.getItemStr(self.node)
         self.xmlStr += """
             </organization>
         </organizations>
-        <resourses>
+        <resources>
         """
         self.xmlStr += self.getResourseStr(self.node)
-        self.xmlStr += "</resourses> \n </manifest>\n"
+        self.xmlStr += "</resources> \n </manifest>\n"
         return self.xmlStr
         
             
@@ -103,8 +115,7 @@ class Manifest(object):
         filename = node.getIdStr()+ ".html"
         id = node.getIdStr().replace(".", "-")
         resStr += """
-            <resource identifier="RESOURSE%s" type="webcontent" 
-            adlcp:scormtype="asset" href="%s">
+            <resource identifier="RESOURSE%s" type="webcontent" href="%s">
                 <file href="%s"/>
             </resource>
             """ %(id, filename, filename)
