607677b41444722e4d1a3a51474abac9238dd8aa
Jenny Lin <jenny@exelearning.org>
2005-02-01 03:28:54 +0000

    updated
    git-svn-id: file:///home/jwt/exe/trunk@184 38b22f21-9aea-0310-abfc-843a9883df58

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@
 import glob
 from distutils.core import setup
 import py2exe
-      
+
 setup(console=["exe/webui/server.py"],
-      version="0.1.132",
+      version="0.1.183",
       packages=["exe", "exe.engine", "exe.webui", "exe.export"],
-      data_files=[('.', ["exe.conf",]),
+      data_files=[('.', ["exe/exe.conf",]),
+                  ('.', ["README",]),
                   ('css', glob.glob('exe/webui/css/*')),
-		  ('images', glob.glob('exe/webui/images/*'))],
+                  ('images', glob.glob('exe/webui/images/*'))],
+      
      )
 
