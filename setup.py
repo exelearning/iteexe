253d330a221b50185770c6a43c6fd78ce47aa97d
Jenny Lin <jenny@exelearning.org>
2005-02-01 02:26:12 +0000

    added style sheet
    git-svn-id: file:///home/jwt/exe/trunk@183 38b22f21-9aea-0310-abfc-843a9883df58

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ from distutils.core import setup
 import py2exe
       
 setup(console=["exe/webui/server.py"],
-      version="0.1",
-      packages=["exe", "exe.util", "exe.engine", "exe.webui", "exe.export"],
-      data_files=[('.', ["exe.conf",])],
+      version="0.1.132",
+      packages=["exe", "exe.engine", "exe.webui", "exe.export"],
+      data_files=[('.', ["exe.conf",]),
+                  ('css', glob.glob('exe/webui/css/*')),
+		  ('images', glob.glob('exe/webui/images/*'))],
      )
 
