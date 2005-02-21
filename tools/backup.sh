ba8cea265ffbc514d7acc79958c2f97cc2b713db
David Moore <djm@exelearning.org>
2005-02-21 04:05:30 +0000

    initial checkin
    git-svn-id: file:///home/jwt/exe/trunk@260 38b22f21-9aea-0310-abfc-843a9883df58

---
+++ tools/backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ===========================================================================
+# backup.sh
+# Copyright 2004, D J Moore (info@linuxsoftware.co.nz)
+# ===========================================================================
+set -x
+
+SVNTOOLS=/usr/lib/subversion/tools
+SVNPARENT=/svn
+TOP=/local/backup
+YYMMDD=$(date "+%y%m%d")
+DBNAME=mantis
+DBUSER=root
+DBPASS=""
+MOIN=/local/web/moin/reptilehouse
+
+mkdir -p $TOP/$YYMMDD
+
+# Backup the Subversion repositories
+for REPO in $SVNPARENT/*; do
+    if [ -d $REPO ]; then
+        echo $REPO
+        $SVNTOOLS/backup/hot-backup.py $REPO $TOP/$YYMMDD
+    fi
+done
+
+# Backup the database (Mantis)
+cd $TOP/$YYMMDD
+mysqldump --add-drop-table -e -u$DBUSER -p$DBPASS $DBNAME > db$YYMMDD.sql
+gzip db$YYMMDD.sql
+
+# Backup the wiki (Reptile House)
+cd $MOIN
+tar cjvf $TOP/$YYMMDD/moin$YYMMDD.tar.bz2 data
