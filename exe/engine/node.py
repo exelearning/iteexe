30fd8db848f522b00500c91c168d53c45fa57cc4
Matthew Sherborne <matiu@exelearning.org>
2005-04-18 03:06:29 +0000

    Restored backwards compat to 0.3
    git-svn-id: file:///home/jwt/exe/trunk@483 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/node.py
+++ exe/engine/node.py
@@ -34,7 +34,7 @@ class Node(object, jelly.Jellyable, jelly.Unjellyable, Versioned):
     """
 
     # Class attributes
-    persistenceVersion = 1
+    persistenceVersion = 2
 
     def __init__(self, package, parent=None, title=""):
         if parent:
@@ -243,6 +243,10 @@ class Node(object, jelly.Jellyable, jelly.Unjellyable, Versioned):
 
     def upgradeToVersion1(self):
         """Upgrades the node from version 0 to 1."""
+        # Nothing to do
+
+    def upgradeToVersion2(self):
+        """Upgrades the node from version 1 to 2."""
         self._title = self.__dict__['title']
         
 # ===========================================================================
