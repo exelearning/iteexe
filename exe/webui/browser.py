50d49c7bcf538cca3aa7c9b7b715f881c29055bb
David Moore <djm@exelearning.org>
2005-04-23 19:06:15 +0000

    Add exe start script and browser.py
    git-svn-id: file:///home/jwt/exe/trunk@501 38b22f21-9aea-0310-abfc-843a9883df58

---
+++ exe/webui/browser.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# ===========================================================================
+# eXe
+# Copyright 2004-2005, University of Auckland
+#
+# This module is for the TwiSteD web server.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# ===========================================================================
+
+"""
+Browser module
+"""
+
+
+import os
+import os.path
+import sys
+from exe.engine.config import Config
+import logging
+import gettext
+_   = gettext.gettext
+ 
+log = logging.getLogger(__name__)
+
+def launchBrowser(config):
+    """
+    Launch the webbrowser (Firefox) for this platform
+    """
+    if sys.platform[:3] == "win":
+        log.info("Broswer path: " + config.browserPath)
+        url     = 'http://localhost:%d' % port
+        log.info("Launch firefox with "+config.browserPath)
+        try:
+            os.spawnl(os.P_DETACH, config.browserPath, 
+                      '"'+config.browserPath+'"', url)
+        except OSError:
+            print "Cannot launch Firefox, please manually run Firefox"
+            print "and go to", url     
+
+    else:
+        os.system(config.browserPath+" http://localhost:%d&" % config.port)
+
+
