eb84b707101fd8e3dd0fce940bcf6c697b27803b
Jenny Lin <jenny@exelearning.org>
2005-07-25 01:17:57 +0000

    added caption to imagewithtext idevice.
    git-svn-id: file:///home/jwt/exe/trunk@1003 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/imagewithtextidevice.py
+++ exe/engine/imagewithtextidevice.py
@@ -74,6 +74,7 @@ the piston with a brief textual summary of the key aspects of each visual.
         self.text = TextAreaField(_(u"Text"))
         self.text.idevice = self
         self.float        = u"left"
+        self.caption      = u""
  
 
     def getResources(self):
