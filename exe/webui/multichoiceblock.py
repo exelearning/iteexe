d8982ecaa8ad5e8ef790d417c67deae9e68e5a95
Brent Simpson <brent@exelearning.org>
2005-07-29 03:29:41 +0000

    changed icon for multichoice and truefalse question.
    git-svn-id: file:///home/jwt/exe/trunk@1053 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/multichoiceblock.py
+++ exe/webui/multichoiceblock.py
@@ -140,7 +140,7 @@ class MultichoiceBlock(Block):
         html += u"<div class=\"iDevice "
         html += u"emphasis"+unicode(self.idevice.emphasis)+"\">\n"
         html += '<img alt="" class="iDevice_icon" '
-        html += "src=\"icon_multichoice.gif\" />\n"
+        html += "src=\"icon_question.gif\" />\n"
         html += "<span class=\"iDeviceTitle\">"       
         html += self.idevice.title+"</span><br/>\n"
         html += self.question+" &nbsp;&nbsp;\n"
@@ -172,7 +172,7 @@ class MultichoiceBlock(Block):
         html += u"emphasis"+unicode(self.idevice.emphasis)+"\" "
         html += u"ondblclick=\"submitLink('edit',"+self.id+", 0);\">\n"
         html += u'<img alt="" class="iDevice_icon" '
-        html += u"src=\"/style/"+style+"/icon_multichoice.gif\" />\n"
+        html += u"src=\"/style/"+style+"/icon_question.gif\" />\n"
         html += u"<span class=\"iDeviceTitle\">"       
         html += self.idevice.title+"</span><br/>\n"
         html += self.question+" &nbsp;&nbsp;\n"
