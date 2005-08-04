bebecddda78bf8c8cd2ad63b68a1fb8855c61b6a
David Moore <djm@exelearning.org>
2005-08-04 02:35:46 +0000

    new options for gtkmozembed
    git-svn-id: file:///home/jwt/exe/trunk@1086 38b22f21-9aea-0310-abfc-843a9883df58

--- win-setup.py
+++ win-setup.py
@@ -7,8 +7,11 @@ from distutils.core import setup
 import py2exe
 from exe.engine import version
 
-g_files = { '.': ["README", "eXe_icon.ico", 
-                  "doc/eXe-tutorial.elp", "installs/windows/exeLicense.txt"]}
+g_files = { '.': ["README", 
+                  "eXe_icon.ico", 
+		  "exe/webui/mr_x.gif",
+                  "doc/eXe-tutorial.elp", 
+		  "installs/windows/exeLicense.txt"]}
 g_oldBase = "exe/webui"
 g_newBase = "."
 def dataFiles(dirs):
@@ -33,15 +36,30 @@ dataFiles(["exe/webui/style",
            "exe/webui/docs",
            "exe/webui/scripts"])
 
-setup(console=["exe/exe"],
+opts = {
+ "py2exe": {
+   "packages": ["encodings", "nevow"],
+   "includes": ["pango", "atk", "gobject",
+                "PngImagePlugin", "JpegImagePlugin", "GifImagePlugin",
+		"IcoImagePlugin", "BmpImagePlugin"],
+   "dll_excludes": ["iconv.dll", "intl.dll", 
+                    "libatk-1.0-0.dll", "libgdk-win32-2.0-0.dll", 
+		    "libgdk_pixbuf-2.0-0.dll", "libglib-2.0-0.dll", 
+		    "libgmodule-2.0-0.dll", "libgobject-2.0-0.dll", 
+                    "libgthread-2.0-0.dll", "libgtk-win32-2.0-0.dll", 
+		    "libpango-1.0-0.dll", "libpangowin32-1.0-0.dll", 
+		    "nspr4.dll", "plc4.dll", "plds4.dll", "xpcom.dll" ]
+ }
+}
+setup(windows=["exe/exe"],
       version=version.release,
       packages=["exe", "exe.engine", "exe.webui", "exe.export"],
       description  = "eLearning XHTML editor",
-      url          = "http://exe.cfdl.auckland.ac.nz",
+      url          = "http://exelearning.org",
       author       = "University of Auckland",
       author_email = "exe@auckland.ac.nz",
       license      = "GPL",
       scripts      = ["exe/exe",],
-      data_files   = g_files.items()
-                    
+      options      = opts,
+      data_files   = g_files.items(),
      )
