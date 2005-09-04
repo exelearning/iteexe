f73ead64ea65fb7ae73b3416cf67810bd5cd8644
David Moore <djm@exelearning.org>
2005-09-04 21:50:31 +0000

    changes since hack and upgrade
    git-svn-id: file:///home/jwt/exe/trunk@1182 38b22f21-9aea-0310-abfc-843a9883df58

--- tools/backup.sh
+++ tools/backup.sh
@@ -6,7 +6,7 @@
 # ===========================================================================
 set -x
 
-SVNTOOLS=/usr/share/doc/subversion-1.1.1/tools/
+SVNTOOLS=/usr/share/doc/subversion-1.2.1/tools/
 SVNPARENT=/svn
 TOP=/local/backup
 YYMMDD=$(date "+%y%m%d")
@@ -17,15 +17,21 @@ MOIN=/local/web/moin/reptilehouse
 mkdir -p $TOP/$YYMMDD
 
 # Backup the Subversion repositories
-for REPO in $SVNPARENT/*; do
-    if [ -d $REPO ] && [ "$REPO" != "lost+found" ]; then
-        echo $REPO
-        $SVNTOOLS/backup/hot-backup.py $REPO $TOP/$YYMMDD
-    fi
-done
+#for REPO in $SVNPARENT/*; do
+#    if [ -d $REPO ] && [ "$REPO" != "lost+found" ]; then
+#        echo $REPO
+#        $SVNTOOLS/backup/hot-backup.py $REPO $TOP/$YYMMDD
+#        svnadmin dump $REPO
+#        svnadmin dump /svn/$REPO > $REPO.$YYMMDD
+#        gzip $REPO.$YYMMDD
+#    fi
+#done
+service httpd stop
+cd $TOP/$YYMMDD
+svnadmin dump /svn/exe > exe.$YYMMDD
+gzip exe.$YYMMDD
 
 # Backup the databases (mantis and exe)
-cd $TOP/$YYMMDD
 mysqldump --add-drop-table -e -u$DBUSER mantis > mantis$YYMMDD.sql
 gzip mantis$YYMMDD.sql
 mysqldump --add-drop-table -e -u$DBUSER exe > exe$YYMMDD.sql
@@ -34,6 +40,7 @@ gzip exe$YYMMDD.sql
 # Backup the wiki (Reptile House)
 cd $MOIN
 tar cjvf $TOP/$YYMMDD/moin$YYMMDD.tar.bz2 data
+service httpd start
 
 # Copy to another machine
 scp -r $TOP/$YYMMDD getback@d.moore.cfdl.auckland.ac.nz:/local/backup
