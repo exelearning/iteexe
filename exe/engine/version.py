91dd1d536e06b302a70845d7a4a1a4ee012b9936
David Moore <djm@exelearning.org>
2005-09-05 22:00:20 +0000

    bumped version number
    git-svn-id: file:///home/jwt/exe/trunk@1193 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/version.py
+++ exe/engine/version.py
@@ -23,7 +23,7 @@ Version Information
 """
 
 project        = "exe"
-release        = "0.7"
+release        = "0.8"
 revision       = "$Revision$"[11:-2]
 version        = release + "." + revision
 
