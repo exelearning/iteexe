6ade6ac9123626f082155cc6a1dfcaa2726322b8
David Moore <djm@exelearning.org>
2005-09-14 02:52:39 +0000

    add link/unlink
    git-svn-id: file:///home/jwt/exe/trunk@1229 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/scripts/armadillo.js
+++ exe/webui/scripts/armadillo.js
@@ -2,10 +2,11 @@
 //## Toolbar Buttons Sets
 //##
 FCKConfig.ToolbarSets["Armadillo"] = [
-	['Bold','Italic','-',
-     'Subscript','Superscript'],
-     ['UnorderedList','OrderedList','Outdent','Indent','Table'],
-     ['Undo','Redo','-',
-     'Find','Replace','SelectAll','-',
+	['Bold', 'Italic', '-',
+     'Subscript',' Superscript',
+     'Link', 'Unlink'],
+     ['UnorderedList', 'OrderedList', 'Outdent', 'Indent', 'Table'],
+     ['Undo', 'Redo', '-',
+     'Find', 'Replace', 'SelectAll', '-',
      'RemoveFormat', 'Source']
 ];
