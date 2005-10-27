95524bc1b82a1cd70194063a15d0e81b43db954f
Brent Simpson <brent@exelearning.org>
2005-10-27 01:17:08 +0000

    more editor stuff??? not sure.
    git-svn-id: file:///home/jwt/exe/trunk@1357 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/authoringpage.py
+++ exe/webui/authoringpage.py
@@ -155,8 +155,9 @@ class AuthoringPage(RenderableResource):
         html += u"italic,underline,separator,sub,sup,separator,"
         html += u"justifyleft,justifycenter,justifyright,justifyfull,"
         html += u"separator,bullist,numlist,indent,outdent,separator,"
-        html += u"cut,copy,paste,link,unlink\",\n"
-        html += u" theme_advanced_buttons2 : \"\",\n"
+        html += u"cut,copy,paste\",\n"
+        html += u" theme_advanced_buttons2 : \",link,unlink\",\n"
+        html += u" theme_advanced_buttons3 : \"\",\n"
       
         html += u"theme_advanced_statusbar_location : \"bottom\",\n"
 	html += u"    theme_advanced_resize_horizontal : false,\n"
