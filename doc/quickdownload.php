60f69a27242d02d74e70f77774988dabdc514517
David Moore <djm@exelearning.org>
2006-03-16 03:55:16 +0000

    Updated for 0.14, added Ubuntu
    git-svn-id: file:///home/jwt/exe/trunk@1963 38b22f21-9aea-0310-abfc-843a9883df58

--- doc/quickdownload.php
+++ doc/quickdownload.php
@@ -6,35 +6,39 @@
     if (strstr($useragent, "win")) 
     {
         print "Download eXe for Windows<br/>\n";
-        print "<a href=\"$eduforge/eXe_install_windows.exe\">";
-        print "eXe_install_windows.exe</a><br/>\n";
+        print "<a href=\"http://eduforge.org/frs/download.php/193/eXe-install-0.14.exe\">";
+        print "eXe_install_0.14.exe</a><br/>\n";
     }
     else if (strstr($useragent, "linux")) 
     {
         print "Download eXe for Linux<br/>\n";
-        print "<a href=\"$eduforge/exe-0.6.1-2.noarch.rpm\">";
-        print "exe-0.6.1-2.noarch.rpm</a><br/>\n";
-        print "<a href=\"$eduforge/exe-twisted-2.0-1.noarch.rpm\">";
-        print "exe-twisted-2.0-1.noarch.rpm</a><br/>\n";
-    }
-    else 
-    {
-        if (strstr($useragent, "mac")) 
+        if (strstr($useragent, "ubuntu")) 
         {
-            print "Creating a Mac install is something we're still working ";
-            print "towards.<br/>\n";
-            print "(There's more information in the ";
-            print "<a href=\"http://exelearning.org/svn/exe/trunk/README\">";
-            print "README</a> ";
-            print "if you're interested in helping.)<br/>\n";
-        } 
+            print "<a href=\"http://eduforge.org/frs/download.php/191/python2.4-exe_0.14-0.1ubuntu1_all.deb\">";
+            print "python2.4-exe_0.14-0.1ubuntu1_all.deb</a><br/>\n";
+            print "<a href=\"http://eduforge.org/frs/download.php/141/python2.4-twisted_2.0.1-999_all.deb\">";
+            print "python2.4-twisted_2.0.1-999_all.deb</a><br/>\n";
+        }
         else
         {
-            print "Sorry I didn't recognize your operating system.<br/>\n";
+            print "<a href=\"http://eduforge.org/frs/download.php/190/exe-0.14-1.noarch.rpm\">";
+            print "exe-0.14-1.noarch.rpm</a><br/>\n";
+            print "<a href=\"http://eduforge.org/frs/download.php/108/exe-twisted-2.0-2.noarch.rpm\">";
+            print "exe-twisted-2.0-1.noarch.rpm</a><br/>\n";
         }
+    }
+    else if (strstr($useragent, "mac")) 
+    {
+        print "Download eXe for Mac OSX<br/>\n";
+        print "<a href=\"http://eduforge.org/frs/download.php/136/exe.mac.experimental.0.11.zip\">";
+        print "exe.mac.experimental.0.11.zip</a><br/>\n";
+    } 
+    else
+    {
+        print "Sorry I didn't recognize your operating system.<br/>\n";
         print "Download eXe sourcecode<br/>\n";
-        print "<a href=\"$eduforge/exe-source-0.6.1.tgz\">";
-        print "exe-source-0.6.1.tgz</a><br/>\n";
+        print "<a href=\"http://eduforge.org/frs/download.php/195/exe-0.14-source.tgz\">";
+        print "exe-source-0.14.tgz</a><br/>\n";
     }
 ?>
 </p>
@@ -43,3 +47,4 @@ Other eXe files are available on
 <a href="http://eduforge.org/frs/?group_id=20">Eduforge</a> or
 <a href="http://sourceforge.net/project/showfiles.php?group_id=94655">Sourceforge</a> 
 </p>
+
