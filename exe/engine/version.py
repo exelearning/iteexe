084c59fbc34b92105f23e8594c5b4fabb94d610b
Matthew Sherborne <matiu@exelearning.org>
2006-08-28 04:22:11 +0000

    Updated version.py
    git-svn-id: file:///home/jwt/exe/trunk@2173 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/version.py
+++ exe/engine/version.py
@@ -24,7 +24,7 @@ Version Information
 """
 
 project        = "exe"
-release        = "0.17"
+release        = "0.18"
 revision       = "$Revision$"[11:-2]
 version        = release + "." + revision
 
