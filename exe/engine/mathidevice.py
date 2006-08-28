0f2c9c111136865ff954ae4b3b2780e294f9d7e7
Sean Kehinde <sean@exelearning.org>
2006-08-28 23:44:15 +0000

    Fix spelling error in instructions for completion earlier added
    git-svn-id: file:///home/jwt/exe/trunk@2182 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/mathidevice.py
+++ exe/engine/mathidevice.py
@@ -36,7 +36,7 @@ class MathIdevice(Idevice):
                          x_(u"University of Auckland"), 
                          x_(u"""The mathematical language LATEX has been 
                         used to enable you to create mathematical formula 
-                        within you content. It does this by translating the 
+                        within your content. It does this by translating the 
                         LATEX into a graphical image which is then displayed
                          within your eXe content. We would recommend that 
                         you use the Free Text iDevice to provide 
