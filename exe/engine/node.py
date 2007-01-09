5a8e1b1a109daa09aace10e370fc573be2961118
Sean Kehinde <sean@exelearning.org>
2007-01-09 02:22:14 +0000

    altered to handle unicode decoding. solves #607
    git-svn-id: file:///home/jwt/exe/trunk@2454 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/node.py
+++ exe/engine/node.py
@@ -135,7 +135,7 @@ class Node(Persistable):
         log.debug(u"getResources ")
         resources = {}
         for idevice in self.idevices:
-            reses = [unicode(res.storageName, 'utf8') for res in idevice.userResources]
+            reses = [toUnicode(res.storageName, 'utf8') for res in idevice.userResources]
             for resource in (idevice.systemResources + reses):
                 resources[resource] = True
         return resources.keys()
