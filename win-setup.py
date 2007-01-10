1782d87b38a7a21dabf76afc660b0c6690307721
Jim Tittsler <jim@exelearning.org>
2007-01-10 02:03:38 +0000

    automatically update the revision number at build time
    git-svn-id: file:///home/jwt/exe/trunk@2455 38b22f21-9aea-0310-abfc-843a9883df58

--- win-setup.py
+++ win-setup.py
@@ -2,10 +2,30 @@
 
 # setup.py
 import glob
-import os.path
+import os
+import subprocess
 from distutils.core import setup
 import py2exe
 from exe.engine import version
+
+# update the svn revision number
+REVISION_FILE = 'exe/engine/version_svn.py'
+
+try:
+    os.unlink(REVISION_FILE)
+except OSError:
+    pass
+
+try:
+    psvn = subprocess.Popen('svnversion', stdout=subprocess.PIPE)
+    psvn.wait()
+    revision = psvn.stdout.read().strip()
+except OSError:
+    print "*** Warning: 'svnversion' tool not available to update revision number"
+    revision = None
+
+if revision:
+    open(REVISION_FILE, 'wt').write('revision = "%s"\n' % revision)
 
 g_files = { '.': ["README", 
                   "COPYING", 
