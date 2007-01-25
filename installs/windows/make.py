c9b560f151c259ca4cfc7fab4fe657190ed922c8
Jim Tittsler <jim@exelearning.org>
2007-01-25 03:39:31 +0000

    include building in the script that makes the installers
    git-svn-id: file:///home/jwt/exe/trunk@2485 38b22f21-9aea-0310-abfc-843a9883df58

--- installs/windows/make.py
+++ installs/windows/make.py
@@ -9,18 +9,34 @@ nsis = 'c:\Program Files\NSIS\makensis.exe'
 # name used for temporary file that contains branded splash screen
 BRANDED_JPG = 'splashb.jpg'
 
+# eXe working directory path
+WDIR = 'C:/exe'
+
+import sys
 import os
+import shutil
 import subprocess
 import Image, ImageFont, ImageDraw
 
+# clean out the build and dist dirs
+os.chdir(WDIR)
+shutil.rmtree('build', True)
+shutil.rmtree('dist', True)
+
+# copy Firefox into the dist tree
+shutil.copytree('exe/webui/Mozilla Firefox', 'dist/Mozilla Firefox')
+
+# build the executable
+subprocess.check_call('python win-setup.py py2exe', shell = True, cwd = WDIR)
+
 # get the version
-import sys
-sys.path.insert(0, '../..')
+sys.path.insert(0, WDIR)
 from exe.engine import version
 versions = "/DEXE_VERSION=%s /DEXE_REVISION=%s /DEXE_SPLASH=%s" \
         % (version.release, version.revision, BRANDED_JPG)
 
 # brand the splash screen
+os.chdir(os.path.join(WDIR, 'installs/windows'))
 font = ImageFont.truetype("arial.ttf", 12)
 fontcolor = '#808080'
 (w, h) = font.getsize("Version:")
