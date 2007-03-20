e57498633731aaaa3aebedc66dcfe058faa9eec3
Jim Tittsler <jim@exelearning.org>
2007-03-20 04:20:44 +0000

    bump to release 0.22
    git-svn-id: file:///home/jwt/exe/trunk@2674 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/version.py
+++ exe/engine/version.py
@@ -24,7 +24,7 @@ Version Information
 """
 
 project        = "exe"
-release        = "0.21.1"
+release        = "0.22"
 revision       = "$Revision$"[11:-2]
 # if it is available, get the SVN revision
 try:
