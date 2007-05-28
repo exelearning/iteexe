f348afc37e99cc81e0515efbef1ce4da14c802d8
Jim Tittsler <jim@exelearning.org>
2007-05-28 01:42:58 +0000

    DPKG: move original debian/changelog to changelog.in so the package looks unmodified to svnversion when being automatically built
    git-svn-id: file:///home/jwt/exe/trunk@2838 38b22f21-9aea-0310-abfc-843a9883df58

--- installs/debian/ubuntu/make.py
+++ installs/debian/ubuntu/make.py
@@ -14,15 +14,12 @@ import tempfile
 EXEDIR = '../../..'
 WDIR = os.getcwd()
 changelog = 'debian/changelog'
-changelog_bak = os.path.abspath(os.path.join(EXEDIR, '..', 'changelog'))
-print changelog_bak
 
 # get the version/revision
 sys.path.insert(0, EXEDIR)
 from exe.engine import version
 
 # update a copy of the changelog with current revision
-os.rename(changelog, changelog_bak)
 cl = open(changelog, 'wt')
 cl.write("python-exe (%s-ubuntu1) unstable; urgency=low\n" % version.version)
 news = open(os.path.join(EXEDIR,'NEWS'))
@@ -38,7 +35,7 @@ cl.write("\n")
 cl.write(" -- eXe Project <exe@exelearning.org>  %s\n" %
         time.strftime("%a, %d %b %Y %H:%M:%S +1200"))
 cl.write("\n")
-cl.write(open(changelog_bak, 'rt').read())
+cl.write(open(changelog+".in", 'rt').read())
 cl.close()
 
 # make the dpkg
@@ -51,8 +48,4 @@ src = os.path.abspath('debian')
 os.chdir(EXEDIR)
 os.symlink(src, 'debian')
 subprocess.check_call('fakeroot debian/rules binary', shell = True)
-
-# put the original changelog back
-os.chdir(WDIR)
-os.rename(changelog_bak, changelog)
 
