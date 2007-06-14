6a0dddd824599e7195e1bc642fbb37100c17c68f
Jim Tittsler <jim@exelearning.org>
2007-06-14 22:47:37 +0000

    add eXeex, an eXe Firefox extension to modify Firefox menus and close behavior
    git-svn-id: file:///home/jwt/exe/trunk@2905 38b22f21-9aea-0310-abfc-843a9883df58

---
+++ exe/webui/linux-profile/extensions/exeex@exelearning.org/chrome/content/exeex.js
@@ -0,0 +1,23 @@
+// Need to set window.* methods after window is loaded apparently
+
+window.addEventListener("load", eXeexInit, true);
+
+function eXeWindowIsClosing() {
+  if (content.document.title == 'exelearning') {
+    alert("Please use eXe's\n   File... Quit\nmenu to close eXe.");
+    return false;
+  }
+  return true;
+}
+
+function eXeTryToClose(arg) {
+  alert("eXe TryToClose");
+  // don't let him close!  :-)
+  return false;
+}
+
+function eXeexInit() {
+  window.WindowIsClosing = eXeWindowIsClosing;
+  window.tryToClose = eXeTryToClose;
+}
+
