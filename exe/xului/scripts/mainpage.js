962c792cd77d9cbed68c2e512c30c9b82693277d
Jim Tittsler <jim@exelearning.org>
2007-06-16 01:10:18 +0000

    remove old code from autosave era
    git-svn-id: file:///home/jwt/exe/trunk@2910 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/xului/scripts/mainpage.js
+++ exe/xului/scripts/mainpage.js
@@ -1,6 +1,7 @@
 // ===========================================================================
 // eXe
 // Copyright 2004-2005, University of Auckland
+// Copyright 2006-2007 eXe Project, New Zealand Tertiary Education Commission
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -489,11 +490,6 @@ function fileQuit() {
     // Call file - save as
     saveWorkInProgress()
     askDirty('doQuit()')
-}
-
-function onclose(aEvent) {
-    alert("closing");
-    return false;
 }
 
 // Closes the window and stops the server
