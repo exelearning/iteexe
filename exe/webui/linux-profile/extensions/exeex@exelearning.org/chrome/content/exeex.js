7e62ed68a0eef7790ed1b58c2807ac927ca48bf5
Jim Tittsler <jim@exelearning.org>
2007-06-21 03:04:05 +0000

    don't prevent closing resources popup windows
    git-svn-id: file:///home/jwt/exe/trunk@2917 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/linux-profile/extensions/exeex@exelearning.org/chrome/content/exeex.js
+++ exe/webui/linux-profile/extensions/exeex@exelearning.org/chrome/content/exeex.js
@@ -5,7 +5,9 @@ window.addEventListener("load", eXeexInit, true);
 function eXeWindowIsClosing() {
   // get a string containing the window location (not a property)
   var loc = content.document.location + '';
-  if (loc.search(/^http:\/\/[^:]+:\d+\/editor$/) == -1) {
+  // currently difficult to identify top window by title or location
+  // so try to identify things that AREN'T the top window
+  if ((loc.search(/^http:\/\/[^:]+:\d+\/editor$/) == -1) && (loc.search(/\/resources\//) == -1) {
     alert("Please use eXe's\n   File... Quit\nmenu to close eXe.");
     return false;
   }
