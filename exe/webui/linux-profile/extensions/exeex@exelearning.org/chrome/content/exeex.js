24aab153147eb117fa069972eaaf141d13792457
Jim Tittsler <jim@exelearning.org>
2007-06-23 04:25:56 +0000

    eXeex: detect mainWindow by id
    git-svn-id: file:///home/jwt/exe/trunk@2927 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/linux-profile/extensions/exeex@exelearning.org/chrome/content/exeex.js
+++ exe/webui/linux-profile/extensions/exeex@exelearning.org/chrome/content/exeex.js
@@ -3,11 +3,8 @@
 window.addEventListener("load", eXeexInit, true);
 
 function eXeWindowIsClosing() {
-  // get a string containing the window location (not a property)
-  var loc = content.document.location + '';
-  // currently difficult to identify top window by title or location
-  // so try to identify things that AREN'T the top window
-  if ((loc.search(/^http:\/\/[^:]+:\d+\/editor$/) == -1) && (loc.search(/\/resources\//) == -1) {
+  // try to prevent closing the main window
+  if (content.document.getElementById('mainWindow').id == 'mainWindow') {
     alert("Please use eXe's\n   File... Quit\nmenu to close eXe.");
     return false;
   }
