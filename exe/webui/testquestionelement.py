e0db2dd853a0d3a8c57724990f1a7b9919637507
Jim Tittsler <jim@exelearning.org>
2007-08-31 02:18:03 +0000

    remove bold tags on SCORM quiz questions, maching other questions  (and making valid XHTML)
    git-svn-id: file:///home/jwt/exe/trunk@3239 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/webui/testquestionelement.py
+++ exe/webui/testquestionelement.py
@@ -1,6 +1,7 @@
 # ===========================================================================
 # eXe 
 # Copyright 2004-2006, University of Auckland
+# Copyright 2004-2007 eXe Project, New Zealand Tertiary Education Commission
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -128,12 +129,12 @@ class TestquestionElement(object):
         """
         Returns an XHTML string for viewing this element
         """
-        html  = "<b>"
+        html  = u""
         if preview: 
             html += self.questionElement.renderPreview()
         else:
             html += self.questionElement.renderView()
-        html += "</b><br/>\n"
+        html += "<br/>\n"
 
         html += "<table>"
         for element in self.options:
