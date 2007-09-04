ddf33f4e8d3df75f81d1d979394701f270f2163a
Jim Tittsler <jim@exelearning.org>
2007-09-04 21:49:20 +0000

    Release 1.01
    git-svn-id: file:///home/jwt/exe/trunk@3265 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/version.py
+++ exe/engine/version.py
@@ -2,7 +2,7 @@
 # ===========================================================================
 # eXe
 # Copyright 2004-2006 University of Auckland
-# Copyright 2006-2007 eXe Project, New Zealand Tertiary Education Commission
+# Copyright 2004-2007 eXe Project, New Zealand Tertiary Education Commission
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ Version Information
 import re
 
 project        = "exe"
-release        = "1.00.1"
+release        = "1.01.0"
 revision       = "$Revision$"[11:-2]
 # if it is available, get the SVN revision
 try:
