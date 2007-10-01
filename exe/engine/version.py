0d8139d194f9fe08654a38862a84050f2166ebca
Jim Tittsler <jim@exelearning.org>
2007-10-01 00:38:08 +0000

    Release 1.02
    git-svn-id: file:///home/jwt/exe/trunk@3303 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/engine/version.py
+++ exe/engine/version.py
@@ -25,7 +25,7 @@ Version Information
 import re
 
 project        = "exe"
-release        = "1.01.1"
+release        = "1.02.0"
 revision       = "$Revision$"[11:-2]
 # if it is available, get the SVN revision
 try:
