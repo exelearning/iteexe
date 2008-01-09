50cd3c667367cf74dfeae4190dc81317a99473d0
Jim Tittsler <jim@exelearning.org>
2008-01-09 03:56:04 +0000

    add rel-license microformat to license URLs on export
    git-svn-id: file:///home/jwt/exe/trunk@3348 38b22f21-9aea-0310-abfc-843a9883df58

--- exe/export/pages.py
+++ exe/export/pages.py
@@ -1,7 +1,7 @@
 # ===========================================================================
 # eXe 
 # Copyright 2004-2005, University of Auckland
-# Copyright 2004-2007 eXe Project, New Zealand Tertiary Education Commission
+# Copyright 2004-2008 eXe Project, http://eXeLearning.org/
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -67,7 +67,7 @@ class Page(object):
         if license <> "None":
             html += '<p align="center">'
             html += _("Licensed under the")
-            html += ' <a href="%s">%s</a></p>' % (licenses[license], license)
+            html += ' <a rel="license" href="%s">%s</a></p>' % (licenses[license], license)
             
         return html
     
