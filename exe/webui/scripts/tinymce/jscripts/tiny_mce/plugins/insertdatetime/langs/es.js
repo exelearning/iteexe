cd3a216db13f5c590ca0b8089e072a581d0a91b7
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2011-08-22 11:18:48 +0000

    Codificación caracteres JS. / JS Character encoding.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@46 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/insertdatetime/langs/es.js
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/insertdatetime/langs/es.js
@@ -7,6 +7,6 @@ insertdate_desc : 'Insertar fecha',
 inserttime_desc : 'Insertar hora',
 inserttime_months_long : new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"),
 inserttime_months_short : new Array("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"),
-inserttime_day_long : new Array("Domingo", "Lunes", "Martes", "Mi&eacute;rcoles", "Jueves", "Viernes", "S&aacute;bado", "Domingo"),
+inserttime_day_long : new Array("Domingo", "Lunes", "Martes", "Mi\u00E9rcoles", "Jueves", "Viernes", "S\u00E1bado", "Domingo"),
 inserttime_day_short : new Array("D", "L", "M", "X", "J", "V", "S", "D")
 });
