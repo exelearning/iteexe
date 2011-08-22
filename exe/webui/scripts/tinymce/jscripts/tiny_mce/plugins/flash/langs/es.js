22394d1c8137a25677c3d003fde6ebf55765d3a6
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2011-08-22 11:18:48 +0000

    Codificación caracteres JS. / JS Character encoding.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@46 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/flash/langs/es.js
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/flash/langs/es.js
@@ -4,7 +4,7 @@ tinyMCE.addToLang('flash',{
 title : 'Insertar/Editar Flash',
 desc : 'Insertar/Editar Flash',
 file : 'Archivo Flash (.swf)',
-size : 'Tama&ntilde;o',
+size : 'Tama\u00F1o',
 list : 'Archivos Flash',
 props : 'Propiedades Flash',
 general : 'General'
