64321062a32543973fcb89db810e4049637f7b7b
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2011-11-18 14:09:04 +0000

    Plugin para TinyMCE (pegar código embebido y código de ordenador). / TinyMCE plugin (paste embed code + computer code).
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@62 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

---
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/pastecode/langs/en.js
@@ -0,0 +1,8 @@
+// UK lang variables
+
+tinyMCE.addToLang('',{
+paste_code_desc : 'Paste as code',
+paste_code_title : 'Use CTRL+V on your keyboard to paste the code into the window.',
+paste_code_desc : 'Paste HTML (embed code)',
+paste_code_title : 'Use CTRL+V on your keyboard to paste the code into the window.'
+});
