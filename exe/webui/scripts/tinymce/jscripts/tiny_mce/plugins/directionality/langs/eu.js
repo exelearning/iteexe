caad340fc909330ebff30c02d201f1b072105a17
pedro_pena <pedro_pena@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2011-11-22 10:43:47 +0000

    [ José Miguel Andonegui ]
    Traducción al euskera de TinyMCE.
    
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@73 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

---
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/directionality/langs/eu.js
@@ -0,0 +1,6 @@
+// EU lang variables
+
+tinyMCE.addToLang('',{
+directionality_ltr_desc : 'Ezkerretik eskuinera',
+directionality_rtl_desc : 'Eskuinetik ezkerrera'
+});
---
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/print/langs/eu.js
@@ -0,0 +1,5 @@
+// EU lang variables
+
+tinyMCE.addToLang('',{
+print_desc : 'Inprimatu'
+});
