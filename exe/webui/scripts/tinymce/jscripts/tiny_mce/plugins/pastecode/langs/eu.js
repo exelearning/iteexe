53d6297280017b27aae66b4b99b65817ead7ce95
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2011-12-02 06:49:15 +0000

    Corrección i18n EN + Traducción al euskera. / EN i18n bug fixed + Basque translation.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@105 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/pastecode/langs/eu.js
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/pastecode/langs/eu.js
@@ -5,5 +5,5 @@ paste_code_desc : 'Kode gisa itsatsi',
 paste_code_title : 'Erabili CTRL+V testua lehioan itsasteko.',
 paste_html_desc : 'HTML kodea itsatsi (kaptsulatutako kodea)',
 paste_html_title : 'Erabili CTRL+V testua lehioan itsasteko.',
-paste_include_wrapper : 'Include wrapper (improves presentation)'
+paste_include_wrapper : 'Edukitzailea sartu (itxura hobetzen du)'
 });
