2849a6ae06113f8b82d3262492aaa1973f865b27
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-01-02 12:00:12 +0100

    add missing ; to the replace

--- exe/export/xliffexport.py
+++ exe/export/xliffexport.py
@@ -103,7 +103,7 @@ class XliffExport(object):
             for field in idevice.getRichTextFields():
                 content += TRANS_UNIT % {'content': safe_unicode(field.content.replace('&', '&amp;')),
                                          'id': '%s-idev%s-field%s' % (id, idevice.id, field.id),
-                                         'target': self.source_copied_in_target and safe_unicode(field.content.replace('&', '&amp')) or u'',
+                                         'target': self.source_copied_in_target and safe_unicode(field.content.replace('&', '&amp;')) or u'',
                                          }
 
             content += '</group>'
