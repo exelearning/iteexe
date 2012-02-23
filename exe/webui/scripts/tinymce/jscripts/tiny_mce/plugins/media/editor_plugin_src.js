3d5e86294608c8a6827fb4f718750521a3885989
pedro_pena <pedro_pena@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-02-23 11:28:55 +0000

    Se elimina la generación del enlace "Resumen textual alternativo" al insertar un elemento multimedia
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@191 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/media/editor_plugin_src.js
+++ exe/webui/scripts/tinymce/jscripts/tiny_mce/plugins/media/editor_plugin_src.js
@@ -2,7 +2,7 @@
  * $Id: editor_plugin_src.js 201 2007-02-12 15:56:56Z spocke $
  *
  * @author Moxiecode
- * @copyright Copyright © 2004-2007, Moxiecode Systems AB, All rights reserved.
+ * @copyright Copyright ï¿½ 2004-2007, Moxiecode Systems AB, All rights reserved.
  */
 
 /* Import plugin specific language pack */
@@ -341,11 +341,6 @@ var TinyMCE_MediaPlugin = {
 					// Insert embed/object chunk
 					chunkBefore = content.substring(0, startPos);
 					chunkAfter = content.substring(endPos);
-					//JR
-					if (incluirDiv) {
-						embedHTML = '<div class="elemento_centrado">\n<div class="elemento_centrado">' + embedHTML;
-						embedHTML = embedHTML + '\n</div>\n<div class="credenciales elemento_centrado"><a href="resumen.html">Resumen textual alternativo</a></div>\n</div>';
-					}
 					content = chunkBefore + embedHTML + chunkAfter;
 				}
 				break;
