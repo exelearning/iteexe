bc39f74d7e7ba53c6b43ac62fb86bcca8c6b0d99
pedro_pena <pedro_pena@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-06-14 16:10:53 +0000

    Revisión de textos de los créditos y tamaño de ventana
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@291 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/xului/scripts/mainpage.js
+++ exe/xului/scripts/mainpage.js
@@ -579,7 +579,7 @@ function metadataEditor() {
 
 // load the About page
 function aboutPage() {
-    var features = "width=299,height=515,status=0,resizable=0,left=260,top=150";
+    var features = "width=360,height=615,status=0,resizable=0,left=260,top=150";
     aboutWin = window.open ("/about", ABOUT, features);
 }
 
