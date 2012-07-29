df5cc5000174f718afb56daf1c32a47afd426650
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-07-29 08:40:12 +0000

    Plugin exemath para TinyMCE (versión intef6.1 de eXeLearning). / intef6.1 exemath plugin for TinyMCE.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@450 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

---
+++ exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/exemath/langs/es.js
@@ -0,0 +1,28 @@
+// ES lang variables
+
+tinyMCE.addToLang('exemath',{
+desc : 'LaTeX a Math',
+contextmenu_update : 'Propiedades de la imagen LaTeX Math',
+generate_title : 'Imagen LaTeX Math para eXe',
+verbose_font_size : 'Seleccione tama\u00F1o de fuente:', 
+font_size : 'Tama\u00F1o de fuente:', 
+compile_tooltip : 'Previsualizar imagen Math',
+compile_label:  'Previsualizar',
+latex_source : 'Introducir c\u00F3digo LaTeX:',
+image_src : 'Imagen Math generada',
+image_alt : 'Texto alternativo de la imagen:',
+tab_general : 'Generador',
+tab_appearance : 'Apariencia de la imagen',
+insert_text: 'Contenido textual',
+insert_newline: 'Nueva l\u00EDnea',
+verbose_insert_symbols: 'Pinche para insertar el c\u00F3digo LaTeX de los s\u00EDmbolos:',
+insert_symbols: ' ',
+verbose_insert_latex: 'A\u00F1adir su propio c\u00F3digo LaTeX:',
+insert_latex: ' ',
+general : 'BOGUS: General',
+title : 'T\u00EDtulo',
+preview : 'Previsualizaci\u00F3n de la imagen',
+constrain_proportions : 'Restringir proporciones',
+style : 'Estilo',
+example_img : 'Imagen&nbsp;de&nbsp;previsualizaci\u00F3n'
+});
---
+++ exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/exemath/langs/eu.js
@@ -0,0 +1,28 @@
+// EU lang variables
+
+tinyMCE.addToLang('exemath',{
+desc : 'LaTeXetik Mathera',
+contextmenu_update : 'LaTeX Math irudiaren ezaugarriak',
+generate_title : 'LaTeX Math irudia eXerentzat',
+verbose_font_size : 'Aukeratu letra-tipoaren tamaina:', 
+font_size : 'Letra-tipoaren tamaina:',
+compile_tooltip : 'Math irudia aurreikusi',
+compile_label:  'Aurreikusi',
+latex_source : 'Latex kodea sartu:',
+image_src : 'Sortutako Math irudia',
+image_alt : 'Irudiaren deskribapena:',
+tab_general : 'Sorgailua',
+tab_appearance : 'Irudiaren itxura',
+insert_text: 'Testu edukia',
+insert_newline: 'Lerro berria',
+verbose_insert_symbols: 'Sinboloen LaTeX kodea txertatu:',
+insert_symbols: ' ',
+verbose_insert_latex: 'Latex kodea erantsi:',
+insert_latex: ' ',
+general : 'BOGUS: General',
+title : 'Izenburua',
+preview : 'Irudiaren aurrebista',
+constrain_proportions : 'Proportzioak mantendu',
+style : 'Estiloa',
+example_img : 'Nola ikusiko den'
+});
