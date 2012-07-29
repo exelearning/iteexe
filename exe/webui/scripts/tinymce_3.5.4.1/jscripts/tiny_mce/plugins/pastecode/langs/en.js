33e1470742db437f54b2c6d683a6dd28760a0f10
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-07-29 09:00:39 +0000

    Plugin pastecode para TinyMCE (versión intef6.1 de eXeLearning). / intef6.1 pastecode plugin for TinyMCE.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@452 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

---
+++ exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/pastecode/langs/en.js
@@ -0,0 +1,9 @@
+// UK lang variables
+
+tinyMCE.addToLang('',{
+paste_code_desc : 'Paste as code',
+paste_code_title : 'Use CTRL+V on your keyboard to paste the code into the window.',
+paste_html_desc : 'Paste HTML (embed code)',
+paste_html_title : 'Use CTRL+V on your keyboard to paste the code into the window.',
+paste_include_wrapper : 'Include wrapper (improves presentation)'
+});
