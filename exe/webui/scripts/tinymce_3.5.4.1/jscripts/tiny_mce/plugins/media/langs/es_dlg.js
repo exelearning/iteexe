065aaca4d6889e192f60b5b59d446f082013466f
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-09-28 05:46:43 +0000

    Refactorización y simplificación del código. / Code simplification and refactoring.
    git-svn-id: https://svn.forge.morfeo-project.org/iteexe/trunk@481 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/media/langs/es_dlg.js
+++ exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/media/langs/es_dlg.js
@@ -6,9 +6,6 @@ recommended_type : "Tipo recomendado",
 confirm_question : "\u00BFContinuar sin hacer cambios?",
 preview_error : "No se ha podido generar una previsualizaci\u00F3n",
 video_download_file_warning : "Si el navegador del usuario no puede reproducir el v\u00EDdeo se mostrar\u00E1 un enlace al archivo.",
-audio_download_file_warning : "Si el navegador del usuario no puede reproducir el audio se mostrar\u00E1 un enlace al archivo.",
-download_file_label : "Texto del enlace",
-file_name : "Nombre del archivo",
-download : "Descargar"
+audio_download_file_warning : "Si el navegador del usuario no puede reproducir el audio se mostrar\u00E1 un enlace al archivo."
 // The New eXeLearning
 });
