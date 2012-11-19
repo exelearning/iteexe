d91913efc92ad6d4339be4b3a3211ad2c84f9ae1
ignaciogros <ignaciogros@da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05>
2012-11-19 18:44:25 +0000

    Gallego (gl) completo (plugins propios). / Galician (gl) complete (eXe plugins).
    git-svn-id: https://forja.cenatic.es/svn/iteexe/trunk@645 da0ca0ef-283e-4aa9-9c6d-6a5f12f73a05

--- exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/pastecode/langs/gl.js
+++ exe/webui/scripts/tinymce_3.5.4.1/jscripts/tiny_mce/plugins/pastecode/langs/gl.js
@@ -1,8 +1,8 @@
 // GL lang variables
 tinyMCE.addI18n('gl.pastecode',{
-paste_code_desc : 'Insertar HTML como texto',
-paste_code_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_html_desc : 'Pegar fragmento de HTML (c\u00F3digo embebido)',
-paste_html_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_include_wrapper : 'Incluir contenedor (mejora la presentaci\u00F3n)'
+paste_code_desc : 'Inserir HTML como texto',
+paste_code_title : 'Utilice CTRL-V para pegalo c\u00F3digo na vent\u00E1.',
+paste_html_desc : 'Pegar anaco de HTML (c\u00F3digo embebido)',
+paste_html_title : 'Utilice CTRL-V para pegalo c\u00F3digo na vent\u00E1.',
+paste_include_wrapper : 'Incluir contedor (mellora a presentaci\u00F3n)'
 });
