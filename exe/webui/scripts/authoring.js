6bcf83c0ab58ab68464be086adefc41d19be0efb
ignaciogros <ignacio@gros.es>
2012-12-04 17:50:29 +0100

    Evitamos el error de las anclas en modo edición. / No error page when clicking on the anchors while editing.

--- exe/webui/scripts/authoring.js
+++ exe/webui/scripts/authoring.js
@@ -493,3 +493,22 @@ function showObjectTags() {
     for (var i=0; i < authoringPluginObjects.length; i++)
         authoringPluginObjects[i].style.visibility = "visible";
 }
+
+var disableAnchorsIneXe = function() {
+    var as = document.getElementsByTagName("A");
+    var i = as.length;
+    while (i--) {
+        if (as[i].href) {
+            if (as[i].href.indexOf("exe-node:")==0) {
+                as[i].onclick = function(){
+                    return false;
+                }
+            }
+        }
+    }
+}
+if (document.addEventListener){
+	window.addEventListener('load',disableAnchorsIneXe,false);
+} else {
+	window.attachEvent('onload',disableAnchorsIneXe);
+}
