c69f3cd18ca3f4cdbbe26c8e5c52e6787474256c
ignaciogros <ignacio@gros.es>
2012-12-14 11:37:55 +0100

    Revisión de las traducciones al euskera. / EU translations revision.

--- exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/advhr/langs/eu_dlg.js
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/advhr/langs/eu_dlg.js
@@ -1,1 +1,1 @@
-tinyMCE.addI18n('eu.advhr_dlg',{size:"Altuera",noshade:"Itzalik gabe",width:"Zabalera",normal:"Normal",widthunits:"Unitateak"});
+tinyMCE.addI18n('eu.advhr_dlg',{size:"Altuera",noshade:"Itzalik gabe",width:"Zabalera",normal:"Ohikoa",widthunits:"Unitateak"});
