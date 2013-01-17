d783c677c64839c64acfc5df39fffe0a77f1ea1d
ignaciogros <ignacio@gros.es>
2013-01-17 12:43:45 +0100

    TinyMCE: los plugins de eXe (o modificados para eXe) en francés. / TinyMCE: eXe plugins (or modified for eXe) in French.

--- exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/fr.js
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/fr.js
@@ -1,8 +1,8 @@
 // FR lang variables
 tinyMCE.addI18n('fr.pastecode',{
-paste_code_desc : 'Insertar HTML como texto',
-paste_code_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_html_desc : 'Pegar fragmento de HTML (c\u00F3digo embebido)',
-paste_html_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_include_wrapper : 'Incluir contenedor (mejora la presentaci\u00F3n)'
+paste_code_desc : 'Ins\u00E9rer HTML sous texte',
+paste_code_title : 'Utiliser CTRL+V pour coller le code dans la fen\u00EAtre',
+paste_html_desc : 'Coller fragment de HTML (code int\u00E9gr\u00E9)',
+paste_html_title : 'Utiliser CTRL+V pour coller le code dans la fen\u00EAtre',
+paste_include_wrapper : 'Inclure contenant (la presentation sera amelior\u00E9e)'
 });
