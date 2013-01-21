ea822f5cfa3c10d1b4c91962123fa149d1d2e3d6
franmacias <franmacias@yahoo.es>
2013-01-21 20:03:10 +0100

    Añadido campo para diferentes items en lista desplegable

--- exe/webui/listablock.py
+++ exe/webui/listablock.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 # ===========================================================================
 # eXe 
 # Copyright 2004-2006, University of Auckland
@@ -25,6 +26,7 @@ import logging
 from exe.webui.block   import Block
 from exe.webui         import common
 from exe.webui.element import TextAreaElement,ElementWithResources
+
 import random
 
 
@@ -37,15 +39,16 @@ class ListaElement(ElementWithResources):
     iDevice para crear listas desplegables, basado en clozeidevice. 
     """
 
-    # Properties
-
+    #otras=''
+    #otrasInstruc=('Write other words')
+     
     @property
     def editorId(self):
         """
         Returns the id string for our midas editor
         """
         return 'editorArea%s' % self.id
-
+    
     @property
     def editorJs(self):
         """
@@ -62,11 +65,15 @@ class ListaElement(ElementWithResources):
         return "document.getElementById('cloze%s')" % self.id
 
     # Public Methods
-
+   
+            
+        
     def process(self, request):
         """
         Sets the encodedContent of our field
         """
+       
+        
         is_cancel = common.requestHasCancel(request)
 
         if is_cancel:
@@ -79,6 +86,12 @@ class ListaElement(ElementWithResources):
                 self.field.content_wo_resourcePaths = ""
                 self.field.idevice.edit = True
             return
+        
+        if "otras"+self.id in request.args \
+        and not is_cancel:
+            self.field.otras = \
+            self.field.ProcessPreviewed(request.args["otras"+self.id][0])
+
 
         if self.editorId in request.args:
             # process any new images and other resources courtesy of tinyMCE:
@@ -102,25 +115,61 @@ class ListaElement(ElementWithResources):
         """
         Enables the user to set up their passage of text
         """
+        def decrypt(word):
+            #             Simple XOR encryptions
+            answer = u""
+            code_key = 'X'
+            
+            codeu=word
+            codeu=codeu.replace("\r", "")
+            codeu=codeu.replace("\n", "")
+
+            code = codeu.decode('base64')
+            char_pos = 0
+            
+            while char_pos < len(code):
+                        # first 2 chars = %u, replace with 0x to get int
+                        # next 4 = the encoded unichr
+                        this_code_char = "0x" + code[char_pos+2 : char_pos+6]
+                        this_code_ord = int(this_code_char, 16)
+                        letter = unichr(ord(code_key)^this_code_ord)
+                        answer += letter
+                        # key SHOULD be ^'d by letter, but seems to be:
+                        code_key = letter
+                        char_pos += 6
+           
+            
+
+            return answer
+        
+
         # to render, choose the content with the preview-able resource paths:
         self.field.encodedContent = self.field.content_w_resourcePaths
-
         this_package = None
         if self.field_idevice is not None \
         and self.field_idevice.parentNode is not None:
             this_package = self.field_idevice.parentNode.package
-
+        if self.field.otras:
+            self.field.otras=decrypt(self.field.otras)
+            
         html = [
             # Render the iframe box
             common.formField('richTextArea', this_package, _('Text'),'',
                              self.editorId, self.field.instruc,
                              self.field.encodedContent),
             # Render our toolbar
-         
+            
             u'  <input type="button" value="%s" ' % _("Hide/Show Word"),
             u' onclick="tinyMCE.execInstanceCommand(\'%s\',\'Underline\', false);" />' % self.editorId,
-            u'</br></br>'
-            
+            u'</br></br>',
+            common.formField('textInput',
+                            this_package,
+                            _('Other words'),
+                            'otras'+self.id, '',
+                            self.field.otrasInstruc,
+                            self.field.otras,
+                            size=80),          
+            u'</br></br>',
             ]
         
         return '\n    '.join(html)
@@ -153,7 +202,7 @@ class ListaElement(ElementWithResources):
             value = str(bool(getattr(self.field, name))).lower()
             return common.hiddenField('clozeFlag%s.%s' % (self.id, name), value)
         #html.append(storeValue('showScore'))
-          
+      
         if feedbackId:
             html.append(common.hiddenField('clozeVar%s.feedbackId' % self.id,
                                            'ta'+feedbackId))
@@ -161,6 +210,8 @@ class ListaElement(ElementWithResources):
         words = ""
         wordslista='<option selected="selected" > </option>'
         wordsarray=[]
+        listaotras=[]
+        listaotras2=[]
         wordsarraylimpo=[] 
         def encrypt(word):
             #             Simple XOR encryptions
@@ -179,8 +230,11 @@ class ListaElement(ElementWithResources):
         for i, (text, missingWord) in enumerate(self.field.parts):
             if missingWord:
                 wordsarray.append([missingWord])
-        
-
+        listaotras=self.field.otras.split('|')
+        for i, (missingWord) in enumerate(listaotras):
+            if missingWord:
+                listaotras2.append([missingWord])
+        wordsarray=wordsarray + listaotras2 
         wordsarraylimpo= [wordsarray[i] for i in range(len(wordsarray)) if wordsarray[i] not in wordsarray[:i]]
         random.shuffle(wordsarraylimpo)
         for wdlista in wordsarraylimpo:
@@ -210,16 +264,16 @@ class ListaElement(ElementWithResources):
         
        
         html += [common.button('submit%s' % self.id,
-                    _(u"Submit"),
+                    _(u"Check"),
                     id='submit%s' % self.id,
                     onclick="clozeSubmit('%s')" % self.id),
                   common.button('submit%s' % self.id,
-                    _(u"Submit"),
-                    id='restart%s' % self.id,
+                    _(u"Check"),
+                    id='restart%s' % self.id,                   
                     style="display: none;",
                     onclick="clozeSubmit('%s')" % self.id),
                 ]       
-         
+        html += ['<input type="hidden" name="otras%s" id="otras%s" value="%s"/>' % (self.id,self.id,encrypt(self.field.otras))]
         html += ['<input type="hidden" name="clozeFlag%s.strictMarking" id="clozeFlag%s.strictMarking" value="false"/>' % (self.id,self.id)]
         html += ['<input type="hidden" name="clozeFlag%s.checkCaps" id="clozeFlag%s.checkCaps" value="false"/>' % (self.id,self.id)]
         html += ['<input type="hidden" name="clozeFlag%s.instantMarking" id="clozeFlag%s.instantMarking" value="false"/>' % (self.id,self.id)]
@@ -269,7 +323,8 @@ class ListaBlock(Block):
         Pre-create our field ids
         """
         Block.__init__(self, parent, idevice)
-
+        
+       
         # to compensate for the strange unpickling timing when objects are 
         # loaded from an elp, ensure that proper idevices are set:
         if idevice.instructionsForLearners.idevice is None: 
