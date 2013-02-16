aa13aa85299b89666eeac39efe0fb9e921bbaa10
ignaciogros <ignacio@gros.es>
2013-02-16 10:24:20 +0100

    TinyMCE en italiano completo. / TinyMCE: Italian translation complete.

--- exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/it.js
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/it.js
@@ -1,8 +1,8 @@
 // IT lang variables
 tinyMCE.addI18n('it.pastecode',{
-paste_code_desc : 'Insertar HTML como texto',
-paste_code_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_html_desc : 'Pegar fragmento de HTML (c\u00F3digo embebido)',
-paste_html_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_include_wrapper : 'Incluir contenedor (mejora la presentaci\u00F3n)'
+paste_code_desc : 'Inserire HTML come solo testo',
+paste_code_title : 'Usare CTRL-V per incollare il codice nella finestra.',
+paste_html_desc : 'Incollare frammento HTML (codice incluso)',
+paste_html_title : 'Usare CTRL-V per incollare il codice nella finestra.',
+paste_include_wrapper : 'Inc. contenitore (visualizzazione migliore)'
 });
