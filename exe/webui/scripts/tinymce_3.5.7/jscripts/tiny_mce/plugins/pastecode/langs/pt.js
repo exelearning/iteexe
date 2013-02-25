de032a989e07e77041eb5cc9f835a2f15b233d08
ignaciogros <ignacio@gros.es>
2013-02-25 20:43:07 +0100

    TinyMCE: plugins de eXe en portugués, por Dionisio Martínez Soler. / TinyMCE: eXe Plugins Portuguese translation by Dionisio Martínez Soler.

--- exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/pt.js
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/pastecode/langs/pt.js
@@ -1,8 +1,8 @@
 // PT lang variables
 tinyMCE.addI18n('pt.pastecode',{
-paste_code_desc : 'Insertar HTML como texto',
-paste_code_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_html_desc : 'Pegar fragmento de HTML (c\u00F3digo embebido)',
-paste_html_title : 'Use CTRL+V para pegar el c\u00F3digo en la ventana.',
-paste_include_wrapper : 'Incluir contenedor (mejora la presentaci\u00F3n)'
+paste_code_desc : 'Inserir HTML como texto',
+paste_code_title : 'Use CTRL+V para colar o c\u00F3digo na janela.',
+paste_html_desc : 'Colar fragmento de HTML (c\u00F3digo incrustado)',
+paste_html_title : 'Use CTRL+V para colar o c\u00F3digo na janela.',
+paste_include_wrapper : 'Incluir contentor (melhora a apresenta\u00E7\u00E3o)'
 });
