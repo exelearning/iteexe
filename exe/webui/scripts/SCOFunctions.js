78125d127465bac9530d5a4ee99632d743a60dd7
ampcrpgfa <a.antonio.moreno@gmail.com>
2013-04-29 17:56:27 +0200

    uevas funciones en SCORM_API_Wrapper - J.Andonegui / New functions added to SCORM_API_Wrapper.js - J.Andonegui

--- exe/webui/scripts/SCOFunctions.js
+++ exe/webui/scripts/SCOFunctions.js
@@ -2,11 +2,11 @@
 ** 
 ** Filename: SCOFunctions.js
 **
-** File Description: adaption of SCOFunctions.js file from ADL Technical Team
+** File Description: adaptation of SCOFunctions.js file from ADL Technical Team
 ** SCOFunctions.js works with SCORM12 and SCOFunctions2004.js with SCORM2004
 ** using SCORM_API_wrapper.js
 **
-** Author: José Miguel Andonegi jm.andonegi@gmail.com
+** Adaptation: José Miguel Andonegi jm.andonegi@gmail.com
 **
 ********************************************************************************
 **
@@ -111,13 +111,12 @@ function computeTime()
    if ( startDate != 0 )
    {
       var currentDate = new Date().getTime();
-      var elapsedSeconds = ( (currentDate - startDate) / 1000 );
-      var formattedTime = convertTotalSeconds( elapsedSeconds );
+      var elapsedMiliSeconds = (currentDate - startDate);
+      var formattedTime = pipwerks.UTILS.convertTotalMiliSeconds(elapsedMiliSeconds);
    }
    else
    {
-	//JM:pending: create a formatting wrapper function
-      formattedTime = "00:00:00.0";
+      formattedTime = pipwerks.UTILS.convertTotalMiliSeconds(0);
    }
 
    scorm.SetSessionTime(formattedTime);
@@ -214,60 +213,6 @@ function unloadPage()
 	//		  liberty of displaying a confirm message box.
 }
 
-/*******************************************************************************
-** this function will convert seconds into hours, minutes, and seconds in
-** CMITimespan type format - HHHH:MM:SS.SS (Hours has a max of 4 digits &
-** Min of 2 digits
-*******************************************************************************/
-function convertTotalSeconds(ts)
-{
-   var sec = (ts % 60);
-
-   ts -= sec;
-   var tmp = (ts % 3600);  //# of seconds in the total # of minutes
-   ts -= tmp;              //# of seconds in the total # of hours
-
-   // convert seconds to conform to CMITimespan type (e.g. SS.00)
-   sec = Math.round(sec*100)/100;
-   
-   var strSec = new String(sec);
-   var strWholeSec = strSec;
-   var strFractionSec = "";
-
-   if (strSec.indexOf(".") != -1)
-   {
-      strWholeSec =  strSec.substring(0, strSec.indexOf("."));
-      strFractionSec = strSec.substring(strSec.indexOf(".")+1, strSec.length);
-   }
-   
-   if (strWholeSec.length < 2)
-   {
-      strWholeSec = "0" + strWholeSec;
-   }
-   strSec = strWholeSec;
-   
-   if (strFractionSec.length)
-   {
-      strSec = strSec+ "." + strFractionSec;
-   }
-
-
-   if ((ts % 3600) != 0 )
-      var hour = 0;
-   else var hour = (ts / 3600);
-   if ( (tmp % 60) != 0 )
-      var min = 0;
-   else var min = (tmp / 60);
-
-   if ((new String(hour)).length < 2)
-      hour = "0"+hour;
-   if ((new String(min)).length < 2)
-      min = "0"+min;
-
-   var rtnVal = hour+":"+min+":"+strSec;
-
-   return rtnVal;
-}
 
 function goBack() {
 	pipwerks.nav.goBack();
