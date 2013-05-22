aa9424c65160c5456e43b800995ee9b2210c89fb
ignaciogros <ignacio@gros.es>
2013-05-22 10:21:49 +0200

    Errores al mostrar/ocultar retroalimentación por el cambio realizado en el commit c4b6c82fedb9836db12c1bee958d4511ed624114 en elementy.py (desaparecen los estilos en línea). / Error showing/hiding the feedback due to a change made in element.py (commit c4b6c82fedb9836db12c1bee958d4511ed624114): No inline styles. / https://forja.cenatic.es/tracker/index.php?func=detail&aid=1756&group_id=197&atid=883

--- exe/webui/scripts/common_src.js
+++ exe/webui/scripts/common_src.js
@@ -443,10 +443,15 @@ function toggleElementVisible(ident) {
     // Toggle the visibility of an element
     var element = document.getElementById(ident);
     if (element) {
-    	if ((element.style.display == "none")||(element.style.display == "")) {
-            element.style.display = "block";
-        } else {
+        var c = element.className;
+        if (c.indexOf("iDevice_hidden_content")!=-1) {
             element.style.display = "none";
+            element.className = c.replace("iDevice_hidden_content","iDevice_content");
+        }
+        if (element.style.display != "none") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "";
         }
     }
 }
@@ -794,38 +799,6 @@ function getClozelangAnswer(ele) {
     }
     return result
 }
-
-// Base64 Decode
-// Base64 code from Tyler Akins -- http://rumkin.com
-//function decode64(input) {
-//   var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-//   var output = "";
-//   var chr1, chr2, chr3;
-//   var enc1, enc2, enc3, enc4;
-//   var i = 0;
-//   // Remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-//   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-//   do {
-//      enc1 = keyStr.indexOf(input.charAt(i++));
-//      enc2 = keyStr.indexOf(input.charAt(i++));
-//      enc3 = keyStr.indexOf(input.charAt(i++));
-//      enc4 = keyStr.indexOf(input.charAt(i++));
-//
-//      chr1 = (enc1 << 2) | (enc2 >> 4);
-//      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-//      chr3 = ((enc3 & 3) << 6) | enc4;
-//
-//      output = output + String.fromCharCode(chr1);
-//
-//      if (enc3 != 64) {
-//         output = output + String.fromCharCode(chr2);
-//      }
-//      if (enc4 != 64) {
-//         output = output + String.fromCharCode(chr3);
-//      }
-//   } while (i < input.length);
-//   return output;
-//}
 
 // Returns the corrected word or an empty string
 function checkClozelangWord(ele) {
