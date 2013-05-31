e08ff6b601680c24043383be696be88002a3f5ae
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2013-05-31 12:53:17 +0200

    Initial version of new common interface for cataloguing

--- exe/jsui/scripts/app/view/forms/PackagePanel.js
+++ exe/jsui/scripts/app/view/forms/PackagePanel.js
@@ -30,19 +30,273 @@ Ext.define('eXe.view.forms.PackagePanel', {
             items: [
                 {
                     xtype: 'fieldset',
-                    title: _('Project Properties'),
+                    title: _('Cataloguing'),
                     margin: 10,
                     defaults: {
                         dirtyCls: 'property-form-dirty'
                     },
                     items: [
                         {
-                            xtype: 'textfield',
-                            inputId: 'pp_title',
-                            fieldLabel: _('Project Title'),
-                            tooltip: _("The project's title."),
-                            anchor: '100%'
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+                                xtype: 'textfield',
+                                inputId: 'pp_title',
+                                dirtyCls: 'property-form-dirty',
+                                fieldLabel: _('Title'),
+                                tooltip: _('The name given to the resource.'),
+                                anchor: '100%'
+                            },
+                            help: _('The name given to the resource.')
                         },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+                                xtype: 'combobox',
+                                inputId: 'dc_language',
+                                dirtyCls: 'property-form-dirty',
+                                fieldLabel: _('Language'),
+                                store: langsStore,
+                                tooltip: _('Select a language.'),
+                                anchor: '100%'
+                            },
+                            help: _('Select a language.')
+                        },
+                        {
+                            xtype: 'fieldset',
+		                    title: _('Description'),
+                            items: [
+                                {
+                                    xtype: 'helpcontainer',
+		                            margin: '0 0 10 0',
+		                            item: {
+			                            xtype: 'textarea',
+			                            inputId: 'pp_description',
+			                            fieldLabel: _('General'),
+			                            tooltip: _('An account of the content of the resource.'),
+			                            height: 60,
+			                            anchor: '100%'
+                                    },
+                                    help: _('An account of the content of the resource.')
+                                },
+                                {
+                                    xtype: 'helpcontainer',
+                                    margin: '0 0 10 0',
+                                    item: {
+                                        xtype: 'textarea',
+                                        inputId: 'pp_objectives',
+                                        fieldLabel: _('Objectives'),
+                                        tooltip: _('Objectives.'),
+                                        height: 60,
+                                        anchor: '100%'
+                                    },
+                                    help: _('Objectives.')
+                                },
+                                {
+                                    xtype: 'helpcontainer',
+                                    margin: '0 0 10 0',
+                                    item: {
+                                        xtype: 'textarea',
+                                        inputId: 'pp_preknowledge',
+                                        fieldLabel: _('Preknowledge'),
+                                        tooltip: _('Preknowledge.'),
+                                        height: 60,
+                                        anchor: '100%'
+                                    },
+                                    help: _('Preknowledge.')
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+	                            xtype: 'textfield',
+	                            inputId: 'pp_author',
+	                            fieldLabel: _('Author'),
+	                            tooltip: _('Primary author of the resource.'),
+	                            anchor: '100%'
+                            },
+                            help: _('Primary author of the resource.')
+                        },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+                                xtype: 'combobox',
+                                inputId: 'cat_learningResourceType',
+                                dirtyCls: 'property-form-dirty',
+                                fieldLabel: _('Learning Resource Type'),
+                                store: lomesVocab.learningResourceTypeValues,
+                                tooltip: _('Learning Resource Type'),
+                                anchor: '100%'
+                            },
+                            help: _('Learning Resource Type')
+                        },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+	                            xtype: 'fieldcontainer',
+	                            fieldLabel: _('Intended End User Role'),
+	                            tooltip: _('Intended End User Role'),
+	                            defaults: {
+	                                defaults: {
+	                                    width: 150
+	                                }
+	                            },
+	                            items: [
+	                                {
+	                                    xtype: 'container',
+	                                    layout: 'hbox',
+	                                    items: [
+			                                {
+				                                xtype: 'radio',
+			                                    boxLabel: 'Aprendiz: Alumno',
+				                                name: 'cat_intendedEndUserRole'
+			                                },
+			                                {
+			                                    xtype: 'checkbox',
+			                                    boxLabel: 'NEE',
+			                                    inputId: 'cat_intendedEndUserRole_1_1'
+			                                },
+			                                {
+			                                    xtype: 'checkbox',
+			                                    boxLabel: 'Altas capacidades',
+			                                    inputId: 'cat_intendedEndUserRole_1_2'
+			                                }
+	                                    ]
+	                                },
+	                                {
+	                                    xtype: 'container',
+	                                    layout: 'hbox',
+	                                    items: [
+	                                        {
+			                                    xtype: 'radio',
+			                                    boxLabel: 'Agrupación: Individual',
+			                                    name: 'cat_intendedEndUserRole'
+			                                },
+			                                {
+			                                    xtype: 'checkbox',
+			                                    boxLabel: 'Grupo',
+			                                    inputId: 'cat_intendedEndUserRole_2'
+			                                }
+	                                    ]
+	                                },
+	                                {
+	                                    xtype: 'container',
+	                                    layout: 'hbox',
+	                                    items: [
+	                                        {
+			                                    xtype: 'radio',
+			                                    boxLabel: 'Educador: Docente',
+			                                    name: 'cat_intendedEndUserRole'
+			                                },
+			                                {
+			                                    xtype: 'checkbox',
+			                                    boxLabel: 'Tutor',
+			                                    inputId: 'cat_intendedEndUserRole_3'
+			                                }
+	                                    ]
+	                                }
+	                            ]
+	                        },
+                            help: _('Intended End User Role')
+                        },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+                                xtype: 'fieldcontainer',
+                                fieldLabel: _('Context'),
+                                tooltip: _('Context'),
+                                defaults: {
+                                    defaults: {
+                                        width: 150
+                                    }
+                                },
+                                items: [
+                                    {
+                                        xtype: 'container',
+                                        layout: 'hbox',
+                                        items: [
+                                            {
+                                                xtype: 'radio',
+                                                boxLabel: 'Lugar: Aula',
+                                                name: 'cat_context'
+                                            },
+                                            {
+                                                xtype: 'checkbox',
+                                                boxLabel: 'Fuera del centro',
+                                                inputId: 'cat_context_1'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'container',
+                                        layout: 'hbox',
+                                        items: [
+                                            {
+                                                xtype: 'radio',
+                                                boxLabel: 'Modalidad: Presencial',
+                                                name: 'cat_context'
+                                            },
+                                            {
+                                                xtype: 'checkbox',
+                                                boxLabel: 'Distancia',
+                                                inputId: 'cat_context_2_1'
+                                            },
+                                            {
+                                                xtype: 'checkbox',
+                                                boxLabel: 'Semipresencial',
+                                                inputId: 'cat_context_2_2'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            help: _('Context')
+                        },
+                        {
+                            xtype: 'helpcontainer',
+                            margin: '0 0 10 0',
+                            item: {
+	                            xtype: 'combobox',
+	                            inputId: 'pp_license',
+	                            fieldLabel: _('License'),
+	                            store: [
+	                                  ["None", "None"],
+	                                  ["GNU Free Documentation License", _("GNU Free Documentation License")],
+	                                  ["Creative Commons Attribution 3.0 License", _("Creative Commons Attribution 3.0 License")],
+	                                  ["Creative Commons Attribution Share Alike 3.0 License", _("Creative Commons Attribution Share Alike 3.0 License")],
+	                                  ["Creative Commons Attribution No Derivatives 3.0 License", _("Creative Commons Attribution No Derivatives 3.0 License")],
+	                                  ["Creative Commons Attribution Non-commercial 3.0 License", _("Creative Commons Attribution Non-commercial 3.0 License")],
+	                                  ["Creative Commons Attribution Non-commercial Share Alike 3.0 License", _("Creative Commons Attribution Non-commercial Share Alike 3.0 License")],
+	                                  ["Creative Commons Attribution Non-commercial No Derivatives 3.0 License", _("Creative Commons Attribution Non-commercial No Derivatives 3.0 License")],
+	                                  ["Creative Commons Attribution 2.5 License", _("Creative Commons Attribution 2.5 License")],
+	                                  ["Creative Commons Attribution-ShareAlike 2.5 License", _("Creative Commons Attribution-ShareAlike 2.5 License")],
+	                                  ["Creative Commons Attribution-NoDerivs 2.5 License", _("Creative Commons Attribution-NoDerivs 2.5 License")],
+	                                  ["Creative Commons Attribution-NonCommercial 2.5 License", _("Creative Commons Attribution-NonCommercial 2.5 License")],
+	                                  ["Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License", _("Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License")],
+	                                  ["Creative Commons Attribution-NonCommercial-NoDerivs 2.5 License", _("Creative Commons Attribution-NonCommercial-NoDerivs 2.5 License")],
+	                                  ["Developing Nations 2.0", _("Developing Nations 2.0")]
+	                            ],
+	                            tooltip: _('Select a license.'),
+	                            anchor: '100%'
+	                        },
+                            help: _('Select a license.')
+                        }
+                    ]
+                },
+                {
+                    xtype: 'fieldset',
+                    title: _('Project Properties'),
+                    margin: 10,
+                    defaults: {
+                        dirtyCls: 'property-form-dirty'
+                    },
+                    items: [
                         {
                             xtype: 'container',
                             layout: 'hbox',
@@ -105,49 +359,10 @@ Ext.define('eXe.view.forms.PackagePanel', {
                             ]
                         },
                         {
-                            xtype: 'textfield',
-                            inputId: 'pp_author',
-                            fieldLabel: _('Author'),
-                            tooltip: _('Primary author of the resource.'),
-                            anchor: '100%'
-                        },
-                        {
-                            xtype: 'combobox',
-                            inputId: 'pp_license',
-                            fieldLabel: _('License'),
-                            store: [
-                                  ["None", "None"],
-                                  ["GNU Free Documentation License", _("GNU Free Documentation License")],
-                                  ["Creative Commons Attribution 3.0 License", _("Creative Commons Attribution 3.0 License")],
-                                  ["Creative Commons Attribution Share Alike 3.0 License", _("Creative Commons Attribution Share Alike 3.0 License")],
-                                  ["Creative Commons Attribution No Derivatives 3.0 License", _("Creative Commons Attribution No Derivatives 3.0 License")],
-                                  ["Creative Commons Attribution Non-commercial 3.0 License", _("Creative Commons Attribution Non-commercial 3.0 License")],
-                                  ["Creative Commons Attribution Non-commercial Share Alike 3.0 License", _("Creative Commons Attribution Non-commercial Share Alike 3.0 License")],
-                                  ["Creative Commons Attribution Non-commercial No Derivatives 3.0 License", _("Creative Commons Attribution Non-commercial No Derivatives 3.0 License")],
-                                  ["Creative Commons Attribution 2.5 License", _("Creative Commons Attribution 2.5 License")],
-                                  ["Creative Commons Attribution-ShareAlike 2.5 License", _("Creative Commons Attribution-ShareAlike 2.5 License")],
-                                  ["Creative Commons Attribution-NoDerivs 2.5 License", _("Creative Commons Attribution-NoDerivs 2.5 License")],
-                                  ["Creative Commons Attribution-NonCommercial 2.5 License", _("Creative Commons Attribution-NonCommercial 2.5 License")],
-                                  ["Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License", _("Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License")],
-                                  ["Creative Commons Attribution-NonCommercial-NoDerivs 2.5 License", _("Creative Commons Attribution-NonCommercial-NoDerivs 2.5 License")],
-                                  ["Developing Nations 2.0", _("Developing Nations 2.0")]
-                            ],
-                            tooltip: _('Select a license.'),
-                            anchor: '100%'
-                        },                        
-                        {
                             xtype: 'textarea',
                             inputId: 'pp_footer',
                             fieldLabel: _('Footer'),
                             tooltip: _('Web pages footer.'),
-                            height: 80,
-                            anchor: '100%'
-                        },
-                        {
-                            xtype: 'textarea',
-                            inputId: 'pp_description',
-                            fieldLabel: _('Description'),
-                            tooltip: _('An account of the content of the resource.'),
                             height: 80,
                             anchor: '100%'
                         }
