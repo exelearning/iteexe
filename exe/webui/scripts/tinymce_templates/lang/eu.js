e6f8ee9e9181c3917686dc23f1cf3869f2570e72
ignaciogros <ignacio@gros.es>
2013-07-05 09:26:11 +0200

    Traducción al euskera. / Translation: EU.

--- exe/webui/scripts/tinymce_templates/lang/eu.js
+++ exe/webui/scripts/tinymce_templates/lang/eu.js
@@ -1,6 +1,6 @@
 var tinyMCETemplateList = [
-	["2 columnas 50% 50%", "/scripts/tinymce_templates/2-50-50.html"],
-	["2 columnas 30% 70%", "/scripts/tinymce_templates/2-30-70.html"],
-	["2 columnas 70% 30%", "/scripts/tinymce_templates/2-70-30.html"],
-	["3 columnas", "/scripts/tinymce_templates/3.html"]
+	["2 zutabe 50% 50%", "/scripts/tinymce_templates/2-50-50.html"],
+	["2 zutabe 30% 70%", "/scripts/tinymce_templates/2-30-70.html"],
+	["2 zutabe 70% 30%", "/scripts/tinymce_templates/2-70-30.html"],
+	["3 zutabe", "/scripts/tinymce_templates/3.html"]
 ];
