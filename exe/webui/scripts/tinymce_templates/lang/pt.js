0a54d0f02d8e31351d0fe8128f4b553e4046fff8
ignaciogros <ignacio@gros.es>
2013-07-18 10:24:27 +0200

    Traducción al portugués (novedades plugins TinyMCE). / TinyMCE plugins: PT translation of the last changes.

--- exe/webui/scripts/tinymce_templates/lang/pt.js
+++ exe/webui/scripts/tinymce_templates/lang/pt.js
@@ -1,6 +1,6 @@
 var tinyMCETemplateList = [
-	["2 columnas 50% 50%", "/scripts/tinymce_templates/2-50-50.html"],
-	["2 columnas 30% 70%", "/scripts/tinymce_templates/2-30-70.html"],
-	["2 columnas 70% 30%", "/scripts/tinymce_templates/2-70-30.html"],
-	["3 columnas", "/scripts/tinymce_templates/3.html"]
+	["2 colunas 50% 50%", "/scripts/tinymce_templates/2-50-50.html"],
+	["2 colunas 30% 70%", "/scripts/tinymce_templates/2-30-70.html"],
+	["2 colunas 70% 30%", "/scripts/tinymce_templates/2-70-30.html"],
+	["3 colunas", "/scripts/tinymce_templates/3.html"]
 ];
