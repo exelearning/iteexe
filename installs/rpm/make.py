053e53ddf7186f00d1b0ff0c69f2d93e1affa52f
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2013-10-16 12:10:32 +0200

    Optimize build of RPM

--- installs/rpm/make.py
+++ installs/rpm/make.py
@@ -8,28 +8,22 @@ import glob
 import subprocess
 
 # TOPDIR root of RPM build tree typically /usr/src/redhat or /home/xxx/.rpm
-#TOPDIR = '/usr/src/redhat'
 TOPDIR = os.path.join(os.environ['HOME'], '.rpm')
-
-# where the SVN exe source directory is
-#SRCDIR = '/usr/local/src'
-SRCDIR = os.path.abspath('../../..')
 
 # this is done in a way consistent with the other builds...
 #   even though we have the info without doing the import
-os.chdir(os.path.join(SRCDIR, 'exe'))
+os.chdir('../..')
 
-sys.path.insert(0, os.path.join(SRCDIR, 'exe'))
+sys.path.insert(0, '.')
 from exe.engine import version
 
 clrelease = 1
 print "Making version: %s release: %s" % (version.version, clrelease)
 
 # create the source tarball
-os.chdir(SRCDIR)
-tarball = os.path.join(TOPDIR, 'SOURCES', 'exe-' + version.release + '-source.tgz')
+tarball = os.path.join(TOPDIR, 'SOURCES', 'intef-exe-' + version.release + '-source.tgz')
 try:
-    ret = subprocess.call('tar -czf %s --wildcards-match-slash --exclude="*.svn*" --exclude "*.pyc" --exclude="*.tmp" --exclude="*~" --exclude="dist/*" --exclude="build/*" --exclude="pyxpcom/*" exe' %
+    ret = subprocess.call('tar -czf %s --wildcards-match-slash --exclude=".git" --exclude="*.svn*" --exclude "*.pyc" --exclude="*.tmp" --exclude="*~" --exclude="dist/*" --exclude="build/*" --exclude="pyxpcom/*" .' %
                               tarball, shell=True)
     if ret < 0:
         print >>sys.stderr, "Unable to make tarball signal", -ret
