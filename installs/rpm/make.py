f2c3167c2586e4d4a555f17b792cde9da4c5f86a
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2013-10-17 12:19:34 +0200

    Move produced binary's to make directory in Linux builds

--- installs/rpm/make.py
+++ installs/rpm/make.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 
-# another thinly disguised shell script written in Python
-
 import sys
 import os
-import glob
 import subprocess
 
-# TOPDIR root of RPM build tree typically /usr/src/redhat or /home/xxx/.rpm
-TOPDIR = os.path.join(os.environ['HOME'], '.rpm')
-
-# this is done in a way consistent with the other builds...
-#   even though we have the info without doing the import
 os.chdir('../..')
 
 sys.path.insert(0, '.')
@@ -22,7 +14,7 @@ print "Making version: %s release: %s" % (version.version, clrelease)
 
 # create the source tarball
 os.chdir('..')
-tarball = os.path.join(TOPDIR, 'SOURCES', 'exe-' + version.release + '-source.tgz')
+tarball = 'intef-exe-' + version.release + '-source.tgz'
 try:
     ret = subprocess.call('tar -czf %s -h --wildcards-match-slash --exclude=".git" --exclude="*.svn*" --exclude "*.pyc" --exclude="*.tmp" --exclude="*~" --exclude="dist/*" --exclude="build/*" --exclude="pyxpcom/*" exe' % tarball, shell=True)
     if ret < 0:
@@ -39,3 +31,4 @@ try:
         sys.exit(ret)
 except OSError, e:
     print >>sys.stderr, "Execution of rpmbuild failed:", e
+subprocess.check_call('mv %s $HOME/rpmbuild/RPMS/i686/intef-exe-%s-%s.*.rpm installs/rpm' % (tarball, version.release, clrelease), shell=True)
