362accfa2eee3e5aa7c23b7a90f3b1eff7978c80
Mike Dawson <mike@toughra.com>
2013-10-30 16:10:02 +0400

    Updated HTML5 Media options:  Ensured autoplay is not enabled by default  Moved extra autoplay and controls option into one line with use javascript player to avoid  scrollbars appearing  Ensured new options were in en_dlg.js for localization

--- exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/media/langs/en_dlg.js
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/media/langs/en_dlg.js
@@ -32,6 +32,8 @@ use_mediaelement : _("Use JavaScript player"),
 "position_right":_("Right"),
 "image_and_caption_margin":_("Group Margin"),
 "caption_explanation":_("You will not be able to edit the header or the caption from this dialog."),
-"caption_warning":_("The header and the caption can't be edited from this dialog. You can change or remove them editing the text itself.")
+"caption_warning":_("The header and the caption can't be edited from this dialog. You can change or remove them editing the text itself."),
+"use_autoplay": _("Use Autoplay"),
+"use_controls": _("Use Controls")
 // The New eXeLearning
 }); 
