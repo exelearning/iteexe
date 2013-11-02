75d21a077eaa0ebd28eae698da8ed80873c09776
Juan Rafael Fernández <jrfern@gmail.com>
2013-11-03 00:05:28 +0100

    A new string. The Spanish translation is complete again.

--- exe/jsui/scripts/i18n/es.js
+++ exe/jsui/scripts/i18n/es.js
@@ -1,5 +1,5 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.0\nReport-Msgid-Bugs-To: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nPOT-Creation-Date: 2013-11-02 23:59+0100\nPO-Revision-Date: 2013-11-02 19:44+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <juanrafael.fernandez@hispalinux.es>\nLanguage-Team: Spanish <jrfern@gmail.com>\nLanguage: es\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n", 
+ "": "Project-Id-Version: eXeLearning 2.0\nReport-Msgid-Bugs-To: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nPOT-Creation-Date: 2013-11-02 23:59+0100\nPO-Revision-Date: 2013-11-03 00:04+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <juanrafael.fernandez@hispalinux.es>\nLanguage-Team: Spanish <jrfern@gmail.com>\nLanguage: es\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n", 
  "DropDown Activity": "Actividad desplegable", 
  "Create the case story. A good case is one \nthat describes a controversy or sets the scene by describing the characters \ninvolved and the situation. It should also allow for some action to be taken \nin order to gain resolution of the situation.": "Cree el caso pr\u00e1ctico. Un buen caso es aquel \nque describe una controversia o propone una escena describiendo los personajes \ninvolucrados y la situaci\u00f3n. Tambi\u00e9n debe permitir que se realicen acciones \npara obtener la resoluci\u00f3n de la situaci\u00f3n.", 
  "Xhosa ": "Xhosa ", 
@@ -1240,6 +1240,7 @@ translations = {
  "Macedonian ": "Macedonio ", 
  "Insert 'return false'": "Insertar 'return false'", 
  "Memory Match Pair Field": "Campo de las parejas del emparejamiento de memoria", 
+ "Show Advanced Options": "Mostrar las opciones avanzadas", 
  "Sortable list of items.": "Lista ordenable de objetos.", 
  "Basque ": "Vasco, euskera ", 
  "Add Word": "A\u00f1adir nueva palabra", 
