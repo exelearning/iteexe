6b74a74cdb8acf828fa79db07bf7c1e7ea535e4e
Juan Rafael Fernández <jrfern@gmail.com>
2013-11-05 23:32:08 +0100

    Rerun mki18n.sh to create correctly spelt msgids.

--- exe/jsui/scripts/i18n/ast.js
+++ exe/jsui/scripts/i18n/ast.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXe Learning 1.04.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2013-11-05 20:16+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: ast <LL@li.org>\nLanguage: ast\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
+ "": "Project-Id-Version: eXe Learning 1.04.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2013-11-05 23:31+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: ast <LL@li.org>\nLanguage: ast\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
 };
--- exe/jsui/scripts/i18n/hr.js
+++ exe/jsui/scripts/i18n/hr.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXe Learning\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2013-11-05 20:16+0100\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage-Team: EXE Team <exe@exelearning.org>\nLanguage: hr\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n", 
+ "": "Project-Id-Version: eXe Learning\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2013-11-05 23:31+0100\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage-Team: EXE Team <exe@exelearning.org>\nLanguage: hr\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n", 
  "?????": "?????"
 };
