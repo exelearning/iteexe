123311ac8e8222a670657d58f15dde0ebdeea48a
Juan Rafael Fernández <jrfern@gmail.com>
2013-11-10 19:18:37 +0100

    Completed the list.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,13 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXe Learning\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2013-11-05 23:31+0100\nPO-Revision-Date: 2011-10-10 16:32+0100\nLast-Translator: Jos\u00e9 Ram\u00f3n Jim\u00e9nez Reyes <jrjimenezr@gmail.com>\nLanguage-Team: EXE Team <exe@exelearning.org>\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n", 
- "Incorrect": "Incorrect selection.", 
- "False": "False.", 
- "?????": "?????", 
- "Correct!": "Correct.", 
- "Correct Option": "Correct.", 
- "Incorrect!": "Incorrect.", 
- "Wrong": "Incorrect.", 
- "Autoevaluacion": "Now it's your turn", 
- "True": "True.", 
- "Correct": "Correct selection."
+ "": "Project-Id-Version: eXeLearning 2.0\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2013-11-05 23:31+0100\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage-Team: EXE Team <jrfern@gmail.com>\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
 };
