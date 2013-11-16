9347c5514e1ae9d7b9f2380a4428ccba0a63066c
ignaciogros <ignacio@gros.es>
2013-11-16 17:39:17 +0100

    Bug #1948 (https://forja.cenatic.es/tracker/?func=detail&atid=883&aid=1948&group_id=197). We set #main-wrapper's width to avoid problems with iframes and wide contents. + We make it easier to maintain (we use CSS classes instead of changing the width of each element with JavaScript).

--- exe/webui/style/INTEF/my_js.js
+++ exe/webui/style/INTEF/my_js.js
@@ -17,47 +17,22 @@ var myTheme = {
     },
     hideMenu : function(){
         $("#siteNav").hide();
-        var p = "20px"; //Padding
-        if ($(window).width()<700) {
-            p = "10px";
-        }          
-        $("#main").css({"width":"auto","padding-left":p,"float":"none"});
-		$(document.body).addClass("no-nav");
+        $(document.body).addClass("no-nav");
         myTheme.params("add");
         $("#toggle-nav").attr("class","show-nav").attr("title",$exe_i18n.show);
     },
     toggleMenu : function(){
-        var c = $("#main");
         var l = $("#toggle-nav");
         if (l.attr("class")=='hide-nav') {       
             l.attr("class","show-nav").attr("title",$exe_i18n.show);
-			$(document.body).addClass("no-nav");
-            $("#siteFooter").hide();
+			$("#siteFooter").hide();
 			$("#siteNav").slideUp(400,function(){
-                var p = "20px"; //Padding
-                if ($(window).width()<700) {
-                    p = "10px";
-                }                
-                $("#main").css({"width":"auto","padding-left":p,"float":"none"});
-				$("#siteFooter").show();
+                $(document.body).addClass("no-nav");
+                $("#siteFooter").show();
             }); 
             myTheme.params("add");
         } else {
             l.attr("class","hide-nav").attr("title",$exe_i18n.hide);
-            var w = "715px"; //Width
-            var f = "right"; //Float
-            var p = "0"; //Padding
-            var ww = $(window).width();
-            if(ww>700 && ww<1015) {
-                w = "auto";
-                f = "none";
-                p = "20px";         
-            } else if (ww<700) {
-                w = "auto";
-                f = "none";
-                p = "10px";
-            }
-            $("#main").css({"width":w,"padding-left":p,"float":f});
             $(document.body).removeClass("no-nav");
 			$("#siteNav").slideDown();
             myTheme.params("delete");            
