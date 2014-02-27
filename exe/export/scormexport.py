0c6a9e0d43a1166b46231928ee99d31f626065de
jmandonegi <jm.andonegi@gmail.com>
2014-02-27 23:09:24 +0100

    Just change header: #!/usr/bin/env python #-*- coding: utf-8 -*-

--- exe/export/scormexport.py
+++ exe/export/scormexport.py
@@ -1,4 +1,5 @@
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 # ===========================================================================
 # eXe 
 # Copyright 2004-2005, University of Auckland
