8db0053064cfe5aedd2c62dc2efba93f388b2196
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2014-06-04 16:21:12 +0200

    Restore previous language names of preferences page

--- exe/webui/preferencespage.py
+++ exe/webui/preferencespage.py
@@ -35,53 +35,59 @@ import os.path
 log = logging.getLogger(__name__)
 
 langNames = {
-    'fi': 'Finnish',
-    'hr': 'Croatian',
-    'ig': "Nunivak Cup'ig",
-    'lo': 'Lao; Laotian',
-    'pt_br': 'Brazillian Portuguese',
-    'ru': 'Russian',
-    'tg': 'Tajik',
-    'yo': 'Yoruba',
-    'el': '\xce\x95\xce\xbb\xce\xbb\xce\xb7\xce\xbd\xce\xb9\xce\xba\xce\xac',
-    'en': 'English',
-    'zh': '\xe7\xae\x80\xe4\xbd\x93\xe4\xb8\xad\xe6\x96\x87',
-    'ee': 'Ewe',
-    'vi': 'Vietnamese',
-    'is': '\xc3\x8dslenska',
-    'it': 'Italiano',
-    'eu': 'Euskara',
-    'zu': 'isiZulu',
-    'cs': '\xc4\x8cesky',
-    'et': 'Eesti',
-    'gl': 'Galego',
-    'id': 'Indonesian',
-    'es': 'Espa\xc3\xb1ol',
-    'nl': 'Nederlands',
-    'pt': 'Portugu\xc3\xaas',
-    'tw': 'Twi',
-    'nb': 'Bokm\xc3\xa5l',
-    'tr': 'Turkish',
-    'tl': 'Tagalog',
-    'th': '\xe0\xb8\xa0\xe0\xb8\xb2\xe0\xb8\xa9\xe0\xb8\xb2\xe0\xb9\x84\xe0\xb8\x97\xe0\xb8\xa2',
-    'ca': 'Catal\xc3\xa0',
-    'ca_VALENCIA': 'Valenci\xc3\xa0',
-    'pl': 'J\xc4\x99zyk Polski, polszczyzna',
-    'fr': 'Fran\xc3\xa7ais',
-    'bg': 'Bulgarian',
-    'zh_tw': '\xe7\xae\x80\xe4\xbd\x93\xe4\xb8\xad\xe6\x96\x87',
-    'da': 'Dansk',
-    'br': 'Breton',
-    'hu': 'Magyar',
-    'ja': '\xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e',
-    'sr': 'Srpski',
-    'mi': 'M\xc4\x81ori',
-    'sv': 'Svenska',
-    'km': 'Khmer',
-    'sk': 'Sloven\xc4\x8dina, Slovensk\xc3\xbd Jazyk',
-    'de': 'Deutsch',
-    'uk': 'Ukrainian',
-    'sl': 'Sloven\xc5\xa1\xc4\x8dina'
+   'ar': 'العربية',                    # Arabic
+   'ast': 'asturianu',
+   'bg': 'Български',                  # Bulgarian
+   'br': 'Brezhoneg',                  # Breton
+   'ca': 'Català',                     # Catal\xc3\xa0, Catalonian
+   # 'ca_VALENCIA': 'Valencià',        # Valenci\xc3\xa0
+   'ca_ES@valencia': 'Valencià',       # Valenci\xc3\xa0, Valencian Catalonian
+   'cs': 'Čeština, český jazyk',       # \xc4\x8cesky, Czech
+   'da': 'Dansk',
+   'de': 'Deutsch',                    # German
+   'ee': 'Eʋegbe',                     # Ewe
+   'el': 'Ελληνικά',                   # \xce\x95\xce\xbb\xce\xbb\xce\xb7\xce\xbd\xce\xb9\xce\xba\xce\xac, Greek
+   'en': 'English',
+   'es': 'Español',                    # Espa\xc3\xb1ol, Spanish
+   'et': 'Eesti',                      # Estonian
+   'eu': 'Euskara',                    # Basque
+   'fa': 'فارسی',                      # Farsi, Persian
+   'fi': 'Suomi',                      # Finnish
+   'fr': 'Français',                   # Fran\xc3\xa7ais, French
+   'gl': 'Galego',                     # Galician
+   'hr': 'Hrvatski',                   # Croatian
+   'hu': 'Magyar',                     # Hungarian
+   'id': 'Bahasa Indonesia',           # Indonesian
+   'ig': 'Asụsụ Igbo',                 # Nunivak Cup'ig, Igbo
+   'is': 'Íslenska',                   # \xc3\x8dslenska, Icelandic
+   'it': 'Italiano',
+   'ja': '日本語',                      # \xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e', Japanese
+   'km': 'ភាសាខ្មែរ',                        # Khmer, Cambodyan
+   'lo': 'ພາສາລາວ',                                         # Lao, Laotian
+   'mi': 'Māori',                      # M\xc4\x81ori
+   'nb':  'Norsk bokmål',              # Bokm\xc3\xa5l, Norwegian Bokmål
+   'nl': 'Nederlands',                 # Dutch
+   'pl': 'Język polski, polszczyzna',  # J\xc4\x99zyk Polski, polszczyzna
+   'pt': 'Português',                  # Portugu\xc3\xaas
+   'pt_br': 'Português do Brazil',     # Brazillian Portuguese
+   'ru': 'Русский',                    # Russian
+   'sk': 'Slovenčina, slovenský jazyk', # Sloven\xc4\x8dina, Slovensk\xc3\xbd Jazyk - Slovak
+   'sl': 'Slovenščina',                # Sloven\xc5\xa1\xc4\x8dina - Slovene
+   'sr': 'Српски / srpski',            # Srpski, serbio
+   'sv': 'Svenska',                    # Swedish
+   'th':  'ไทย',                                                            #  \xe0\xb8\xa0\xe0\xb8\xb2\xe0\xb8\xa9\xe0\xb8\xb2\xe0\xb9\x84\xe0\xb8\x97\xe0\xb8\xa2 - Thai
+   'tl': 'Wikang Tagalog, ᜏᜒᜃᜅ᜔ ᜆᜄᜎᜓᜄ᜔', # Tagalog
+   'tg': 'тоҷикӣ, toğikī, تاجیکی‎',    # Tajik
+   'tr': 'Türkçe',                     # Turkish
+   'tw': 'Twi',
+   'uk': 'Українська',                 # Ukrainian
+   'vi': 'Tiếng Việt',                 # Vietnamese
+   'yo': 'Yorùbá',                     # Yoruba
+   'zh': '\xe7\xae\x80\xe4\xbd\x93\xe4\xb8\xad\xe6\x96\x87',
+   # 'zh_tw': '\xe7\xae\x80\xe4\xbd\x93\xe4\xb8\xad\xe6\x96\x87',
+   'zh_CN': '简体中文',
+   'zh_TW': '正體中文（台灣)',
+   'zu': 'isiZulu'
 }
 
 browsersHidden = ('xdg-open', 'gvfs-open', 'x-www-browser', 'gnome-open', 'kfmclient', 'www-browser', 'links', 
