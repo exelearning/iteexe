8c5205d0213bee9f9bfdd9112f1b5cfa2232e9ce
ignaciogros <ignacio@gros.es>
2014-10-02 13:13:00 +0200

    New TinyMCE Plugin to add a quote with a cite.

---
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/blockquoteandcite/editor_plugin_src.js
@@ -0,0 +1,48 @@
+// Ignacio Gros (http://www.gros.es) for eXeLearning.net
+// Based in OtherXtras by José Ramón Jiménez Reyes para eXeLearning.net
+(function() {
+	tinymce.PluginManager.requireLangPack('blockquoteandcite');
+	tinymce.create('tinymce.plugins.BlockQuoteAndCite', {
+		init : function(ed, url) {
+
+			ed.addCommand('mceBlockQuoteAndCite', function() {
+				ed.windowManager.open({
+					file : url + '/blockquoteandcite.htm',
+					width : 450,
+					height : 270,
+					inline : 1
+				});
+			});
+			
+			ed.onInit.add(function() {
+				if (ed.settings.content_css !== false) ed.dom.loadCSS(url + "/css/content.css");
+			});			
+
+			ed.addButton('blockquoteandcite', {title : 'blockquoteandcite.title', cmd : 'mceBlockQuoteAndCite', image : url + '/img/blockquoteandcite.png' });
+            
+			ed.onNodeChange.add(function(ed, cm, n, co) {
+				n = ed.dom.getParent(n,'BLOCKQUOTE');
+
+				cm.setActive('blockquoteandcite', co);
+				cm.setActive('blockquoteandcite', 0);
+                if (n && n.nodeName=="BLOCKQUOTE") {
+                    cm.setActive('blockquoteandcite', true);
+                }
+			});
+            
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'Cite (based in Other Xtras Plugin)',
+				author : 'José Ramón Jiménez Reyes (modified by Ignacio Gros)',
+				authorurl : '',
+				infourl : '',
+				version : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('blockquoteandcite', tinymce.plugins.BlockQuoteAndCite);
+})();
---
+++ exe/webui/scripts/tinymce_3.5.7/jscripts/tiny_mce/plugins/blockquoteandcite/js/blockquoteandcite.js
@@ -0,0 +1,107 @@
+function initCommonAttributes(elm) {
+	var dom = tinyMCEPopup.editor.dom;
+	if (elm.nodeName == 'BLOCKQUOTE') {
+		var childs = elm.childNodes;
+		var qlang = dom.getAttrib(elm, 'lang');
+		var alang = "";
+		if (childs.length == 2) {
+			var quote = childs[0].innerHTML;
+			var author = childs[1].innerHTML;
+			alang = dom.getAttrib(childs[1], 'lang');
+		} else if (childs.length == 1) {
+			var quote = childs[0].innerHTML;
+			var author = "";
+		}
+		setFormValue('blockquote', quote);
+		setFormValue('blockquoteLang', qlang);
+		setFormValue('author', author);
+		setFormValue('authorLang', alang);
+	}
+}
+
+function setFormValue(n,v) {
+	var e = document.forms[0].elements[n];
+	if (e) e.value = v;
+}
+
+function init() {
+	
+	var elm = tinyMCEPopup.editor.dom.getParent(tinyMCEPopup.editor.selection.getNode(), 'blockquote');
+	var s = tinyMCE.activeEditor.selection.getContent();
+	
+	if (s.length > 0 && elm == null) {
+		setFormValue('blockquote', s);
+	}
+	
+	var action = "insert";
+	
+	if (elm!=null && elm.nodeName=='BLOCKQUOTE') {
+		action = "update";
+		initCommonAttributes(elm);
+		document.getElementById("remove").style.display = 'block';
+	}
+
+	document.forms[0].insert.value = tinyMCEPopup.getLang(action, 'Insert', true);
+	
+}
+
+function insertQuote() {
+    
+	var elm = tinyMCEPopup.editor.dom.getParent(tinyMCEPopup.editor.selection.getNode(), 'blockquote');
+	var formObj = document.forms[0];
+	var quote = formObj.elements['blockquote'];	
+	
+	if (quote.value == "") {
+		tinyMCEPopup.alert(tinyMCEPopup.getLang('blockquoteandcite.no_quote'));
+		return;
+	}
+	
+	var author = document.forms[0].author;
+	
+	if (author.value == "") {
+		tinyMCEPopup.alert(tinyMCEPopup.getLang('blockquoteandcite.no_author'));
+		return;
+	}
+
+	tinyMCEPopup.execCommand('mceBeginUndoLevel');
+	
+	var q = quote.value;
+	var a = author.value;
+	var qL = formObj.elements['blockquoteLang'].value;
+	var aL = formObj.elements['authorLang'].value;
+	
+	var h = '<blockquote class="exe-quote-cite"';
+	if (qL != "") h += ' lang="' + qL + '"';
+	h += '><p>' + q + '</p>';
+	if (a != "") {
+		h = h + '<cite';
+		if (aL != "") h = h + ' lang="' + aL + '"';
+		h = h + '>' + a + '</cite>';
+	}
+	h += '</blockquote>';
+	
+	if (elm == null) {
+		h += "&nbsp;";
+		tinyMCEPopup.execCommand('mceInsertRawHTML', false, h);
+	} else {
+		tinyMCEPopup.editor.dom.remove(elm);
+		tinyMCE.execCommand('mceInsertContent', false, h);
+	}
+	
+	tinyMCEPopup.editor.nodeChanged();
+	tinyMCEPopup.execCommand('mceEndUndoLevel');
+	tinyMCEPopup.close();
+}
+
+function removeQuote() {
+	var elm = tinyMCEPopup.editor.dom.getParent(tinyMCEPopup.editor.selection.getNode(), 'blockquote');
+	if (elm){
+		tinyMCEPopup.execCommand('mceBeginUndoLevel');
+		tinyMCEPopup.editor.dom.remove(elm);
+		tinyMCEPopup.editor.nodeChanged();
+		tinyMCEPopup.execCommand('mceEndUndoLevel');
+	}
+	tinyMCEPopup.close();
+}
+
+tinyMCEPopup.onInit.add(init);
