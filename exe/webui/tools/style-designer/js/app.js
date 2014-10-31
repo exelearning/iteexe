7b484c634dfd6de6df87b3b2958281f98ac6c6f0
ignaciogros <ignacio@gros.es>
2014-10-31 09:31:52 +0100

    Style Designer (no functionality).

---
+++ exe/webui/tools/style-designer/js/app.js
@@ -0,0 +1,93 @@
+﻿/*
+ * eXeLearning Style Designer 1.0
+ * By Ignacio Gros (http://www.gros.es/) for eXeLearning (http://exelearning.net/)
+ * Creative Commons Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/3.0/)
+ */
+var $app = {
+	init : function() {
+		
+		// Is it IE<9?
+		this.isOldBrowser = false;
+		var ie = this.checkIE;
+		if (ie && ie<9) this.isOldBrowser = true;
+
+		$("#save").click(function(){
+			alert("Save");
+			// var css = $app.composeCSS();
+		});
+		
+		this.i18n();
+		this.enableColorPicker();
+		
+	},
+	template : function(templateid,data){
+		return document.getElementById(templateid).innerHTML.replace(/%(\w*)%/g,
+		function(m,key){
+			return data.hasOwnProperty(key)?data[key]:"";
+		});
+	},
+	i18n : function(){
+		document.title = $i18n.Style_Designer;
+		document.getElementById("cssWizard").innerHTML=this.template("cssWizard",$i18n);
+	},
+	showTab : function(id) {
+		$("li","#tabs").attr("class","");
+		$("#"+id+"-tab").attr("class","current");
+		$("div.panel").removeClass("current");
+		$("#"+id).addClass("current");
+	},
+	checkIE : function(){
+		var n = navigator.userAgent.toLowerCase();
+		return (n.indexOf('msie') != -1) ? parseInt(n.split('msie')[1]) : false;
+	},
+	getRadioButtonsValue : function(radioObj) {
+		if(!radioObj) return "";
+		var radioLength = radioObj.length;
+		if(radioLength == undefined)
+			if(radioObj.checked) return radioObj.value;
+			else return "";
+		for(var i = 0; i < radioLength; i++) {
+			if(radioObj[i].checked) return radioObj[i].value;
+		}
+		return "";
+	},
+	formatCSS : function(css) {
+		css=css.replace(/{/g, "{\n");
+		css=css.replace(/}/g, "}\n\n");
+		css=css.replace(/;/g, ";\n");
+		return css;	
+	},
+	composeCSS : function(){
+		alert("composeCSS");
+		var css = "";
+		return formatCSS(css)
+	},
+	enableColorPicker : function(){
+		$.fn.jPicker.defaults.images.clientPath='images/jpicker/';	
+		$('.color').jPicker(
+			{
+				window:{
+					title: $i18n.Color_Picker,
+					position:{
+						x: 'top',
+						y: 'left'
+					},
+					effects:{
+						type:'show',
+						speed:{
+							show : 0,
+							hide : 0
+						}
+					}
+				},
+				localization : $i18n.Color_Picker_Strings
+			},
+			function(color, context){
+				//alert("Preview");
+			}
+		);
+	}
+}
+$(function(){
+	$app.init();
+});
--- exe/webui/tools/style-designer/js/style-designer.js
+++ exe/webui/tools/style-designer/js/style-designer.js
@@ -1,3 +1,8 @@
+/*
+ * eXeLearning Style Designer 1.0
+ * By Ignacio Gros (http://www.gros.es/) for eXeLearning (http://exelearning.net/)
+ * Creative Commons Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/3.0/)
+ */
 var $designer = {
 	init : function(){	
 		var sd_style = "base";
@@ -14,11 +19,33 @@ var $designer = {
 	printExtraBody : function(){
 		this.disableAllLinks();
 		document.write('<script type="text/javascript" src="/style/'+this.styleId+'/_my_js.js"></script>');
+		this.openDesigner();
 	},
 	disableAllLinks : function(){
 		$("A","#content").click(function(){
 			return false;
 		});
+	},
+	openDesigner : function(){
+		// Settings
+		var mypage="/tools/style-designer/";
+		var myname="eXeLearning_Style_Designer";
+		var w=650;
+		var h=500;
+		var features="";
+		// / Settings
+		var win = null;
+		var winl = (screen.width-w)/2;
+		var wint = (screen.height-h)/2;
+		if (winl < 0) winl = 0;
+		if (wint < 0) wint = 0;
+		var settings = 'height=' + h + ',';
+		settings += 'width=' + w + ',';
+		settings += 'top=' + wint + ',';
+		settings += 'left=' + winl + ',';
+		settings += features;
+		win = window.open(mypage,myname,settings);
+		if (win) win.focus();
 	}
 }
 $designer.init();
