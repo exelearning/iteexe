cb27c07f8137766e1f6370884f841944976315a9
Juan Rafael Fernández <jrfern@gmail.com>
2015-02-26 13:30:54 +0100

    Updated Spanish translation.

--- exe/jsui/scripts/i18n/ast.js
+++ exe/jsui/scripts/i18n/ast.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.0.2\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2015-02-26 13:11+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: ast <LL@li.org>\nLanguage: ast\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
+ "": "Project-Id-Version: eXeLearning 2.0.2\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2015-02-26 13:24+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: ast <LL@li.org>\nLanguage: ast\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
 };
