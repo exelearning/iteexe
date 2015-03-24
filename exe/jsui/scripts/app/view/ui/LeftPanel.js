03cba30401a5a02041ad53548f74355d2a9aeea4
Mike Dawson <mike@toughra.com>
2015-03-24 12:08:43 +0400

    Re-arrange LeftPanel.js to make showing/hiding sections tidier

--- exe/jsui/scripts/app/view/ui/LeftPanel.js
+++ exe/jsui/scripts/app/view/ui/LeftPanel.js
@@ -32,156 +32,160 @@ Ext.define('eXe.view.ui.LeftPanel', {
 
     initComponent: function() {
         var me = this;
-
-        Ext.applyIf(me, {
+        var leftOutlinePanel = {
+            xtype: 'panel',
+            layout: {
+                type: 'border'
+            },
+            region: 'center',
             items: [
                 {
-                    xtype: 'panel',
-                    layout: {
-                        type: 'border'
-                    },
-                    region: 'center',
-                    items: [
-                        {
-                            xtype: 'outlinetoolbar1',
-                            region: 'north'
-                        },
-                        {
-                            xtype: 'treepanel',
-                            itemId: 'outline_treepanel',
-                            autoScroll: true,
-                            title: _('Outline'),
-                            store: 'OutlineXmlTreeStore',
-                            rootVisible: false,
-                            region: 'center',
-                            animate: false,
-                            viewConfig: {
-
-                            }
-                        },
-                        {
-                            xtype: 'outlinetoolbar2',
-                            region: 'south'
-                        }
-                    ]
+                    xtype: 'outlinetoolbar1',
+                    region: 'north'
                 },
                 {
-                    xtype: 'gridpanel',
-                    itemId: 'idevice_panel',
-                    height: 150,
+                    xtype: 'treepanel',
+                    itemId: 'outline_treepanel',
                     autoScroll: true,
-                    store: 'IdeviceXmlStore',
-                    flex: 1,
-                    plugins: [{ptype: 'cellediting', clicksToEdit: 1}],
-                    features: [{
-                       ftype: 'grouping',
-                	   startCollapsed: true,
-                	   groupHeaderTpl: '{name}',
-                	   
-                    }],
-             	    tbar: [
-             	        {
-             	        	xtype: 'button',
-             	        	text: _('Ungroup iDevices'),
-             	        	handler: function(button) {
-             	        		var panel = button.up("#idevice_panel");
-             	        		if (button.getText() == _('Ungroup iDevices')) {
-             	        			panel.view.features[0].disable();
-             	        			button.setText(_('Group iDevices'));
-             	        		}
-             	        		else {
-             	        			panel.view.features[0].enable();
-             	        			button.setText(_('Ungroup iDevices'));
-             	        		}
-             	        	}
-             	        },
-             	        {
-             	        	xtype: 'button',
-             	        	text: _('Edit iDevices'),
-             	        	handler: function(button) {
-             	        		var panel = button.up("#idevice_panel"),
-	             	        		leftpanel = panel.up("leftpanel"),
-	          	        			gbutton = button.prev("button");
-	          	        		if (button.getText() == _('Edit iDevices')) {
-             	        			button.leftPanelSize = leftpanel.getWidth();
-             	        			if (button.leftPanelSize < 350)
-             	        				leftpanel.setWidth(350);
-             	        			panel.editing = true;
-             	        			panel.columns[1].show();
-             	        			panel.columns[2].show();
-             	        			panel.store.clearFilter();
-             	        			button.setText(_('Save iDevices'));
-             	        			panel.view.features[0].disable();
-             	        			gbutton.disable();
-             	        		}
-             	        		else {
-             	        			var restore = function() {
-	         	        				panel.editing = false;
-	         	        				panel.columns[1].hide();
-	         	        				panel.columns[2].hide();
-	         	        				leftpanel.setWidth(button.leftPanelSize);
-	         	        				button.setText(_('Edit iDevices'));
-	         	        				gbutton.enable();
-	         	        				if (gbutton.getText() == _('Ungroup iDevices'))
-	         	        					panel.view.features[0].enable();
-	         	        				panel.store.filter('visible', true);
-             	        			};
-             	        			if (panel.store.getModifiedRecords().length) {
-             	        				panel.store.sync({
-             	        					callback: restore
-             	        				});
-             	        			}
-             	        			else {
-             	        				restore();
-             	        			}
-             	        		}
-             	        	}
-             	        }
-             	    ],
-					selModel: {
-					    selType: 'cellmodel'
-					},
-                    region: 'south',
-                    split: true,
-                    columns: [
-                        {
-                            xtype: 'gridcolumn',
-                            sortable: false,
-                            dataIndex: 'label',
-                            fixed: true,
-                            flex: 1,
-                            hideable: false,
-                            menuDisabled: true,
-                            text: _('iDevices')
-                        },
-                        {
-                            xtype: 'gridcolumn',
-                            hidden: true,
-                            sortable: false,
-                            dataIndex: 'category',
-                            fixed: true,
-                            flex: 1,
-                            hideable: false,
-                            menuDisabled: true,
-                            text: _('Category')
-                        },
-                        {
-                            xtype: 'checkcolumn',
-                            width: 50,
-                            hidden: true,
-                            sortable: false,
-                            dataIndex: 'visible',
-                            fixed: true,
-                            flex: 0,
-                            hideable: false,
-                            menuDisabled: true,
-                            text: _('Visible'),
-                            editor: {
-                                xtype: 'checkbox'
-                            }
-                        }
-                    ]
+                    title: _('Outline'),
+                    store: 'OutlineXmlTreeStore',
+                    rootVisible: false,
+                    region: 'center',
+                    animate: false,
+                    viewConfig: {
+
+                    }
+                },
+                {
+                    xtype: 'outlinetoolbar2',
+                    region: 'south'
                 }
+            ]
+        };
+        
+        var leftIdevicePanel = {
+            xtype: 'gridpanel',
+            itemId: 'idevice_panel',
+            height: 150,
+            autoScroll: true,
+            store: 'IdeviceXmlStore',
+            flex: 1,
+            plugins: [{ptype: 'cellediting', clicksToEdit: 1}],
+            features: [{
+               ftype: 'grouping',
+        	   startCollapsed: true,
+        	   groupHeaderTpl: '{name}',
+        	   
+            }],
+     	    tbar: [
+     	        {
+     	        	xtype: 'button',
+     	        	text: _('Ungroup iDevices'),
+     	        	handler: function(button) {
+     	        		var panel = button.up("#idevice_panel");
+     	        		if (button.getText() == _('Ungroup iDevices')) {
+     	        			panel.view.features[0].disable();
+     	        			button.setText(_('Group iDevices'));
+     	        		}
+     	        		else {
+     	        			panel.view.features[0].enable();
+     	        			button.setText(_('Ungroup iDevices'));
+     	        		}
+     	        	}
+     	        },
+     	        {
+     	        	xtype: 'button',
+     	        	text: _('Edit iDevices'),
+     	        	handler: function(button) {
+     	        		var panel = button.up("#idevice_panel"),
+         	        		leftpanel = panel.up("leftpanel"),
+      	        			gbutton = button.prev("button");
+      	        		if (button.getText() == _('Edit iDevices')) {
+     	        			button.leftPanelSize = leftpanel.getWidth();
+     	        			if (button.leftPanelSize < 350)
+     	        				leftpanel.setWidth(350);
+     	        			panel.editing = true;
+     	        			panel.columns[1].show();
+     	        			panel.columns[2].show();
+     	        			panel.store.clearFilter();
+     	        			button.setText(_('Save iDevices'));
+     	        			panel.view.features[0].disable();
+     	        			gbutton.disable();
+     	        		}
+     	        		else {
+     	        			var restore = function() {
+     	        				panel.editing = false;
+     	        				panel.columns[1].hide();
+     	        				panel.columns[2].hide();
+     	        				leftpanel.setWidth(button.leftPanelSize);
+     	        				button.setText(_('Edit iDevices'));
+     	        				gbutton.enable();
+     	        				if (gbutton.getText() == _('Ungroup iDevices'))
+     	        					panel.view.features[0].enable();
+     	        				panel.store.filter('visible', true);
+     	        			};
+     	        			if (panel.store.getModifiedRecords().length) {
+     	        				panel.store.sync({
+     	        					callback: restore
+     	        				});
+     	        			}
+     	        			else {
+     	        				restore();
+     	        			}
+     	        		}
+     	        	}
+     	        }
+     	    ],
+			selModel: {
+			    selType: 'cellmodel'
+			},
+            region: 'south',
+            split: true,
+            columns: [
+                {
+                    xtype: 'gridcolumn',
+                    sortable: false,
+                    dataIndex: 'label',
+                    fixed: true,
+                    flex: 1,
+                    hideable: false,
+                    menuDisabled: true,
+                    text: _('iDevices')
+                },
+                {
+                    xtype: 'gridcolumn',
+                    hidden: true,
+                    sortable: false,
+                    dataIndex: 'category',
+                    fixed: true,
+                    flex: 1,
+                    hideable: false,
+                    menuDisabled: true,
+                    text: _('Category')
+                },
+                {
+                    xtype: 'checkcolumn',
+                    width: 50,
+                    hidden: true,
+                    sortable: false,
+                    dataIndex: 'visible',
+                    fixed: true,
+                    flex: 0,
+                    hideable: false,
+                    menuDisabled: true,
+                    text: _('Visible'),
+                    editor: {
+                        xtype: 'checkbox'
+                    }
+                }
+            ]
+        };
+        
+        
+        Ext.applyIf(me, {
+            items: [
+                leftOutlinePanel,
+                leftIdevicePanel
             ]
         });
 
