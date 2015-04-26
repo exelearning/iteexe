d91abcaa9033141b33d3685d4f8be3b340ec71cb
Juan Rafael Fernández <jrfern@gmail.com>
2015-04-26 14:53:07 +0200

    Preparing the js part of AM and BN i18n

---
+++ exe/jsui/scripts/i18n/bn.js
@@ -0,0 +1,3 @@
+translations = {
+ "": "Project-Id-Version: eXeLearning 2.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2015-04-22 23:34+0200\nPO-Revision-Date: 2015-04-19 20:58+0200\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: bn <LL@li.org>\nLanguage: bn\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
+};
