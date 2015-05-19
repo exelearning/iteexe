26b52225bb362bfffb5455812eeb8581788b7a05
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2015-05-19 18:34:27 +0200

    Fix windows binary build

--- win-setup.py
+++ win-setup.py
@@ -76,7 +76,7 @@ dataFiles(["exe/jsui/templates",
 
 opts = {
  "py2exe": {
-   "packages": ["encodings", "nevow", "nevow.flat", "cProfile", "functools", "csv", "libxml2", "robotparser", "chardet", "lxml", "feedparser", "BeautifulSoup", "oauth2client", "googleapiclient", "six"],
+   "packages": ["encodings", "nevow", "nevow.flat", "cProfile", "functools", "csv", "libxml2", "robotparser", "chardet", "lxml", "feedparser", "BeautifulSoup", "oauth2client", "googleapiclient", "BaseHTTPServer"],
    "includes": ["PngImagePlugin", "JpegImagePlugin", "GifImagePlugin",
                 "IcoImagePlugin", "BmpImagePlugin"],
 
