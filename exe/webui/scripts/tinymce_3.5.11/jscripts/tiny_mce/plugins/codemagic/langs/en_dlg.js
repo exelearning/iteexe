27a12e3158efaa300f7e07b9f95151ffd918badf
ignaciogros <ignacio@gros.es>
2015-09-08 11:44:05 +0200

    New Code Editor. CodeMagic (sutulustus.com). It's Andrew Collington's (amnuts.com) version, and it's under the MIT license. It uses CodeMirror (codemirror.net), also under the MIT license.

---
+++ exe/webui/scripts/tinymce_3.5.11/jscripts/tiny_mce/plugins/codemagic/langs/en_dlg.js
@@ -0,0 +1,13 @@
+tinyMCE.addI18n('en.codemagic_dlg',{
+    code_title           : _("CodeMagic - syntax coloring and intendation"),
+    code_label           : _("Edit code"),
+    toggle_autoclosetags : _("Auto close tags"),
+    toggle_wraptext      : _("Wrap text"),
+    toggle_highlight     : _("Highlight syntax"),
+    theme_select         : _("Select a theme"),
+    search               : _("Search"),
+    find_next            : _("Find next"),
+    find_previous        : _("Find previous"),
+    replace              : _("Replace"),
+    replace_all          : _("Replace all")
+});
