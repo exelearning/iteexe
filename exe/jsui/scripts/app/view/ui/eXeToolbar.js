f44531cc05fede86ea0d34593620bde8e0a9c4fd
Mercedes Cotelo Lois <mclois@gmail.com>
2015-10-20 19:29:47 +0200

    Revert "Delete "Publish Google Drive" link menu: (File > export > web site)"
    This reverts commit 7d677bf9ac46205bc3bb29a2abed619a12c14f12.

--- exe/jsui/scripts/app/view/ui/eXeToolbar.js
+++ exe/jsui/scripts/app/view/ui/eXeToolbar.js
@@ -283,6 +283,12 @@ Ext.define('eXe.view.ui.eXeToolbar', {
                                                     },
                                                     {
                                                         xtype: 'accesskey_menuitem',
+                                                        text: _('Publish in Google Drive'),
+                                                        accesskey: 'f',
+                                                        itemId: 'file_export_googledrive'
+                                                    },
+                                                    {
+                                                        xtype: 'accesskey_menuitem',
                                                         text: _('Zip File'),
                                                         accesskey: 'z',
                                                         itemId: 'file_export_zip'
