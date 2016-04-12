ac08ff825bdb12e5d74852aa9bbca4ca78a72d45
Juan Rafael Fernández <jrfern@gmail.com>
2016-04-12 16:40:09 +0200

    Hopefully the last search for new strings bofore the new version.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2016-03-30 17:33+0200\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage-Team: EXE Team <jrfern@gmail.com>\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
+ "": "Project-Id-Version: eXeLearning 2.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2016-04-12 16:37+0200\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage-Team: EXE Team <jrfern@gmail.com>\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 1.3\n"
 };
