8c506493ca142b4b708bab8d62f856b2a1d7d746
ignaciogros <ignacio@gros.es>
2016-05-13 12:54:30 +0200

    CodeMagic: Replacement source code editor plugin by Josh Lobe. Adapted to work with eXe's i18n mechanism.

--- exe/webui/scripts/tinymce_4_settings.js
+++ exe/webui/scripts/tinymce_4_settings.js
@@ -8,13 +8,12 @@ _ = parent._;
 
 var $exeTinyMCE = {
 	
-	plugins: "exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks code fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
+	plugins: "exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks codemagic fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
 	buttons1 : "newdocument | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify clearfloat addcontent | bullist numlist definitionlist | outdent indent blockquote blockquoteandcite | formatselect | fontsizeselect | fontselect | forecolor backcolor | superscript subscript | fullscreen",	
 	buttons2 : "undo redo | cut copy paste pastetext | pastehtml pastecode | searchreplace | link unlink anchor tooltips | exeimage exemedia | removeformat | insertdatetime hr | charmap",
-	buttons3 : "template | table | code help",
+	buttons3 : "template | table | codemagic help",
 	// To add:
-		// buttons2 : "exeeffects,exegames,cite,abbr,acronym,del,ins,attribs,nonbreaking,exemath,styleprops",
-		// buttons3 : "codemagic",	
+		// buttons2 : "exeeffects,exegames,cite,abbr,acronym,del,ins,attribs,nonbreaking,exemath,styleprops"
 	content_css: "/css/extra.css," + exe_style,
 	browser_spellcheck: true,
 	templates: [
