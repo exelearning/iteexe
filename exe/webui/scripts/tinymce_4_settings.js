dc6427f7f9c66865c29f13beef94f2f81844adbf
ignaciogros <ignacio@gros.es>
2016-05-16 12:42:51 +0200

    Effects Plugin. To be used with the files in this folder: /scripts/exe_effects/ + CITE added to the "format" menu.

--- exe/webui/scripts/tinymce_4_settings.js
+++ exe/webui/scripts/tinymce_4_settings.js
@@ -8,12 +8,12 @@ _ = parent._;
 
 var $exeTinyMCE = {
 	
-	plugins: "easyattributes exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks codemagic fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
+	plugins: "exeeffects easyattributes exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks codemagic fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
 	buttons1 : "bold italic | formatselect fontsizeselect fontselect | forecolor backcolor",
 	buttons2 : "alignleft aligncenter alignright alignjustify clearfloat addcontent | bullist numlist definitionlist | link unlink | outdent indent | blockquote blockquoteandcite",	
-	buttons3 : "undo redo | cut copy paste pastetext | pastehtml pastecode | tooltips | exeimage exemedia | template | table | easyattributes | codemagic | fullscreen",	
+	buttons3 : "undo redo | cut copy paste pastetext | pastehtml pastecode | tooltips exeeffects | exeimage exemedia | template | table | easyattributes | codemagic | fullscreen",	
 	// To add:
-		// buttons2 : "exeeffects,exegames,cite,del,ins,attribs,nonbreaking,exemath,styleprops"
+		// buttons2 : "exegames,nonbreaking,exemath,styleprops"
 	content_css: "/css/extra.css," + exe_style,
 	browser_spellcheck: true,
 	templates: [
@@ -70,8 +70,9 @@ var $exeTinyMCE = {
 					{title: _('Subscript'), icon: 'subscript', format: 'subscript'},
 					{title: _('Code'), icon: 'code', format: 'code'},
 					// Deletion and Insertion are not part of the default list
-					{title: _('Deletion'), inline: 'del'},
-					{title: _('Insertion'), inline: 'ins'}
+					{title: _('Deletion')+": <del>", inline: 'del'},
+					{title: _('Insertion')+": <ins>", inline: 'ins'},
+					{title: _('Cite')+": <cite>", inline: 'cite'}
 				]}
 			],
 			toolbar: [
