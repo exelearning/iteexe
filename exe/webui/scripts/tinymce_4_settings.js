ee7fedb2a76dc5728325ff032ace54ddb11b8443
ignaciogros <ignacio@gros.es>
2016-05-17 14:54:29 +0200

    Hangman Game Plugin to work with /scripts/exe_games/

--- exe/webui/scripts/tinymce_4_settings.js
+++ exe/webui/scripts/tinymce_4_settings.js
@@ -8,12 +8,12 @@ _ = parent._;
 
 var $exeTinyMCE = {
 	
-	plugins: "exeeffects easyattributes exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks codemagic fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
+	plugins: "exegames_hangman exeeffects easyattributes exelist autolink lists link charmap print preview anchor tooltips searchreplace visualblocks codemagic fullscreen insertdatetime table contextmenu paste template textcolor hr clearfloat addcontent definitionlist blockquoteandcite pastecode exeimage exealign exemedia abbr",
 	buttons1 : "bold italic | formatselect fontsizeselect fontselect | forecolor backcolor",
 	buttons2 : "alignleft aligncenter alignright alignjustify clearfloat addcontent | bullist numlist definitionlist | link unlink | outdent indent | blockquote blockquoteandcite",	
 	buttons3 : "undo redo | cut copy paste pastetext | pastehtml pastecode | tooltips exeeffects | exeimage exemedia | template | table | easyattributes | codemagic | fullscreen",	
 	// To add:
-		// buttons2 : "exegames,nonbreaking,exemath,styleprops"
+		// buttons2 : "nonbreaking,exemath,styleprops"
 	content_css: "/css/extra.css," + exe_style,
 	browser_spellcheck: true,
 	templates: [
