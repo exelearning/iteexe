a9e8991db5dc2f7201213d6cc2c3918fdceff1e4
ignaciogros <ignacio@gros.es>
2016-10-20 11:42:57 +0200

    Example iDevice (simplified).

--- exe/webui/scripts/idevices/example-idevice/edition/example-idevice-02.js
+++
@@ -1,4 +0,0 @@
-// Test file (second JS)
-$(function(){
-	$(".exe-editButtons").before("<p>example-idevice-02.js is loaded!</p>")
-});
--- exe/webui/scripts/idevices/example-idevice/export/example-idevice-03.js
+++
@@ -1,6 +0,0 @@
-// Test file (third JS)
-$(function(){
-	$(".exe-exampleIdevice").each(function(instance){
-		$(this).append("<p style='color:green'><strong>example-idevice-03.js</strong> (instance "+instance+")</p>");
-	});
-});
