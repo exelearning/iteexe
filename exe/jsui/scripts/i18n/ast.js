2d8868af2394454461c28eb7eaa6200598e661ac
Juan Rafael Fernández <jrfern@gmail.com>
2016-12-22 18:02:47 +0100

    Regenerating the PO files (85 new strings)

--- exe/jsui/scripts/i18n/ast.js
+++ exe/jsui/scripts/i18n/ast.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2016-11-22 18:34+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: ast\nLanguage-Team: ast <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.3.4\n"
+ "": "Project-Id-Version: eXeLearning 2.1.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2016-12-22 18:00+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: ast\nLanguage-Team: ast <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.3.4\n"
 };
