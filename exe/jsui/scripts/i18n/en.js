ac05979f1e5336a8266e4ea665165dff404279d3
ignaciogros <ignacio@gros.es>
2016-12-27 12:48:33 +0100

    Back to this revision: 5dc52785736d19ad32145f10d887facfb589c10e

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2016-12-22 18:00+0100\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.3.4\n"
+ "": "Project-Id-Version: eXeLearning 2.1.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2016-11-22 18:34+0100\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.3.4\n"
 };
