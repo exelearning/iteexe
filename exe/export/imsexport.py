6067e4557da74a81699e000e8d9d1ef31e10ef1a
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2017-02-03 15:11:43 +0000

    Mark exported SCORMS and IMS resources in LOM-es metadata as editable by eXe

--- exe/export/imsexport.py
+++ exe/export/imsexport.py
@@ -87,6 +87,16 @@ class Manifest(object):
             if not title.get_string():
                 title.add_string(lomsubs.LangStringSub(self.package.lang.encode('utf-8'), self.package.name))
                 metadata.get_general().set_title(title)
+            if self.package.exportSource:
+                technical = metadata.get_technical()
+                if not technical:
+                    technical = lomsubs.technicalSub('technical')
+                    metadata.set_technical(technical)
+                opr = technical.get_otherPlatformRequirements()
+                if not opr:
+                    opr = lomsubs.otherPlatformRequirementsSub()
+                    technical.set_otherPlatformRequirements(opr)
+                opr.add_string(lomsubs.LangStringSub(self.package.lang.encode('utf-8'), 'editor: eXe Learning'))
             metadata.export(output, 0, namespace_=namespace, pretty_print=False)
             xml = output.getvalue()
         if self.metadataType == 'LOM':
