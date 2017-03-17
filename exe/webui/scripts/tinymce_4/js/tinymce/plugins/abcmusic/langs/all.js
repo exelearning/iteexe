23c7793f95936c1f1bc595e74af4ae5e6f230661
ignaciogros <ignacio@gros.es>
2017-03-17 13:00:44 +0100

    ABC Music notation plugin for TinyMCE. Not finished yet.

---
+++ exe/webui/scripts/tinymce_4/js/tinymce/plugins/abcmusic/langs/all.js
@@ -0,0 +1,14 @@
+tinymce.addI18n('all', {
+	'ABC Music Notation': 'Notación musical (ABC)',
+	'Animation': 'Animación',
+	'Animate': 'Animar',
+	'Paste or type a valid ABC code:': 'Pega o escribe código ABC válido',
+	'Preview': 'Previsualizar',
+	'Visit eXeABC for help, tunes, ABC software...': 'Visita eXeABC para encontrar ayuda, canciones, software...',
+	'jQuery is required': 'No se encuentra jQuery',
+	'ABC Music': 'Música ABC',
+	'Download': 'Descargar',
+	'No valid ABC code found.': 'No se ha encontrado código ABC válido.',
+	'Play': 'Reproducir',
+	'Stop': 'Detener'
+});
