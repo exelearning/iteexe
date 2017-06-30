d4bc9d9e7cf6b644d4c2ed0220522fec69e24d50
ignaciogros <ignacio@gros.es>
2017-06-30 15:16:31 +0200

    Three simple text iDevices for PBL: Information, Learning Diary and Activity.

---
+++ exe/webui/scripts/idevices/pbl-diary/edition/pbl-diary.js
@@ -0,0 +1,31 @@
+/**
+ * Simple Text iDevice (edition code)
+ *
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: Ignacio Gros (http://gros.es/) for http://exelearning.net/
+ *
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ */
+var $exeDevice = {
+	
+	init : function(){
+		
+		// Add some instructions
+		$("TEXTAREA").before("<p>"+_("The Learning Diary is focused on the student's reflection and reasoning to construct their own learning.")+"</p>");
+		
+		// Enable TinyMCE
+		if (tinymce.majorVersion==4) $exeTinyMCE.init("single",".jsContentEditor");
+		else if (tinymce.majorVersion==3) $exeTinyMCE.init("specific_textareas","jsContentEditor");
+		
+	},
+	
+	save : function(){
+		
+		// Just return true
+		// authoring.js will save the content
+		// See $exeAuthoring.iDevice.init
+		return true;
+		
+	}
+
+}
