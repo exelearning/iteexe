b93202c4dcc419cfda0dfd2e9323afc917472d5f
Juan Rafael Fernández <jrfern@gmail.com>
2017-08-30 13:15:09 +0200

    Beta new Spanish translation.

--- exe/jsui/scripts/i18n/he.js
+++ exe/jsui/scripts/i18n/he.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2017-08-30 12:47+0200\nPO-Revision-Date: 2016-02-14 21:17+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+ "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2017-08-30 12:54+0200\nPO-Revision-Date: 2016-02-14 21:17+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
 };
--- exe/jsui/scripts/i18n/ig.js
+++ exe/jsui/scripts/i18n/ig.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2017-08-30 12:47+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2017-08-30 12:54+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
