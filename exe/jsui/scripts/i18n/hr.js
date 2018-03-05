25e550447b1f107d28c6aefdc04d6043a6af2ffc
Juan Rafael Fernández <jrfern@gmail.com>
2018-03-05 19:08:51 +0100

    Run mki18n.sh - some errors reported.

--- exe/jsui/scripts/i18n/hr.js
+++ exe/jsui/scripts/i18n/hr.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2017-08-31 12:00+0200\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage: hr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 19:00+0100\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage: hr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
--- exe/jsui/scripts/i18n/lo.js
+++ exe/jsui/scripts/i18n/lo.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2017-08-31 12:00+0200\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 19:00+0100\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
