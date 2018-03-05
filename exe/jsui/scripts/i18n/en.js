3f7d198b655d7f966f3d0737453c0a9c011e10a6
Juan Rafael Fernández <jrfern@gmail.com>
2018-03-05 20:39:31 +0100

    Rerun mki18n.sh - no error messages now.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 19:00+0100\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+ "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 20:38+0100\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
 };
