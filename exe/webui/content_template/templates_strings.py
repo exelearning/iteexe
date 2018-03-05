3ae5d441d485636ddee837b7c3be47cc7768a102
Juan Manuel Casanova González <juan.casanova@sdweb.es>
2018-03-05 12:15:44 +0100

    Manually generated templates' strings file

---
+++ exe/webui/content_template/templates_strings.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+# This is a dummy file so Babel can get to the templates strings
+
+## Base.elt strings
+# _levelNames
+Base_elt__levelNames_1 = _(u'Topic')
+Base_elt__levelNames_2 = _(u'Section')
+Base_elt__levelNames_3 = _(u'Unit')
+# _title
+Base_elt__title = _(u'')
+# _description
+Base_elt__description = _(u'')
+# footer
+Base_elt_footer = _(u'')
+# _objectives
+Base_elt__objectives = _(u'')
+# _preknowledge
+Base_elt__preknowledge = _(u'')
+# _author
+Base_elt__author = _(u'')
+
+# Nodes
+# _title
+Base_elt_Node_0__title = _(u'Home')
+
+
+## EDIAProject-PBL.elt
+# _levelNames
+EDIAProject_PBL_elt__levelNames_1 = _(u'Topic')
+EDIAProject_PBL_elt__levelNames_2 = _(u'Section')
+EDIAProject_PBL_elt__levelNames_3 = _(u'Unit')
+# _title
+EDIAProject_PBL_elt__title = _(u'Project\'s name')
+# _description
+EDIAProject_PBL_elt__description = _(u'Project\'s final goal.')
+# footer
+EDIAProject_PBL_elt_footer = _(u'''&lt;script&gt;
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59443119-1', 'auto');
+  ga('send', 'pageview');
+
+&lt;/script&gt;''')
+# _objectives
+EDIAProject_PBL_elt__objectives = _(u'Point out what students are about to learn.')
+# _preknowledge
+EDIAProject_PBL_elt__preknowledge = _(u'Students need no preknowledge to complete all these tasks because there are materials enough to be independent in their accomplishment. Students also have the help and orientation from their teacher.')
+# _author
+EDIAProject_PBL_elt__author = _(u'Author\'s name')
+
+# Nodes
+# _title
+EDIAProject_PBL_elt_Node_0__title = _(u'Front page')
+
+# Idevices
+# _title
+EDIAProject_PBL_elt_Node_0_Idevice_0__title = _(u'Free Text')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_0_Idevice_0_Field_0_content_w_resourcePaths = _(u'''&lt;p&gt;Motivating inspiration.&lt;/p&gt;
+&lt;p&gt;Project / Final Challenge&lt;/p&gt;
+&lt;p style=&quot;text-align: center;&quot;&gt;&lt;img src=&quot;resources/images_pbl_english.png&quot; alt=&quot;&quot; width=&quot;560&quot; height=&quot;315&quot; /&gt;&lt;/p&gt;
+&lt;p&gt;What to learn and how to learn.&lt;/p&gt;
+&lt;p&gt;Motivation for the project development.&lt;/p&gt;''')
+
+# _title
+EDIAProject_PBL_elt_Node_0_Idevice_1__title = _('Free Text')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_0_Idevice_1_Field_0_content_w_resourcePaths = _(u'''<p style="text-align: center;"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width: 0px; display: block; margin-left: auto; margin-right: auto;" src="resources/88x31.png" width="88" height="31" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">EDIA Project: English PBL</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://cedec.educalab.es/" property="cc:attributionName" target="_blank">CeDeC</a>.</p> <p></p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_1__title = _(u'Breaking the ice')
+# Idevices
+# _title
+EDIAProject_PBL_elt_Node_1_Idevice_0__title = _(u'Your project: general information')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_1_Idevice_0_Field_0_content_w_resourcePaths = _(u'''&lt;p&gt;Explanation of the final challenge.&lt;/p&gt;
+&lt;p&gt;Overview of suggested tasks.&lt;/p&gt;
+&lt;p style=&quot;text-align: center;&quot;&gt;&lt;img src=&quot;resources/images_pbl_english.png&quot; alt=&quot;&quot; width=&quot;560&quot; height=&quot;315&quot; /&gt;&lt;/p&gt;
+&lt;p&gt;&lt;/p&gt;
+&lt;p&gt;&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;General information about:&lt;/span&gt;&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;ul&gt;
+&lt;li&gt;Assessment&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;Class team management&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;Learning diary and personal site.&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;Dissemination of project activities.&lt;/span&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p&gt;Starting motivation&lt;span style=&quot;font-size: 1em; line-height: 19.2px;&quot;&gt;&lt;/span&gt;&lt;/p&gt;''')
+
+# _title
+EDIAProject_PBL_elt_Node_1_Idevice_1__title = _(u'Starting Activity')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_1_Idevice_1_Field_0_content_w_resourcePaths = _(u'''<p>Introductory Activity to the project involving:</p> <ul> <li>Team work</li> <li>Reflection and motivation</li> <li>Expression of feelings.</li> </ul> <p style="text-align: center;"><img src="resources/images_pbl_english.png" alt="" width="560" height="315" /></p> <p><strong>Steps for the suggested activity<span style="color: #ff0000;"><br /></span></strong></p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_1_Idevice_2__title = _(u'Learning Diary')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_1_Idevice_2_Field_0_content_w_resourcePaths = _(u'''<p>First activity. Creation of the learning diary in the suggested format.</p> <p>Creation of the first post heading.</p> <p></p> <p></p>''')
+EDIAProject_PBL_elt_Node_1_Idevice_2_Field_1_content_w_resourcePaths = _(u'''<div class="block"> <div id="3_93-viewer"> <p>Script for writing the post. It must lead the student to think about and reflect:</p> <ul> <li>What the final challenge is</li> <li>What the assessment will be like</li> <li>What they will learn</li> <li>How class groups work</li> </ul> </div> </div>''')
+
+
+# _title
+EDIAProject_PBL_elt_Node_2__title = _(u'Mission 1')
+# _title
+EDIAProject_PBL_elt_Node_2_Idevice_0__title = _('Introduction')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_2_Idevice_0_Field_0_content_w_resourcePaths = _(u'''<p>What the final mission will be.</p> <p>What sudents will learn about.</p> <p style="text-align: center;"><img src="resources/images_pbl_english.png" alt="" width="560" height="315" /></p> <p style="text-align: center;"></p> <p>Starting activity</p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_2_Idevice_1__title = _('Task (Mission)')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_2_Idevice_1_Field_0_content_w_resourcePaths = _(u'''<p>General introduction</p> <p>What the "final mission" will be and link to the assessment rubric.</p> <p style="text-align: center;"><img src="resources/images_pbl_english.png" alt="" width="560" height="315" /></p> <p>What learning objective this mission helps to achieve.</p> <p></p> <p></p> <p><strong>Steps for your mission</strong></p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_2_Idevice_2__title = _('Learning Diary')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_2_Idevice_2_Field_0_content_w_resourcePaths = _(u'''<p>Guidelines to open a new post.</p> <p></p>''')
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_2_Idevice_2_Field_1_content_w_resourcePaths = _(u'''<div id="8_93-viewer"> <div class="block"> <div id="3_93-viewer"> <div class="block"> <div id="3_93-viewer"> <div id="8_93-viewer"> <div class="block"> <div id="3_93-viewer"> <div class="block"> <div id="3_93-viewer"> <p>Questions or guidelines for students to revise and reflect:</p> <ul> <li>The task they have done</li> <li>The resources  they have checked</li> <li>Materials they have created</li> <li>What they have learnt</li> <li>Difficulties found.</li> <li>Remaining doubts</li> <li>Class team functioning</li> </ul> </div> </div> </div> </div> </div> </div> </div> </div> </div> </div>''')
+
+
+# _title
+EDIAProject_PBL_elt_Node_3__title = _(u'Didactic guidelines')
+# Idevices
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_0__title = _(u'Fact sheet')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_0_Field_0_content_w_resourcePaths = _(u'''<table style="margin-left: auto; margin-right: auto; width: 624.333px; border-color: #000000;" cellspacing="4" cellpadding="4"><colgroup><col width="116" /><col width="509" /></colgroup> <tbody> <tr style="height: 35px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Títle</strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p>...</p> </td> </tr> <tr style="height: 35.375px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Subject</strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p>...</p> </td> </tr> <tr style="height: 35px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Level / course<br /></strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p>...</p> </td> </tr> <tr style="height: 35px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Final project<br /></strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...<br /></span></span></span></p> </td> </tr> <tr style="height: 35px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Contents</strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">..</span></span></span></p> </td> </tr> <tr style="height: 35px;" valign="top"> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><strong>Timing</strong></p> </td> <td style="height: 35px; border-color: #000000; width: 248px; text-align: left;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> </tr> </tbody> </table> <p></p>''')
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_0_Field_1_content_w_resourcePaths = _(u'''<p><span>Resource structure<br /></span></p> <p><span>Types of tasks and activities</span></p> <p>Methodological approach</p> <p>Use of the learning diary</p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_1__title = _(u'Learning Pathway and timing')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_1_Field_0_content_w_resourcePaths = _(u'''<p></p> <table style="width: 95.5125%;" cellspacing="0" cellpadding="4"><colgroup><col width="64*" /> <col width="96*" /> <col width="96*" /></colgroup> <tbody> <tr valign="top"> <td style="width: 35%; text-align: left;"> <h3 style="text-align: left;" align="center"><strong><span color="#000000"><span face="Liberation Serif, serif"><span size="3">Steps</span></span></span></strong></h3> </td> <td style="width: 47%; text-align: left;"> <h3 style="text-align: left;" align="center"><strong><span color="#000000"><span face="Liberation Serif, serif"><span size="3">Main aim</span></span></span></strong></h3> </td> <td style="width: 36.5786%;"> <h3 align="center"><strong><span color="#000000"><span face="Liberation Serif, serif"><span size="3">Timing</span></span></span></strong></h3> </td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">Before you start</span></span></span></p> </td> <td style="width: 47%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> <td style="width: 36.5786%; text-align: center;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...sessions</span></span></span></p> </td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">Breaking the ice<br /></span></span></span></p> </td> <td style="width: 47%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> <td style="width: 36.5786%; text-align: center;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...sessions</span></span></span></p> </td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">(Mission 1)<br /></span></span></span></p> </td> <td style="width: 47%;"> <p><span size="2" style="font-size: small;">...</span></p> </td> <td style="width: 36.5786%; text-align: center;"><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...sessions</span></span></span></td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">(Mission 2)</span></span></span></p> </td> <td style="width: 47%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> <td style="width: 36.5786%; text-align: center;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...sessions</span></span></span></p> </td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">(Mission 3)</span></span></span></p> </td> <td style="width: 47%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> <td style="width: 36.5786%; text-align: center;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...sessions</span></span></span></p> </td> </tr> <tr valign="top"> <td style="width: 35%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">(Final Challenge)<br /></span></span></span></p> </td> <td style="width: 47%;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">...</span></span></span></p> </td> <td style="width: 36.5786%; text-align: center;"> <p><span color="#000000"><span face="Liberation Serif, serif"><span size="3">3 sessions</span></span></span></p> </td> </tr> </tbody> </table> <p></p> <h3><span style="color: #008080;"><span>Interdisciplinary nature</span></span></h3> <p>Recommendations to involve other subjects</p> <p></p> <p></p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_2__title = _(u'Assessment and curricular references')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_2_Field_0_content_w_resourcePaths = _(u'''<p><span>Link to the final rubric of the project.</span></p> <p>Assessment recommendations / resources of each task.</p> <p>Assessment criteria and guidance on how to assess.</p> <p>Contents / learning standards and key competences.</p> <p></p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_3__title = _(u'Need any help?')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_3_Field_0_content_w_resourcePaths = _(u'''<h3>ICT resources/apps/tools</h3>''')
+
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_3__title = _(u'Rubrics and other documents')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_3_Field_0_content_w_resourcePaths = _(u'''<p>You can download all the documents to print, share, reuse...</p>''')
+
+# _title
+EDIAProject_PBL_elt_Node_3_Idevice_4__title = _(u'Free Text')
+# Fields
+# content_w_resourcePaths
+EDIAProject_PBL_elt_Node_3_Idevice_4_Field_0_content_w_resourcePaths = _(u'''<p style="text-align: center;"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width: 0px; display: block; margin-left: auto; margin-right: auto;" src="resources/88x31.png" width="88" height="31" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">EDIA Project: English PBL</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://cedec.educalab.es/" property="cc:attributionName" target="_blank">CeDeC</a>.</p>''')
