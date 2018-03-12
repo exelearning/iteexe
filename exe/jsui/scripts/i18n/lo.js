568e7b627d8f3c2753d6f1f9b28bb9d4d7e61631
Juan Rafael Fernández <jrfern@gmail.com>
2018-03-12 17:59:52 +0100

    Run mki18n.sh again - some source strings had been modified.

--- exe/jsui/scripts/i18n/lo.js
+++ exe/jsui/scripts/i18n/lo.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 20:38+0100\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-12 17:59+0100\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
--- exe/jsui/scripts/i18n/tr.js
+++ exe/jsui/scripts/i18n/tr.js
@@ -1,5 +1,5 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-05 20:38+0100\nPO-Revision-Date: 2008-11-24 21:02+1300\nLast-Translator: G\u00fcrkan G\u00fcr <seqizz@gmail.com>\nLanguage: tr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.1.4\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-03-12 17:59+0100\nPO-Revision-Date: 2008-11-24 21:02+1300\nLast-Translator: G\u00fcrkan G\u00fcr <seqizz@gmail.com>\nLanguage: tr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "Title": "Ba\u015fl\u0131k", 
  "Text Line": "Yaz\u0131 Sat\u0131r\u0131", 
  "Quit": "\u00c7\u0131k", 
