6ca1b5ea79921c097183d11b393c575a7d87051a
Juan Rafael Fernández <jrfern@gmail.com>
2018-04-17 17:22:41 +0200

    Run mki18n.sh twice. Why? Go figure.

--- exe/jsui/scripts/i18n/hr.js
+++ exe/jsui/scripts/i18n/hr.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-04-14 16:59+0200\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage: hr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-04-17 17:21+0200\nPO-Revision-Date: 2007-03-02 10:18+1200\nLast-Translator: \nLanguage: hr\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
