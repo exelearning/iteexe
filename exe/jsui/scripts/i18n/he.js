39b1264bcad3e9ab36da16027e78797d8bf08a57
Juan Rafael Fernández <jrfern@gmail.com>
2018-04-18 20:22:15 +0200

    New Italian translation, by Andrea Primiani.

--- exe/jsui/scripts/i18n/he.js
+++ exe/jsui/scripts/i18n/he.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2018-04-17 17:21+0200\nPO-Revision-Date: 2016-02-14 21:17+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+ "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2018-04-18 20:21+0200\nPO-Revision-Date: 2016-02-14 21:17+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
 };
