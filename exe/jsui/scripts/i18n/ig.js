7a0259849a24220bf4c515f6cfdf94dd22be867b
Juan Rafael Fernández <jrfern@gmail.com>
2018-04-25 20:48:40 +0200

    New Portuguese translation, by Dionisio Martínez Soler.

--- exe/jsui/scripts/i18n/ig.js
+++ exe/jsui/scripts/i18n/ig.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-04-24 19:26+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-04-25 20:47+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
