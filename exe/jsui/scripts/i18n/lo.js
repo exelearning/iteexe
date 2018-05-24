113cae8598938b06d0283629617f9f5720378f55
Juan Rafael Fernández <jrfern@gmail.com>
2018-05-24 19:00:10 +0200

    The final string freeze, v3.

--- exe/jsui/scripts/i18n/lo.js
+++ exe/jsui/scripts/i18n/lo.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-05-23 20:09+0200\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-05-24 18:58+0200\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
