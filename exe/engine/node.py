exelearning/iteexe

commit 200b97cebf5b75f97a22ffaad7f2551616e8b9bd

Fix #352 - Can't insert package

exe/engine/node.py
@@ -579,6 +579,11 @@ class Node(Persistable):
                 # continuing on with the delete anyhow, though...
             self.idevices[0].delete()
 
+        # We also have to make sure that no references to the node
+        # remain in package's anchor_nodes
+        if pruningZombies and self in self.package.anchor_nodes:
+            self.package.anchor_nodes.remove(self)
+
         if self.package: 
             self.package.isChanged = True
             self._package = None

commit e8c6871f3faba5c17be68c20447a07eef959375c

Fix regression introduced in 200b97cebf5b75f97a22ffaad7f2551616e8b9bd

exe/engine/node.py
@@ -581,7 +581,7 @@ class Node(Persistable):
 
         # We also have to make sure that no references to the node
         # remain in package's anchor_nodes
-        if pruningZombies and self in self.package.anchor_nodes:
+        if pruningZombies and hasattr(self.package, 'anchor_nodes') and self in self.package.anchor_nodes:
             self.package.anchor_nodes.remove(self)
 
         if self.package: 