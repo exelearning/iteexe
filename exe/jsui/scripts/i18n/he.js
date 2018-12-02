0102e1fd8876b4fc77d755e5fa4852f5b44e1fe8
Juan Rafael Fernández <jrfern@gmail.com>
2018-12-02 17:06:28 +0100

    New full Spanish translation.

--- exe/jsui/scripts/i18n/he.js
+++ exe/jsui/scripts/i18n/he.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2.1\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2018-10-05 16:35+0200\nPO-Revision-Date: 2018-10-15 18:36+0200\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
+ "": "Project-Id-Version: eXeLearning 2.3\nReport-Msgid-Bugs-To: eXe Translations <translations@exelearning.net>\nPOT-Creation-Date: 2018-12-02 16:57+0100\nPO-Revision-Date: 2018-10-15 18:36+0200\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: he\nLanguage-Team: he <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
 };
--- exe/jsui/scripts/i18n/lo.js
+++ exe/jsui/scripts/i18n/lo.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.2.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-10-05 16:35+0200\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-12-02 16:57+0100\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n", 
  "?????": "?????"
 };
