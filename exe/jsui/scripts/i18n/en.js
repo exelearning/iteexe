ccf09b6152923064127c3320eccd1e9efce84f01
Juan Rafael Fernández <jrfern@gmail.com>
2019-05-05 12:12:56 +0200

    New full Catalan translation, by Carles Ferrando Garcia.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.3.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2019-05-02 17:37+0200\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
+ "": "Project-Id-Version: eXeLearning 2.3.2\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2019-05-03 14:01+0200\nPO-Revision-Date: 2013-11-10 19:17+0100\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
 };
