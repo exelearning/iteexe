03cb01ccdc78bde1ec62794821f5ff01d792dcda
Juan Rafael Fernández <jrfern@gmail.com>
2019-11-06 20:23:15 +0100

    New Spanish translation.

--- exe/jsui/scripts/i18n/lo.js
+++ exe/jsui/scripts/i18n/lo.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2019-10-20 19:56+0200\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2019-11-06 18:20+0100\nPO-Revision-Date: 2007-03-02 10:24+1200\nLast-Translator: \nLanguage: lo\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n", 
  "?????": "?????"
 };
