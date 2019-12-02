9d10b70fdbc42fc823fb7af19667f56bcbfa6994
Juan Rafael Fernández <jrfern@gmail.com>
2019-12-02 20:27:52 +0100

    Finish adding Dzongkha.

---
+++ exe/jsui/scripts/i18n/dz.js
@@ -0,0 +1,3 @@
+translations = {
+ "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2019-11-29 20:41+0100\nPO-Revision-Date: 2019-12-02 20:19+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: dz\nLanguage-Team: dz <LL@li.org>\nPlural-Forms: nplurals=1; plural=0\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
+};
