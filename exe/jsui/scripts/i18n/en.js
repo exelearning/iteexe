1c75c326e7aef26e2d8c32307686d2a516ee0772
Juan Rafael Fernández <jrfern@gmail.com>
2019-12-07 12:31:44 +0100

    It seems an EN file is necessary somehow.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2019-11-29 20:41+0100\nPO-Revision-Date: 2019-10-20 20:24+0200\nLast-Translator: Juan Rafael Fern\u00e1ndez <jrfern@gmail.com>\nLanguage: en\nLanguage-Team: EXE Team <jrfern@gmail.com>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
+ "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2019-11-29 20:41+0100\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
 };
