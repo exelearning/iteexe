ce3611e74b813ddc4d19b0a9e778028213394969
Manuel Narvaez <mnarvaez007@gmail.com>
2020-03-06 12:15:16 +0100

    Nuevo idevice: Candado activity

---
+++ exe/webui/scripts/idevices/candado-activity/edition/candado-activity.js
@@ -0,0 +1,243 @@
+/**
+ * candado Activity iDevice (edition code)
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: Manuel Narváez Martínez
+ * Graphic design: Ana María Zamora Moreno, Francisco Javier Pulido
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ */
+var $exeDevice = {
+    // i18n
+    i18n: {
+        name: _('Candado Activity'),
+    },
+    iDevicePath: "/scripts/idevices/candado-activity/edition/",
+    msgs: {},
+    active: 0,
+    typeActive:0,
+    challengesGame: [],
+    candadoInstructions: '',
+    candadoRetro: '',
+    candadoTime: 40,
+    candadoSolution: '',
+    candadoShowMinimize:false,
+    candadoReboot:false,
+    ci18n: {
+        "msgOk": _("Aceptar"),
+        "msgMinimize": _("Minimize"),
+        "msgMaximize": _("Maximize"),
+        "msgTime": _("Time per question"),
+        "msgInstructions": _("Instructions"),
+        "msgFeedback": _("Feedback"),
+        "msgCodeAccess": _("Access code"),
+        "msgEnterCode": _("Enter the access code"),
+        "msgErrorCode": _("The access code is not correct"),
+        "msgRequiredAccessKey": _("Access code required"),
+        "msgSuccesses": _("Right! | Excellent! | Great! | Very good! | Perfect!"),
+        "msgFailures": _("It was not that! | Incorrect! | Not correct! | Sorry! | Error!"),
+        "msgEShowActivity" :_("Mostrar la actividad"),
+
+      
+    },
+
+    init: function () {
+        this.setMessagesInfo();
+        this.createForm();
+    },
+    setMessagesInfo: function () {
+        var msgs = this.msgs;
+        msgs.msgEGeneralSettings = _("General settings");
+        msgs.msgEIntrucctions = _("Indica las instruciones de la actividad");
+        msgs.msgTime= _("Tiempo");
+        msgs.msgERetro= _("Escribe la retroalimentación de la actividad");
+        msgs.msgCodeAccess=_("Access code"),
+        msgs.msgEnterCodeAccess= _("Enter the access code");
+        msgs.msgEInstructions = _("Instructions");
+        msgs.msgEREtroalimatacion = _("Feedback");
+        msgs.msgEShowMinimize =_("Show minimized.");
+        msgs.msgERebootActivity  =_("Repetir actividad.")
+
+    },
+    showMessage: function (msg) {
+        eXe.app.alert(msg);
+    },
+
+    createForm: function () {
+        var msgs=this.msgs;
+        var html = '\
+			<div id="candadoIdeviceForm">\
+				<div class="exe-form-tab" title="' +  msgs.msgEGeneralSettings+ '">\
+                        <div class="candado-EPanel" id="candadoEPanel">\
+                            <div class="candadoToggle">\
+                                    <input class="candado-Type" checked="checked" id="candadoECandado" type="radio" name="dsfDesRet" value="0" />\
+                                    <label for="candadoECandado">' + msgs.msgEInstructions + '</label>\
+                                    <input class="candado-Type"  id="candadoERetro" type="radio" name="dsfDesRet" value="1" />\
+                                    <label for="candadoERetro">' +  msgs.msgEREtroalimatacion + '</label>\
+                            </div>\
+                            <div class="candado-EDAtaGame">\
+                                <div class="candado-ERadioDatos">\
+                                    <textarea id="candadoEDescription" class="exe-html-editor"\> </textarea>\
+                                </div>\
+                                <div class="candado-EOptions">\
+                                        <label for="candadoEDSolution">' + msgs.msgCodeAccess + ': <input type="text" id="candadoEDSolution" /></label>\
+                                        <label id="candadolblEDTime" for="candadoEDTime">' + msgs.msgTime+ ':\
+                                        <select id="candadoEDTime">\
+                                            <option value="0"></option>\
+                                            <option value="1">1m</option>\
+                                            <option value="3">3m</option>\
+                                            <option value="5">5m</option>\
+                                            <option value="10" selected>10m</option>\
+                                            <option value="15">15m</option>\
+                                            <option value="20">20m</option>\
+                                            <option value="25">25m</option>\
+                                            <option value="30">30m</option>\
+                                            <option value="35">35m</option>\
+                                            <option value="40" >40m</option>\
+                                            <option value="40">45m</option>\
+                                            <option value="50">50m</option>\
+                                            <option value="55">55m</option>\
+                                            <option value="60">60m</option>\
+                                            </select>\
+                                        </label>\
+                                        <label for="candadoEShowMinimize"><input type="checkbox" id="candadoEShowMinimize"> ' + msgs.msgEShowMinimize + ' </label>\
+                                        <label for="candadoEReboot"><input type="checkbox" id="candadoEReboot" checked> ' + msgs.msgERebootActivity + ' </label>\
+                                </div>\
+                            </div>\
+                        </div>\
+                </div>\
+				' + $exeAuthoring.iDevice.gamification.common.getLanguageTab(this.ci18n) + '\
+		    </div>\
+			';
+        var field = $("textarea.jsContentEditor").eq(0)
+        field.before(html);
+        $exeAuthoring.iDevice.tabs.init("candadoIdeviceForm");
+        $exeDevice.loadPreviousValues(field);
+        $exeDevice.addEvents();
+        $exeAuthoring.iDevice.gamification.scorm.init();
+
+
+    },
+    isJsonString: function (str) {
+        try {
+            var o = JSON.parse(str, null, 2);
+            if (o && typeof o === "object") {
+                return o;
+            }
+        } catch (e) {}
+        return false;
+    },
+    loadPreviousValues: function (field) {
+
+        var originalHTML = field.val();
+        if (originalHTML != '') {
+            var wrapper = $("<div></div>");
+            wrapper.html(originalHTML);
+            var json = $('.candado-DataGame', wrapper).text();
+            var dataGame = $exeDevice.isJsonString(json);
+            $exeDevice.candadoInstructions=dataGame.candadoInstructions;
+            $exeDevice.candadoRetro=dataGame.candadoRetro;
+            $exeDevice.candadoSolution=dataGame.candadoSolution;
+            $exeDevice.candadoTime=dataGame.candadoTime;
+            $exeDevice.candadoInstructions = $(".candado-instructions", wrapper).eq(0).html();
+            $exeDevice.candadoRetro= $(".candado-retro", wrapper).eq(0).html();
+            $exeAuthoring.iDevice.gamification.common.setLanguageTabValues(dataGame.msgs);
+            $exeDevice.typeActive=0;
+            $('#candadoEDSolution').val($exeDevice.candadoSolution);
+            $('#candadoEDTime').val($exeDevice.candadoTime);
+            $('#candadoEShowMinimize').prop("checked", dataGame.candadoShowMinimize);
+            $('#candadoEReboot').prop("checked", dataGame.candadoReboot);
+
+
+            if(tinyMCE.get('candadoEDescription')){
+                tinyMCE.get('candadoEDescription').setContent($exeDevice.candadoInstructions);
+            }else{
+                $('#candadoEDescription').val($exeDevice.candadoInstructions)
+            }
+
+        }
+    },
+
+
+    save: function () {
+        if(!$exeDevice.validateCandado()){
+            return;
+        }
+        var dataGame = this.validateData();
+        if (!dataGame) {
+            return false;
+        }
+        var fields = this.ci18n,
+            i18n = fields;
+        for (var i in fields) {
+            var fVal = $("#ci18n_" + i).val();
+            if (fVal != "") i18n[i] = fVal;
+        }
+        dataGame.msgs = i18n;
+        var json = JSON.stringify(dataGame);
+        var html = '<div class="candado-IDevice">';
+        html += '<div class="candado-instructions sr-av">' + $exeDevice.candadoInstructions + '</div>';
+        html += '<div class="candado-retro  sr-av">' + $exeDevice.candadoRetro+ '</div>';
+        html += '<div class="candado-DataGame">' + json + '</div>';
+        html += '</div>';
+        return html;
+    },
+
+    validateCandado: function () {
+        var message = '';
+        $exeDevice.candadoTime = parseInt($('#candadoEDTime option:selected').val());
+        $exeDevice.candadoSolution=$('#candadoEDSolution').val();
+        $exeDevice.candadoShowMinimize = $('#candadoEShowMinimize').is(':checked');
+        $exeDevice.candadoReboot = $('#candadoEReboot').is(':checked');
+        if($exeDevice.typeActive==0){
+            $exeDevice.candadoInstructions=tinymce.editors[0].getContent();
+        }else if($exeDevice.typeActive==1){
+            $exeDevice.candadoRetro=tinymce.editors[0].getContent();
+        }
+        if ($exeDevice.candadoInstructions.length==0){
+            message = $exeDevice.msgs.msgEIntrucctions;
+        }else if ($exeDevice.candadoRetro.length==0){
+            message = $exeDevice.msgs.msgERetro;
+        }else if ($exeDevice.candadoSolution.length == 0) {
+            message = $exeDevice.msgs.msgEnterCodeAccess;
+        }
+        if (message.length != 0) {
+            $exeDevice.showMessage(message);
+            message = false;
+        } else {
+            message=true;
+        }
+        return message;
+    },
+
+    validateData: function () {
+           var data = {
+            'candadoTime': $exeDevice.candadoTime,
+            'candadoSolution': $exeDevice.candadoSolution,
+            'candadoInstructions': $exeDevice.candadoInstructions,
+            'candadoRetro': $exeDevice.candadoRetro,
+            'candadoShowMinimize': $exeDevice.candadoShowMinimize,
+            'candadoReboot': $exeDevice.candadoReboot
+        }
+        return data;
+    },
+    removeTags: function (str) {
+        var wrapper = $("<div></div>");
+        wrapper.html(str);
+        return wrapper.text();
+    },
+    addEvents: function () {
+        $('#candadoERetro').on('click', function (e) {
+            $exeDevice.candadoInstructions=tinymce.editors[0].getContent();
+            tinymce.editors[0].setContent( $exeDevice.candadoRetro);
+            $exeDevice.typeActive=1;
+        });
+        $('#candadoECandado').on('click', function (e) {
+            $exeDevice.candadoRetro=tinymce.editors[0].getContent();
+            tinymce.editors[0].setContent( $exeDevice.candadoInstructions);
+            $exeDevice.typeActive=0;
+        });
+
+
+    },
+
+
+}
---
+++ exe/webui/scripts/idevices/candado-activity/export/candado-activity.js
@@ -0,0 +1,325 @@
+/**
+ * Candado Activity iDevice (export code)
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: Manuel Narváez Martínez
+ * Graphic design: Ana María Zamora Moreno, Francisco Javier Pulido
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ */
+var $eXeCandado = {
+    idevicePath: "",
+    borderColors: {
+        black: "#1c1b1b",
+        blue: '#5877c6',
+        green: '#2a9315',
+        red: '#ff0000',
+        white: '#ffffff',
+        yellow: '#f3d55a'
+    },
+    colors: {
+        black: "#1c1b1b",
+        blue: '#d5dcec',
+        green: '#cce1c8',
+        red: '#f7c4c4',
+        white: '#ffffff',
+        yellow: '#f5efd6'
+    },
+    image: '',
+    options: {},
+    msgs: '',
+    hasSCORMbutton: false,
+    isInExe: false,
+    init: function () {
+        this.activities = $('.candado-IDevice');
+        if (this.activities.length == 0) return;
+        if (typeof ($exeAuthoring) != 'undefined' && $("#exe-submitButton").length > 0) {
+            this.activities.hide();
+            if (typeof (_) != 'undefined') this.activities.before('<p>' + _('Candado') + '</p>');
+            return;
+        }
+        if ($(".QuizTestIdevice .iDevice").length > 0) this.hasSCORMbutton = true;
+        if (typeof ($exeAuthoring) != 'undefined') this.isInExe = true;
+        this.idevicePath = this.isInExe ? "/scripts/idevices/candado-activity/export/" : "";
+        $eXeCandado.loadGame();
+    },
+
+    loadGame: function () {
+        $eXeCandado.options = [];
+        $eXeCandado.activities.each(function (i) {
+            var dl = $(".candado-DataGame", this),
+                mOption = $eXeCandado.loadDataGame(dl),
+                msg = mOption.msgs.msgPlayStart;
+            mOption.candadoInstructions= $(".candado-instructions", this).eq(0).html();
+            mOption.candadoRetro= $(".candado-retro", this).eq(0).html();
+            mOption.counter=mOption.candadoTime* 60;
+            mOption.candadoStarted=false;
+            mOption.candadoSolved=false;
+            $eXeCandado.options.push(mOption);
+            var candado = $eXeCandado.createInterfaceQuExt(i);
+            dl.before(candado).remove();
+            $('#candadoGameMinimize-' + i).hide();
+            $('#candadoGameContainer-' + i).hide();
+            if (mOption.candadoShowMinimize) {
+                $('#candadoGameMinimize-' + i).css({
+                    'cursor': 'pointer'
+                }).show();
+            } else {
+                $('#candadoGameContainer-' + i).show();
+            }
+            $('#candadoMessageMaximize-' + i).text(msg);
+            $eXeCandado.addEvents(i);
+        });
+
+    },
+    createInterfaceQuExt: function (instance) {
+        var html = '',
+            path = $eXeCandado.idevicePath,
+            msgs = $eXeCandado.options[instance].msgs;
+        html += '<div class="candado-MainContainer">\
+                <div class="candado-GameMinimize" id="candadoGameMinimize-' + instance + '">\
+                    <a href="#" class="candado-LinkMaximize" id="candadoLinkMaximize-' + instance + '" title="' + msgs.msgMaximize + '"><img src="' + path + 'candadoIcon.png" class="candado-Icons candado-IconMinimize" alt="' + msgs.msgEShowActivity + '">\
+                        <div class="candado-MessageMaximize" id="candadoMessageMaximize-' + instance + '">' + msgs.msgEShowActivity + '</div>\
+                    </a>\
+                </div>\
+                <div class="candado-GameContainer" id="candadoGameContainer-' + instance + '">\
+                    <div class="candado-GameScoreBoard">\
+                        <div class="candado-TimeNumber" id="candadoTimeNumber-' + instance + '">\
+                            <div class="candado-TimeQuestion" id="candadoTimeQuestion-' + instance + '">\
+                                <strong><span class="sr-av">' + msgs.msgTime + ':</span></strong>\
+                                <div class="exeQuextIcons34  exeQuextIcons34-Time"></div>\
+                                <p id="candadoPTime-' + instance + '">00:00</p>\
+                            </div>\
+                            <a href="#" class="candado-LinkMinimize" id="candadoLinkMinimize-' + instance + '" title="' + msgs.msgMinimize + '">\
+                                <strong><span class="sr-av">' + msgs.msgMinimize + ':</span></strong>\
+                                <div class="exeQuextIcons34 exeQuextIcons34-Minimize"></div>\
+                            </a>\
+                        </div>\
+                    </div>\
+                    <div class="candado-Instructions" id="candadoInstructions-' + instance + '"></div>\
+                    <div class="candado-MessageInfo" id="candadoMessageInfo-' + instance + '">\
+                    <div class="sr-av">'+msgs.msgInstructions+'</div>\
+                        <p id="candadoPInformation-' + instance + '"></p>\
+                    </div>\
+                    <div class="candado-SolutionDiv" id="candadoSolutionDiv-' + instance + '">\
+                        <label class="labelSolution">'+msgs.msgCodeAccess+':</label><input type="text" class="candado-Solution"  id="candadoSolution-' + instance + '">\
+                        <input type="button" class="candado-SolutionButton" id="candadoSolutionButton-' + instance + '"   value="' + msgs.msgOk + '" />\
+                    </div>\
+                    <div class="candado-SolutionDiv" id="candadoNavigator-' + instance + '">\
+                        <input type="button" class="candado-ShowIntro" id="candadoShowIntro-' + instance + '"   value="'+msgs.msgInstructions+'" />\
+                        <input type="button" class="candado-ShowRetro" id="candadoShowRetro-' + instance + '"   value="'+msgs.msgFeedback+'" />\
+                    </div>\
+                </div>\
+            </div>'
+        return html;
+    },
+
+
+    loadDataGame: function (data) {
+        var json = data.text(),
+            mOptions = $eXeCandado.isJsonString(json);
+            return mOptions;
+    },
+    isJsonString: function (str) {
+        try {
+            var o = JSON.parse(str, null, 2);
+            if (o && typeof o === "object") {
+                return o;
+            }
+        } catch (e) {}
+        return false;
+    },
+    addZero: function (i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    },
+    saveDataStorage: function (instance) {
+        var mOptions = $eXeCandado.options[instance],
+        tiempo=mOptions.counter<0?0:mOptions.counter;
+
+        var data = {
+            'candadoStarted': mOptions.candadoStarted,
+            'candadoSolved': mOptions.candadoSolved,
+            'counter': tiempo,
+            'candadoTime': mOptions.candadoTime,
+            'candadoReboot':mOptions.candadoReboot
+
+        }
+        localStorage.setItem('dataCandado-' + instance, JSON.stringify(data));
+    },
+        getDataStorage: function (instance) {
+        var data = $eXeCandado.isJsonString(localStorage.getItem('dataCandado-' + instance));
+        return data;
+    },
+    addEvents: function (instance) {
+        var mOptions = $eXeCandado.options[instance];
+        window.addEventListener('unload', function () {
+            if (mOptions.candadoStarted) {
+               $eXeCandado.saveDataStorage(instance);
+            }
+        });
+        $('#candadoLinkMaximize-' + instance).on('click touchstart', function (e) {
+            e.preventDefault();
+            $("#candadoGameContainer-" + instance).show()
+            $("#candadoGameMinimize-" + instance).hide();
+            if(!mOptions.candadoStarted){
+                $eXeCandado.startGame(instance);
+            };
+            $('#candadoSolution-' + instance).focus();
+        });
+        $("#candadoLinkMinimize-" + instance).on('click touchstart', function (e) {
+            e.preventDefault();
+            $("#candadoGameContainer-" + instance).hide();
+            $("#candadoGameMinimize-" + instance).css('visibility', 'visible').show();
+        });
+
+        $('#candadoSolution-' + instance).on("keydown", function (event) {
+            if (event.which === 13 || event.keyCode === 13) {
+                $eXeCandado.answerActivity(instance);
+                return false;
+            }
+            return true;
+        });
+        $('#candadoSolutionButton-' + instance).on('click touchstart', function (e) {
+            e.preventDefault();
+            $eXeCandado.answerActivity(instance);
+        });
+        $('#candadoShowIntro-' + instance).on('click touchstart', function (e) {
+            $('#candadoInstructions-' + instance).html(mOptions.candadoInstructions);
+            $('#candadoInstructions-' + instance).show();
+            $('#candadoSolutionDiv-' + instance).hide();
+        });
+        $('#candadoShowRetro-' + instance).on('click touchstart', function (e) {
+            $('#candadoInstructions-' + instance).html(mOptions.candadoRetro);
+            $('#candadoInstructions-' + instance).show();
+            $('#candadoSolutionDiv-' + instance).hide();
+        });
+        $('#candadoInstructions-' + instance).html(mOptions.candadoInstructions);
+        $('#candadoInstructions-' + instance).show();
+        $('#candadoSolution-' + instance).focus();
+        $('#candadoMessageInfo-'+ instance).show();
+        $('#candadoNavigator-' + instance).hide();
+        if(mOptions.candadoShowMinimize){
+            $("#candadoGameContainer-" + instance).hide();
+            $("#candadoGameMinimize-" + instance).css('visibility', 'visible').show();
+        }
+        if(mOptions.candadoTime==0){
+            $('#candadoTimeQuestion-'+ instance).hide();
+            $('#candadoTimeNumber-' + instance).css('width','32px');
+        }
+         var dataCandado = $eXeCandado.getDataStorage(instance);
+         mOptions.candadoSolved=false;
+         mOptions.counter=mOptions.candadoTime*60;
+         if (dataCandado) {
+             if(mOptions.candadoTime!=dataCandado.candadoTime || mOptions.candadoReboot!=dataCandado.candadoReboot  || (mOptions.candadoReboot && dataCandado.candadoSolved) ){
+                    localStorage.removeItem('dataCandado-' + instance);
+             }else{
+                 mOptions.candadoSolved=dataCandado.candadoSolved;
+                 mOptions.counter=dataCandado.counter;
+             }
+         }
+        if(!mOptions.candadoShowMinimize){
+            $eXeCandado.startGame(instance);
+        }
+        $('#candadoSolution-' + instance).focus();
+    },
+    startGame: function (instance) {
+       var mOptions = $eXeCandado.options[instance];
+       mOptions.candadoStarted=true;
+       if(mOptions.candadoSolved && !mOptions.candadoReboot){
+            $eXeCandado.showFeedback(instance);
+            return;
+       }
+       if(mOptions.candadoTime==0){
+            return;
+       }
+       $eXeCandado.uptateTime(0, instance);
+            mOptions.counterClock = setInterval(function () {
+                mOptions.counter--;
+                $eXeCandado.uptateTime(mOptions.counter, instance);
+                if (mOptions.counter <= 0 || mOptions.candadoSolved) {
+                    $eXeCandado.showFeedback(instance);
+                }
+
+        }, 1000);
+    },
+    showFeedback:function(instance){
+        var mOptions = $eXeCandado.options[instance];
+        clearInterval(mOptions.counterClock);
+        mOptions.candadoSolved=true;
+        $eXeCandado.uptateTime(mOptions.counter, instance);
+        $('#candadoInstructions-' + instance).html(mOptions.candadoRetro);
+        $('#candadoInstructions-' + instance).show();
+        $('#candadoSolutionDiv-' + instance).hide();
+        $('#candadoNavigator-' + instance).show();
+        $('#candadoMessageInfo-' + instance).hide();
+    },
+    uptateTime: function (tiempo, instance) {
+        tiempo=tiempo<0?0:tiempo;
+        var mTime = $eXeCandado.getTimeToString(tiempo);
+        $('#candadoPTime-' + instance).text(mTime);
+    },
+    getTimeToString: function (iTime) {
+        var mMinutes = parseInt(iTime / 60) % 60;
+        var mSeconds = iTime % 60;
+        return (mMinutes < 10 ? "0" + mMinutes : mMinutes) + ":" + (mSeconds < 10 ? "0" + mSeconds : mSeconds);
+    },
+    answerActivity: function (instance) {
+        var mOptions = $eXeCandado.options[instance],
+            answord = $('#candadoSolution-' + instance).val(),
+            message = "",
+            typeMessage = 0;
+        if (answord.length == 0) {
+            $eXeCandado.showMessage(1, mOptions.msgs.msgEnterCode, instance);
+            return;
+        }
+        if ($eXeCandado.checkWord(answord, mOptions.candadoSolution))  {
+            $eXeCandado.showFeedback(instance);
+        } else {
+            message = $eXeCandado.getRetroFeedMessages(false, instance) +  mOptions.msgs.msgErrorCode;
+            typeMessage = 1;
+            $('#candadoSolution-' + instance).val('');
+        }
+
+        $eXeCandado.showMessage(typeMessage, message, instance);
+
+    },
+    checkWord: function (answord, solution) {
+        var sSolution=$.trim(solution).replace(/\s+/g, " ").toUpperCase(),
+            sAnsWord=$.trim(answord).replace(/\s+/g, " ").toUpperCase();
+		if (sSolution.indexOf('|') == -1) {
+			return  sSolution== sAnsWord;
+		}
+		var words = sSolution.split('|');
+		for (var i = 0; i < words.length; i++) {
+			if ($.trim(words[i]) == sAnsWord) {
+				return true;
+            }
+
+		}
+		return false;
+	},
+    getRetroFeedMessages: function (iHit, instance) {
+        var msgs = $eXeCandado.options[instance].msgs;
+        var sMessages = iHit ? msgs.msgSuccesses : msgs.msgFailures;
+        sMessages = sMessages.split('|');
+        return sMessages[Math.floor(Math.random() * sMessages.length)];
+    },
+    showMessageAlert: function (tmsg) {
+        window.alert(tmsg)
+    },
+    showMessage: function (type, message, instance) {
+        var colors = ['#555555', $eXeCandado.borderColors.red, $eXeCandado.borderColors.green, $eXeCandado.borderColors.blue, $eXeCandado.borderColors.yellow];
+        color = colors[type];
+        $("#candadoPInformation-" + instance).text(message);
+        $("#candadoPInformation-" + instance).css({
+            'color': color,
+            'font-weight': 'bold'
+        });
+    },
+
+
+}
+$(function () {
+    $eXeCandado.init();
+});
