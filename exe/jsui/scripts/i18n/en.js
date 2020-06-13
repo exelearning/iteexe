99221fbba19b53d4d679b2a961f22694a108f4eb
jrfern <jrfern@gmail.com>
2020-06-13 16:51:32 +0200

    New Greek translation by Yannis Kaskamanidis.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2020-05-25 12:48+0200\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+ "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2020-06-13 16:20+0200\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
 };
--- exe/jsui/scripts/i18n/ig.js
+++ exe/jsui/scripts/i18n/ig.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2020-05-25 12:48+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2020-06-13 16:20+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "?????": "?????"
 };
