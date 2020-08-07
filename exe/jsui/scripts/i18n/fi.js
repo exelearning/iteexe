1cd59b142432977b482bcec75b5be57f53bbe96e
jrfern <jrfern@gmail.com>
2020-08-07 21:15:50 +0200

    Makes the new translations operative.

--- exe/jsui/scripts/i18n/fi.js
+++ exe/jsui/scripts/i18n/fi.js
@@ -1,5 +1,5 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2020-06-13 16:20+0200\nPO-Revision-Date: 2008-01-28 00:15+1300\nLast-Translator: Pekka Henttonen <pekka.henttonen@uta.fi>\nLanguage: fi\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
+ "": "Project-Id-Version: eXeLearning 2.5\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2020-06-25 19:39+0200\nPO-Revision-Date: 2008-01-28 00:15+1300\nLast-Translator: Pekka Henttonen <pekka.henttonen@uta.fi>\nLanguage: fi\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n", 
  "Click for completion instructions": "Klikkaa saadaksesi t\u00e4ydennysohjeita", 
  "Click \non the picture below or the \"Add Image\" button to select an image file to be \nmagnified.": "Klikkaa \nallaolevaa kuvaa tai \"Lis\u00e4\u00e4 kuva\" -painiketta valitaksesi \nsuurennettavan kuvan.", 
  "Emphasis": "Korostus", 
