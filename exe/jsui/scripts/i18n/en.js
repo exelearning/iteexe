444c4e9e82623a33f15d82b942d08897e40b8848
Juan Rafael Fernández <jrfern@gmail.com>
2020-08-20 19:15:15 +0200

    New full Spanish translation.

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.4.3\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2020-01-07 16:41+0100\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.6.0\n"
+ "": "Project-Id-Version: eXeLearning 2.5.1\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2020-08-20 18:31+0200\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
 };
