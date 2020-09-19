158e81906cb1a091b03da6595c391a7d5c66fab0
Juan Rafael Fernández <jrfern@gmail.com>
2020-09-19 14:28:47 +0200

    Adds the newly generated Breton files.

---
+++ exe/jsui/scripts/i18n/br.js
@@ -0,0 +1,3 @@
+translations = {
+ "": "Project-Id-Version: eXeLearning 2.5.1\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2020-09-17 13:53+0200\nPO-Revision-Date: 2020-09-19 14:21+0200\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: br\nLanguage-Team: br <LL@li.org>\nPlural-Forms: nplurals=6; plural=(n==1 ? 0 : n%10==1 && n%100!=11 && n%100!=71 && n%100!=91 ? 1 : n%10==2 && n%100!=12 && n%100!=72 && n%100!=92 ? 2 : (n%10==3 || n%10==4 || n%10==9) && n%100!=13 && n%100!=14 && n%100!=19 && n%100!=73 && n%100!=74 && n%100!=79 && n%100!=93 && n%100!=94 && n%100!=99 ? 3 : n%1000000==0 ? 4 : 5)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+};
