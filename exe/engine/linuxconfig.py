d9860414ed4af6312e660e2c3792cf943a5cbdb8
xpuig <xpuig@apsl.net>
2021-03-29 11:47:20 +0200

    Fixed #565 add eXeUIversion to linuxconfig.py

--- exe/engine/linuxconfig.py
+++ exe/engine/linuxconfig.py
@@ -72,6 +72,7 @@ class LinuxConfig(Config):
         self.audioMediaConverter_wav = "/usr/bin/sox %(infile)s %(outfile)s"
         self.audioMediaConverter_mp3 = "/usr/bin/sox %(infile)s -t wav - | /usr/bin/lame -b 32 - %(outfile)s"
         self.ffmpegPath = "/usr/bin/ffmpeg"
+        self.eXeUIversion = 2
 
     def _getConfigPathOptions(self):
         """
