3aaa2330836352d92e9e675ca4ed54924ed06372
IG Ignacio Gros <ignacio@gros.es>
2021-04-01 18:45:25 +0200

    Untranslated strings.

---
+++ exe/webui/scripts/tinymce_4/js/tinymce/plugins/exemindmap/editor/js/langs/all.js
@@ -0,0 +1,15 @@
+_ = top._;
+var customStrings = {
+	openMap : _("Open map"),
+	openMapInstructions : _("Select a json file."),
+	saveMap : _("Save map"),
+	saveMapInstructions : _("Save the mind map as a json file."),
+	save : _("Save"),
+	size : _("Size"),
+	textStyle : _("Text style"),
+	textColor : _("Text color"),
+	branchColor : _("Line color"),
+	recursive : _("Recursive"),
+	"export" : _("Export"),
+	footer : _("It works with mindmaps")
+}
