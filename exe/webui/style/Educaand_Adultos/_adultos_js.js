24849b1ee12833067212cb5c3733cea47f3ffdd9
IG Ignacio Gros <ignacio@gros.es>
2021-05-05 12:32:12 +0200

    Indent (tab instead of spaces).

--- exe/webui/style/Educaand_Adultos/_adultos_js.js
+++ exe/webui/style/Educaand_Adultos/_adultos_js.js
@@ -1,38 +1,40 @@
 var myTheme = {
-    collapseActivities : true, // Minimize interactive activities
-    activities : [
-        "Lista",
-        "QuizTest",
-        "ScrambledList",
-        "Multichoice",
-        "MultiSelect",
-        "TrueFalse",
-        "Cloze",
-        "interactive-video",
-        "GeoGebra",
-        "Eleccionmultiplefpd",
-        "Clozefpd",
-        "Clozelangfpd",
-        "Seleccionmultiplefpd",
-        "Verdaderofalsofpd",
-        "Ejercicioresueltofpd"
-    ],
-    init : function(){
+	collapseActivities : false, // Minimize interactive activities
+	// Activities (to minify if collapseActivities is true)
+	// Activities using any of these icons will be minified too: icon_udl_exp_tarea, icon_udl_exp_interactivo
+	activities : [
+		"Lista",
+		"QuizTest",
+		"ScrambledList",
+		"Multichoice",
+		"MultiSelect",
+		"TrueFalse",
+		"Cloze",
+		"interactive-video",
+		"GeoGebra",
+		"Eleccionmultiplefpd",
+		"Clozefpd",
+		"Clozelangfpd",
+		"Seleccionmultiplefpd",
+		"Verdaderofalsofpd",
+		"Ejercicioresueltofpd"
+	],
+	init : function(){
 		var ie_v = $exe.isIE();
 		if (ie_v && ie_v<8) return false;
-        setTimeout(function(){
-            $(window).resize(function() {
-                myTheme.reset();
-            });
-        },1000);
+		setTimeout(function(){
+			$(window).resize(function() {
+				myTheme.reset();
+			});
+		},1000);
 		var tit = $exe_i18n.menu+" ("+$exe_i18n.hide.toLowerCase()+")";
 		var navToggler = '<p id="header-options">';
 				navToggler += '<a href="#" class="hide-nav" id="toggle-nav" title="'+tit+'">';
 					navToggler += '<span>'+$exe_i18n.menu+'</span>';
 				navToggler += '</a>';
 			navToggler += '</p>';
-        var l = $(navToggler);
-        var nav = $("#siteNav");
+		var l = $(navToggler);
+		var nav = $("#siteNav");
 		nav.before(l);
 		$("#toggle-nav").click(function(){
 			myTheme.toggleMenu(this);
@@ -43,32 +45,32 @@ var myTheme = {
 			return false;
 		});		
 		if ( $("A",nav).attr("class").indexOf("active")==0 ) $("BODY").addClass("home-page");
-        var url = window.location.href;
-        url = url.split("?");
-        if (url.length>1){
-            if (url[1].indexOf("nav=false")!=-1) {
-                myTheme.hideMenu();
-            }
-        }
+		var url = window.location.href;
+		url = url.split("?");
+		if (url.length>1){
+			if (url[1].indexOf("nav=false")!=-1) {
+				myTheme.hideMenu();
+			}
+		}
 		// Set the min-height for the content wrapper
 		$("#main-wrapper").css("min-height",(nav.height()+25)+"px");
-    },
+	},
 	init2 : function(){
 		var ie_v = $exe.isIE();
 		if (ie_v && ie_v<8) return false;
-        setTimeout(function(){
-            $(window).resize(function() {
-                myTheme.reset();
-            });
-        },1000);
+		setTimeout(function(){
+			$(window).resize(function() {
+				myTheme.reset();
+			});
+		},1000);
 		var tit = $exe_i18n.menu+" ("+$exe_i18n.hide.toLowerCase()+")";
 		var navToggler = '<p id="header-options">';
 				navToggler += '<a href="#" class="hide-nav" id="toggle-nav" title="'+tit+'">';
 					navToggler += '<span>'+$exe_i18n.menu+'</span>';
 				navToggler += '</a>';
 			navToggler += '</p>';
-        var l = $(navToggler);
-        var nav = $("#siteNav");
+		var l = $(navToggler);
+		var nav = $("#siteNav");
 		nav.before(l);
 		$("#toggle-nav").click(function(){
 			myTheme.toggleMenu(this);
@@ -79,85 +81,85 @@ var myTheme = {
 			return false;
 		});		
 		if ( $("A",nav).attr("class").indexOf("active")==0 ) $("BODY").addClass("home-page");
-        var url = window.location.href;
-        url = url.split("?");
-        if (url.length>1){
-            if (url[1].indexOf("nav=false")!=-1) {
-                myTheme.hideMenu();
-            }
-        }
+		var url = window.location.href;
+		url = url.split("?");
+		if (url.length>1){
+			if (url[1].indexOf("nav=false")!=-1) {
+				myTheme.hideMenu();
+			}
+		}
 		// Set the min-height for the content wrapper
 		$("#main-wrapper").css("min-height",(nav.height()+25)+"px");
 		myTheme.hideMenu();
 	
-    },
-    hideMenu : function(){
-        $("#siteNav").hide();
-        $(document.body).addClass("no-nav");
-        myTheme.params("add");
+	},
+	hideMenu : function(){
+		$("#siteNav").hide();
+		$(document.body).addClass("no-nav");
+		myTheme.params("add");
 		var tit = $exe_i18n.menu+" ("+$exe_i18n.show.toLowerCase()+")";
-        $("#toggle-nav").attr("class","show-nav").attr("title",tit);
-    },
-    toggleMenu : function(e){
-        if (typeof(myTheme.isToggling)=='undefined') myTheme.isToggling = false;
-        if (myTheme.isToggling) return false;
-        
-        var l = $("#toggle-nav");
-        
-        if (!e && $(window).width()<900 && l.css("display")!='none') return false; // No reset in mobile view
-        if (!e) {
-            var tit = $exe_i18n.menu+" ("+$exe_i18n.show.toLowerCase()+")";
-            l.attr("class","show-nav").attr("title",tit); // Reset
-        }
-        
-        myTheme.isToggling = true;
-        
-        if (l.attr("class")=='hide-nav') {  
+		$("#toggle-nav").attr("class","show-nav").attr("title",tit);
+	},
+	toggleMenu : function(e){
+		if (typeof(myTheme.isToggling)=='undefined') myTheme.isToggling = false;
+		if (myTheme.isToggling) return false;
+		
+		var l = $("#toggle-nav");
+		
+		if (!e && $(window).width()<900 && l.css("display")!='none') return false; // No reset in mobile view
+		if (!e) {
 			var tit = $exe_i18n.menu+" ("+$exe_i18n.show.toLowerCase()+")";
-            l.attr("class","show-nav").attr("title",tit);
-            $("#siteFooter").hide();
+			l.attr("class","show-nav").attr("title",tit); // Reset
+		}
+		
+		myTheme.isToggling = true;
+		
+		if (l.attr("class")=='hide-nav') {  
+			var tit = $exe_i18n.menu+" ("+$exe_i18n.show.toLowerCase()+")";
+			l.attr("class","show-nav").attr("title",tit);
+			$("#siteFooter").hide();
 			$("#siteNav").slideUp(400,function(){
-                $(document.body).addClass("no-nav");
-                $("#siteFooter").show();
-                myTheme.isToggling = false;
-            }); 
-            myTheme.params("add");
-        } else {
-            var tit = $exe_i18n.menu+" ("+$exe_i18n.hide.toLowerCase()+")";
+				$(document.body).addClass("no-nav");
+				$("#siteFooter").show();
+				myTheme.isToggling = false;
+			}); 
+			myTheme.params("add");
+		} else {
+			var tit = $exe_i18n.menu+" ("+$exe_i18n.hide.toLowerCase()+")";
 			l.attr("class","hide-nav").attr("title",tit);
-            $(document.body).removeClass("no-nav");
+			$(document.body).removeClass("no-nav");
 			$("#siteNav").slideDown(400,function(){
-                myTheme.isToggling = false;
-            });
-            myTheme.params("delete");            
-        }
-        
-    },
-    param : function(e,act) {
-        if (act=="add") {
-            var ref = e.href;
-            var con = "?";
-            if (ref.indexOf(".html?")!=-1) con = "&";
-            var param = "nav=false";
-            if (ref.indexOf(param)==-1) {
-                ref += con+param;
-                e.href = ref;                    
-            }            
-        } else {
-            // This will remove all params
-            var ref = e.href;
-            ref = ref.split("?");
-            e.href = ref[0];
-        }
-    },
-    params : function(act){
-        $("A",".pagination").each(function(){
-            myTheme.param(this,act);
-        });
-    },
-    reset : function() {
-        myTheme.toggleMenu();        
-    },
+				myTheme.isToggling = false;
+			});
+			myTheme.params("delete");			
+		}
+		
+	},
+	param : function(e,act) {
+		if (act=="add") {
+			var ref = e.href;
+			var con = "?";
+			if (ref.indexOf(".html?")!=-1) con = "&";
+			var param = "nav=false";
+			if (ref.indexOf(param)==-1) {
+				ref += con+param;
+				e.href = ref;					
+			}			
+		} else {
+			// This will remove all params
+			var ref = e.href;
+			ref = ref.split("?");
+			e.href = ref[0];
+		}
+	},
+	params : function(act){
+		$("A",".pagination").each(function(){
+			myTheme.param(this,act);
+		});
+	},
+	reset : function() {
+		myTheme.toggleMenu();		
+	},
 	common : {
 		init : function(c){
 			var iDevices = $(".iDevice_wrapper");
@@ -178,11 +180,11 @@ var myTheme = {
 					}
 				}
 			}); 
-            if (myTheme.collapseActivities) {
-                var as = myTheme.activities;
-                var editor = $("#activeIdevice");
-                if (typeof(_)!='function' || editor.length!=1) {
-                    if ($(".iDevice").length>1) {
+			if (myTheme.collapseActivities) {
+				var as = myTheme.activities;
+				var editor = $("#activeIdevice");
+				if (typeof(_)!='function' || editor.length!=1) {
+					if ($(".iDevice").length>1) {
 						for (var z=0;z<as.length;z++){
 							var a = as[z];
 							// Minimize those iDevices (like clicking on .toggle-idevice a)
@@ -209,12 +211,12 @@ var myTheme = {
 							$(".toggle-idevice a",this).trigger("click");
 						}).css("cursor","pointer");
 					}
-                }
-            }   
+				}
+			}   
 			// "Do it here" will be the default title of the Interactive Activities
 			if (document.body.className.indexOf("exe-authoring-page")==0) {
 				if (typeof(top._)!='undefined') {
-                    var d = [
+					var d = [
 						"DropDown Activity",
 						"SCORM Quiz",
 						"Scrambled List",
