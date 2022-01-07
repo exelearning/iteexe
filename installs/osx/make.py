d560c9f74e6a4fa94e929d200caf4b3df424b3a9
Javier <janto@apsl.net>
2022-01-07 15:48:51 +0100

    trying to beautify the installation

--- installs/osx/make.py
+++ installs/osx/make.py
@@ -12,8 +12,11 @@ import subprocess
 os.chdir('../..')
 WDIR = os.getcwd()
 TEMPLATE = os.path.join(WDIR, 'installs/osx', 'exe.dmg')
-VOL = '/Volumes/exe'
+#VOL = '/Volumes/exe'
+VOL = '/tmp/exe'
 OUTPUT = os.path.join(WDIR, 'installs/osx')
+
+os.mkdir(VOL)
 
 do_upload = False
 try:
@@ -42,7 +45,7 @@ from exe.engine import version
 outpathn = os.path.join(OUTPUT, 'INTEF-exe-%s.dmg' % version.release)
 
 # attach the disk image template
-subprocess.check_call('hdiutil attach %s' % TEMPLATE, shell=True)
+#subprocess.check_call('hdiutil attach %s' % TEMPLATE, shell=True)
 
 # copy the app to the template
 os.chdir(VOL)
@@ -55,16 +58,25 @@ shutil.rmtree('README.txt', True)
 shutil.copy(os.path.join(WDIR, 'README'), 'README.txt')
 shutil.rmtree('NEWS.txt', True)
 shutil.copy(os.path.join(WDIR, 'NEWS'), 'NEWS.txt')
+shutil.copy(os.path.join(WDIR, 'COPYING'), 'COPYING')
 shutil.rmtree('Changelog.txt', True)
 shutil.copy(os.path.join(WDIR, 'debian/changelog'), 'Changelog.txt')
 os.chmod('exe.app/Contents/Resources/exe/templates/mimetex-darwin.cgi', 0755)
 open('exe.app/Contents/Resources/exe/version', 'w').write(version.version)
-os.chdir(WDIR)
+
+shutil.copy(os.path.join(WDIR, 'installs/osx', 'exedmg.json'), 'exedmg.json')
+shutil.copy(os.path.join(WDIR, 'installs/osx', 'exedmg.png'), 'exedmg.png')
+shutil.copy(os.path.join(WDIR, 'installs/osx', 'exe.icns'), 'exe.icns')
+#subprocess.check_call('ln -s /Applications Applications', shell=True)
+
+#os.chdir(WDIR)
 
 # detatch the disk image template
-subprocess.check_call('hdiutil detach %s' % VOL, shell=True)
+#subprocess.check_call('hdiutil detach %s' % VOL, shell=True)
 
-# build a compressed image
-subprocess.check_call('hdiutil convert -ov -format UDZO -o %s %s' %
-        (outpathn, TEMPLATE),
-        shell=True)
+# build the final DMG image with appdmg
+subprocess.check_call("appdmg exedmg.json %s" % outpathn, shell=True)
+
+#subprocess.check_call('hdiutil convert -ov -format UDZO -o %s %s' %
+#        (outpathn, TEMPLATE),
+#        shell=True)
