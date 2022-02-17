240df7251447a96f8ed79640cacaea7131b0a952
IG Ignacio Gros <ignacio@gros.es>
2022-02-17 12:44:27 +0100

    New full Spanish translation, by @jrfern (Juan Rafael Fernández).

--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.6\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2021-05-04 18:24+0200\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.4.0\n"
+ "": "Project-Id-Version: eXeLearning 2.6.1\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2022-02-16 16:16+0100\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.3.4\n"
 };
