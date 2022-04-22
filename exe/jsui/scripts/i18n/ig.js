978284be02332c32f953a4f5d484d5eeed29d22a
IG Ignacio Gros <ignacio@gros.es>
2022-04-22 14:30:38 +0200

    Translations (compiled files, after recovering previously translated strings).

--- exe/jsui/scripts/i18n/ig.js
+++ exe/jsui/scripts/i18n/ig.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.6.1\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2022-02-23 11:01+0100\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n", 
+ "": "Project-Id-Version: eXeLearning 2.7\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2022-04-22 12:24+0200\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n", 
  "?????": "?????"
 };
