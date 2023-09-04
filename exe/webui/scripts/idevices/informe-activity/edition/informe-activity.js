015b195f64da34c2546beb4d25482eff8e5d830d
mnarvaezm <mnarvaez007@gmail.com>
2023-09-04 18:06:37 +0200

    Nuevo IDevice: Informe de progreso

---
+++ exe/webui/scripts/idevices/informe-activity/edition/informe-activity.js
@@ -0,0 +1,192 @@
+/**
+/**
+ * informe Activity iDevice (edition code)
+ *
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: Manuel Narvaez Martinez
+ * Ana María Zamora Moreno
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ */
+var $exeDevice = {
+    i18n: {
+        category: _('Games'),
+        name: _('Informe de progreso')
+    },
+    msgs: {
+    },
+    id: '',
+
+    ci18n: {
+        "msgSummary": _("Resumen de las actividades"),
+        "msgNoCompletedActivities": _("Aún no has realizado ninguna de las actividades evaluables propuestas en este recurso educativo."),
+        "msgNoPendientes": _("Número de actividades pendientes: %s."),
+        "msgCompletedActivities": _("La siguiente tabla recoge los resultados que has obtenido en las actividades evaluables propuestas en este recurso educativo que ya has completado."),
+        "msgAverageScore": _("Nota media"),
+        "msgReboot": _("Reiniciar"),
+        "msgReload": _("Actualizar"),
+        "mssActivitiesNumber": _("Nº Actividades: %s"),
+        "msgActivitiesCompleted": _("Completadas: %s"),
+        "msgAverageScore1": _("Nota media: %s"),
+        "msgAverageScoreCompleted": _("Nota media completadas: %s"),
+        "msgDelete": _("Esto eliminará las puntuaciones almacenadas de todas las actividades ¿Estás seguro de que deseas continuar?"),
+        "msgSections": _("Secciones del recurso educativo"),
+        "msgSave": _("Guardar"),
+        "msgReport": _("informe_de_progreso"),
+        "msgReportTitle": _("Informe de progreso"),
+        "msgType": _("Tipo"),
+        "msgSeeActivity": _("Ir a la actividad"),
+        "mgsSections": _("Secciones del recurso educativo"),
+        "msgName": _("Nombre"),
+        "msgDate": _("Fecha")
+    },
+    init: function () {
+        this.createForm();
+        this.addEvents();
+    },
+    setMessagesInfo: function () {
+        var msgs = this.msgs;
+        msgs.msgEProvideID = _("Tienes que introducir el identificador de este informe de progreso");
+    },
+    createForm: function () {
+        var html = '\
+			<div id="gameQEIdeviceForm">\
+                <div class="exe-idevice-info">' + _("Muestra los resultados obtenidos por en alumnos en las activiades asociadas al mismo y su nota media.") + ' <a href="https://youtu.be/t1tGpTuHN5k" hreflang="es" rel="lightbox" target="_blank">' + _("Use Instructions") + '</a></div>\				<div class="exe-form-tab" title="' + _('General settings') + '">\
+					<fieldset class="exe-fieldset">\
+						<legend><a href="#">' + _("Options") + '</a></legend>\
+                        <div>\
+                           <p>' + _("El identificador puede ser un número o una palabra de más de cuatro caracteres. Debes utilizar este mismo identificador en todas las actividades que deseas evluar en este informe de progreso.") + '</p>\
+                            <p>\
+								<label for="informeEEvaluationID">' + _("Identificador") + ':\
+								<input type="text" id="informeEEvaluationID"/> </label>\
+                            </p>\
+                            <p>\
+								<label for="informeENumber">' + _("Nº de activiades evaluables") + ':\
+								<input type="number" name="informeENumber" id="informeENumber" value="1" min="1" max="99" step="1" /> </label>\
+                            </p>\
+                            <p>\
+                                <label for="informeEShowDate"><input type="checkbox" id="informeEShowDate">' + _("Mostrar fecha y hora") + '. </label>\
+                            </p>\
+                            <p>\
+                                <label for="informeEShowTypeGame"><input type="checkbox" id="informeEShowTypeGame">' + _("Mostrar tipo de iDevice") + '. </label>\
+                            </p>\
+                            <p>\
+                                <label for="informeEActiveLinks"><input type="checkbox" id="informeEActiveLinks">' + _("Enlazar actividades del informe") + '. </label>\
+                            </p>\
+                            <p>\
+                                <label for="informeUserData"><input type="checkbox" id="informeUserData" /> ' +   _("Datos del usuario") + '. </label>\
+                            </p>\
+                            <p class="exe-idevice-info">' + _("<strong>IDevices compatibles con el informe:</strong> Adivina, Completa, Identifica, Mapa, Operaciones matemáticas, Problemas matemáticos, QuExt, Rosco, Selecciona, Sopa de letras, Tarjetas de memoria, VídeoQuExt, Geogebra,  Lista desordenada y Vídeo Interactivo.") + '</p>\
+                         </div>\
+                    </fieldset>\
+                </div>\
+				' + $exeAuthoring.iDevice.gamification.common.getLanguageTab(this.ci18n) + '\
+		    </div>\
+			';
+        var field = $("textarea.jsContentEditor").eq(0)
+        field.before(html);
+        $exeDevice.loadPreviousValues(field);
+        $exeAuthoring.iDevice.tabs.init("gameQEIdeviceForm");
+    },
+    loadPreviousValues: function (field) {
+        var originalHTML = field.val();
+        if (originalHTML != '') {
+            var wrapper = $("<div></div>");
+            wrapper.html(originalHTML);
+            var json = $('.informe-DataGame', wrapper).text();
+            var dataGame = $exeDevice.isJsonString(json);
+            $exeDevice.updateFieldGame(dataGame);
+            $exeAuthoring.iDevice.gamification.common.setLanguageTabValues(dataGame.msgs);
+        }
+    },
+    isJsonString: function (str) {
+        try {
+            var o = JSON.parse(str, null, 2);
+            if (o && typeof o === "object") {
+                return o;
+            }
+        } catch (e) {}
+        return false;
+    },
+    save: function () {
+        var dataGame = $exeDevice.validateData();
+
+        if (!dataGame) {
+            return false;
+        }
+        var fields = this.ci18n,
+            i18n = fields;
+        for (var i in fields) {
+            var fVal = $("#ci18n_" + i).val();
+            if (fVal != "") i18n[i] = fVal;
+        }
+        dataGame.msgs = i18n;
+        var json = JSON.stringify(dataGame),
+            html = '<div class="informe-IDevice">';
+        html += '<div class="informe-DataGame js-hidden">' + json + '</div>';
+        html += '<div class="informe-bns js-hidden">' + $exeDevice.msgs.msgNoSuportBrowser + '</div>';
+        html += '</div>';
+        return html;
+    },
+
+
+    showMessage: function (msg) {
+        eXe.app.alert(msg);
+    },
+
+    validateData: function () {
+        var evaluationID = $('#informeEEvaluationID').val(),
+            number = parseInt($('#informeENumber').val()),
+            showDate = $('#informeEShowDate').is(':checked');
+            showTypeGame = $('#informeEShowTypeGame').is(':checked');
+            activeLinks = $('#informeEActiveLinks').is(':checked');
+            userData = $("#informeUserData").is(":checked");
+        if (!evaluationID || evaluationID.length < 5) {
+            $exeDevice.showMessage(_('El identificador del informe debe tener al menos 5 caracteres'));
+            return false;
+
+        }
+        var
+            data = {
+                'typeGame': 'Evaluación',
+                'evaluationID': evaluationID,
+                'number': number,
+                'showDate':showDate,
+                'showTypeGame':showTypeGame,
+                'activeLinks':activeLinks,
+                'userData': userData
+            }
+        return data;
+    },
+
+    addEvents: function () {
+        $('#informeENumber').on('keyup', function () {
+            var v = this.value;
+            v = v.replace(/\D/g, '');
+            v = v.substring(0, 2);
+            this.value = v;
+        });
+        $('#informeENumber').on('focusout', function () {
+            this.value = this.value.trim() == '' ? 1 : this.value;
+            this.value = this.value > 99 ? 99 : this.value;
+            this.value = this.value < 1 ? 1 : this.value;
+        });
+
+    },
+
+    updateFieldGame: function (game) {
+        game.evaluationID = typeof game.evaluationID != "undefined" ? game.evaluationID : '';
+        game.number = typeof game.number != "undefined" ? game.number : 1;
+        game.number = typeof game.number != "undefined" ? game.number : 1;
+        game.showDate = typeof game.showDate != "undefined" ? game.showDate : false;
+        game.showTypeGame = typeof game.showTypeGame != "undefined" ? game.showTypeGame : false;
+        game.activeLinks = typeof game.activeLinks != "undefined" ? game.activeLinks : false;
+        game.userData= typeof  game.userData=="undefined"?false: game.userData;
+        $('#informeEEvaluationID').val(game.evaluationID);
+        $('#informeENumber').val(game.number);
+        $('#informeEShowDate').prop('checked', game.showDate);
+        $('#informeEShowTypeGame').prop('checked', game.showTypeGame);
+        $('#informeEActiveLinks').prop('checked', game.activeLinks);
+        $("#informeUserData").prop("checked", game.userData);
+    },
+
+}
---
+++ exe/webui/scripts/idevices/informe-activity/export/informe-activity.js
@@ -0,0 +1,573 @@
+/**
+ * Evaluation activity (Export)
+ *
+ * Released under Attribution-ShareAlike 4.0 International License.
+ * Author: Manuel Narváez Martínez
+ * Ana María Zamora Moreno
+ * License: http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+var $eXeInforme = {
+  idevicePath: "",
+  borderColors: {
+    black: "#1c1b1b",
+    blue: "#5877c6",
+    green: "#00a300",
+    red: "#b3092f",
+    white: "#ffffff",
+    yellow: "#f3d55a",
+  },
+  colors: {
+    black: "#1c1b1b",
+    blue: "#dfe3f1",
+    green: "#caede8",
+    red: "#fbd2d6",
+    white: "#ffffff",
+    yellow: "#fcf4d3",
+  },
+  options: {},
+  hasSCORMbutton: false,
+  isInExe: false,
+  data: null,
+  menusNav: [],
+  objeMenuNav: [],
+  init: function () {
+    this.activities = $(".informe-IDevice");
+    if (this.activities.length == 0) {
+      $(".informe-IDevice").hide();
+      return;
+    }
+    if (!$eXeInforme.supportedBrowser("informe")) return;
+    if (
+      typeof $exeAuthoring != "undefined" &&
+      $("#exe-submitButton").length > 0
+    ) {
+      this.activities.hide();
+      if (typeof _ != "undefined")
+        this.activities.before("<p>" + _("Informe de progreso") + "</p>");
+      return;
+    }
+    if (typeof $exeAuthoring != "undefined") this.isInExe = true;
+    this.idevicePath = this.isInExe
+      ? "/scripts/idevices/informe-activity/export/"
+      : "";
+    this.enable();
+  },
+
+  enable: function () {
+    $eXeInforme.loadGame();
+  },
+  loadGame: function () {
+    $eXeInforme.options = {};
+    $eXeInforme.activities.each(function (i) {
+      if (i == 0) {
+        var dl = $(".informe-DataGame", this),
+          mOption = $eXeInforme.loadDataGame(dl);
+        $eXeInforme.options = mOption;
+        var informe = $eXeInforme.createInterfaceinforme();
+        dl.before(informe).remove();
+        $eXeInforme.addEvents();
+        $eXeInforme.menusNav = $eXeInforme.menusRead();
+        var data = $eXeInforme.getDataStorage(mOption.evaluationID);
+        $eXeInforme.generateTable1(data, mOption.number);
+      }
+    });
+  },
+
+  loadDataGame: function (data) {
+    var json = data.text();
+    var mOptions = $eXeInforme.isJsonString(json);
+    mOptions.activeLinks =
+      !$("body").hasClass("exe-scorm") &&
+      typeof mOptions.activeLinks != "undefined"
+        ? mOptions.activeLinks
+        : false;
+    return mOptions;
+  },
+
+  isJsonString: function (str) {
+    try {
+      var o = JSON.parse(str, null, 2);
+      if (o && typeof o === "object") {
+        return o;
+      }
+    } catch (e) {}
+    return false;
+  },
+  createInterfaceinforme: function () {
+    $("#informeMessage").html(
+      "<p>" +
+        $eXeInforme.options.msgs.msgNoCompletedActivities +
+        "</p><p>" +
+        $eXeInforme.options.msgs.msgNoPendientes.replace(
+          "%s",
+          $eXeInforme.options.number
+        ) +
+        "</p>"
+    );
+
+    var html = "";
+    html = "";
+    html +=
+      '<div class="INFMP-MainContainer">\
+        <div class="INFMP-Message" id="informeMessage"><p>' +
+      $eXeInforme.options.msgs.msgNoCompletedActivities +
+      "</p><p>" +
+      $eXeInforme.options.msgs.msgNoPendientes.replace(
+        "%s",
+        $eXeInforme.options.number
+      ) +
+      '</p></div>\
+            <div class="INFMP-GameContainer" id="informeGameContainer">\
+                <div id="informeData" class="INFMP-Data" ></div>\
+            </div>\
+            <a id="informeDownloadLink" href="#" download="imagen.jpg" style="display: none;">Descargar imagen</a>\
+        </div>\
+    </div>';
+    return html;
+  },
+  menusRead: function () {
+    var menus = [];
+    if ($eXeInforme.isInExe) {
+      menus = $eXeInforme.generateNavArrayExe();
+    } else {
+      menus = $eXeInforme.generateNavArray();
+    }
+    return menus;
+  },
+
+  generateNavArrayExe: function () {
+    var sbody = $("body", window.parent.document)
+        .find('[id^="treepanel"][id$="body"]')
+        .find(".x-grid-table .x-grid-row"),
+      results = [],
+      position = "",
+      index1 = 1,
+      index2 = 0,
+      index3 = 0,
+      index4 = 0,
+      index5 = 0;
+    index6 = 0;
+    sbody.each(function (i) {
+      var text = $(this).find(".x-grid-cell-inner").text().trim();
+      var link = "";
+      if (i == 0) {
+        position = "1.";
+      } else {
+        var imgs = $(this).find(".x-grid-cell-inner img").length;
+        if (imgs == 3) {
+          index1++;
+          index2 = 0;
+          index3 = 0;
+          index4 = 0;
+          index5 = 0;
+          index6 = 0;
+        } else if (imgs == 4) {
+          index2++;
+          index3 = 0;
+          index4 = 0;
+          index5 = 0;
+          index6 = 0;
+        } else if (imgs == 5) {
+          index3++;
+          index4 = 0;
+          index5 = 0;
+          index6 = 0;
+        } else if (imgs == 6) {
+          index4++;
+          index5 = 0;
+          index6 = 0;
+        } else if (imgs == 7) {
+          index5++;
+          index6 = 0;
+        } else if (imgs == 8) {
+          index6++;
+        }
+        position =
+          index1 +
+          "." +
+          index2 +
+          "." +
+          index3 +
+          "." +
+          index4 +
+          "." +
+          index5 +
+          "." +
+          index6 +
+          ".";
+        position = position.replace(/\.0\..*/, "");
+        if (position.slice(-1) !== ".") {
+          position += ".";
+        }
+      }
+      results.push({
+        text: text,
+        link: link,
+        position: position,
+      });
+    });
+    return results;
+  },
+  generateNavArray: function () {
+    var arr = [];
+    function processListItems(listItems, prefix) {
+      listItems.each(function (index) {
+        var position = prefix + (index + 1),
+          linkElement = $(this).children("a").first(),
+          obj = {
+            text: linkElement.text(),
+            link: linkElement.attr("href"),
+            position: position.toString() + ".",
+          };
+        arr.push(obj);
+        var subListItems = $(this).find("> ul > li");
+        if (subListItems.length > 0) {
+          processListItems(subListItems, position + ".");
+        }
+      });
+    }
+    processListItems($("#siteNav > ul > li"), "");
+    return arr;
+  },
+
+  createLinkNode: function (node) {
+    var link = node.text;
+    if (!$eXeInforme.isInExe && $eXeInforme.options.activeLinks) {
+      link =
+        '<a href="' +
+        node.link +
+        '" style="color:#333333">' +
+        node.text +
+        "</a>";
+    }
+    return link;
+  },
+
+  linkNode: function (texto_link, numnod) {
+    var texto_link_sanitized = texto_link
+      .replace(/ /g, "_")
+      .replace(/[^\w\s]/gi, "");
+    var url = texto_link_sanitized.toLowerCase() + ".html";
+    if (numnod == 0) {
+      url = "index.html";
+    }
+    return url;
+  },
+
+  createRowIdevice: function (
+    idevice,
+    bcolor,
+    color,
+    type,
+    date,
+    node,
+    margin
+  ) {
+    var txtIdevice = "";
+    if (!$eXeInforme.isInExe && $eXeInforme.options.activeLinks) {
+      txtIdevice +=
+        '<div class="INFMP-Activity" style="background-color:' +
+        bcolor +
+        "; margin-left:" +
+        margin +
+        '">\
+            <div><span id="informeName" style="color:' +
+        color +
+        ';"><a href="' +
+        node.link +
+        "#ac-" +
+        idevice.id +
+        '" style="color:' +
+        color +
+        ';" title="' +
+        $eXeInforme.options.msgs.msgSeeActivity +
+        '">' +
+        idevice.name +
+        "</a></span>" +
+        type +
+        '</div>\
+            <div class="INFMP-DateScore"></span>' +
+        date +
+        ' </span><span style="color:' +
+        color +
+        ';">' +
+        idevice.score +
+        "</span>\
+            </div>\
+            </div>";
+    } else {
+      txtIdevice +=
+        '<div class="INFMP-Activity" style="background-color:' +
+        bcolor +
+        "; margin-left:" +
+        margin +
+        '">\
+            <div ><span id="informeName" style="color:' +
+        color +
+        ';">' +
+        idevice.name +
+        "</span>" +
+        type +
+        '</div>\
+            <div class="INFMP-DateScore"><span>' +
+        date +
+        '</span><span style="color:' +
+        color +
+        ';">' +
+        idevice.score +
+        "</span>\
+            </div>\
+            </div>";
+    }
+    return txtIdevice;
+  },
+
+  generateTable1: function (data, number) {
+    var num = number;
+    var scoretotal = 0;
+    const nodes = {};
+    var cabecera = "";
+    var table = "";
+    var z = 0;
+    var ud = $eXeInforme.options.userData ? "flex" : "none";
+    if (data) {
+      $("#informeMessage").text(
+        $eXeInforme.options.msgs.msgCompletedActivities
+      );
+    } else {
+      $("#informeMessage").html(
+        "<p>" +
+          $eXeInforme.options.msgs.msgNoCompletedActivities +
+          "</p><p>" +
+          $eXeInforme.options.msgs.msgNoPendientes.replace(
+            "%s",
+            $eXeInforme.options.number
+          ) +
+          "</p>"
+      );
+    }
+    table +=
+      '<div class="INFMP-Node">' +
+      $eXeInforme.options.msgs.mgsSections +
+      ":</div>";
+    for (var nod = 0; nod < $eXeInforme.menusNav.length; nod++) {
+      var node = $eXeInforme.menusNav[nod],
+        margin = (node.position.split(".").length - 2) * 16 + "px";
+
+      table +=
+        '<div class="INFMP-Node" style="margin-left:' +
+        margin +
+        '">' +
+        node.position +
+        "&nbsp;" +
+        $eXeInforme.createLinkNode(node) +
+        "</div>";
+      var j = 0,
+        bcolor = "#dddddd",
+        color = "#007F5F";
+      if (data) {
+        for (var acts = 0; acts < data.length; acts++) {
+          if ($eXeInforme.menusNav[nod].text == data[acts].node) {
+            var date = $eXeInforme.options.showDate
+              ? '<span id="informeDate">(' + data[acts].date + ") </span>"
+              : "";
+            var type = $eXeInforme.options.showTypeGame
+              ? '<span id="informeType">. ' +
+                $eXeInforme.options.msgs.msgType +
+                ":&nbsp" +
+                data[acts].type +
+                " </span>"
+              : "";
+            color = parseFloat(data[acts].score) < 5 ? "#B61E1E" : "#007F5F";
+            table += $eXeInforme.createRowIdevice(
+              data[acts],
+              bcolor,
+              color,
+              type,
+              date,
+              node,
+              margin
+            );
+            j++;
+            z++;
+            scoretotal += parseFloat(data[acts].score);
+            if (j % 2 == 0) {
+              bcolor = "#dddddd";
+            } else {
+              bcolor = "#f9f9f9";
+            }
+          }
+        }
+      }
+    }
+    var scorepartial = z > 0 ? (scoretotal / z).toFixed(2) : "0.00";
+    scoretotal = (scoretotal / num).toFixed(2);
+    var bgc = scoretotal < 5 ? "#B61E1E" : "#007F5F";
+    table +=
+      '<div class="INFMP-GameScore" style="background-color:' +
+      bgc +
+      ';"><div>' +
+      $eXeInforme.options.msgs.msgAverageScore +
+      "</div><div>" +
+      scoretotal +
+      "</div></div>";
+    table += "</div>";
+    table +=
+      '<div id="informeButtons" class="INFMP-LinksInforme" style="background-color:white; text-align:right">\
+                        <a id="informeReboot" href="#">' +
+      $eXeInforme.options.msgs.msgReboot +
+      '</a>\
+                        <a id="informeCapture" href="#">' +
+      $eXeInforme.options.msgs.msgSave +
+      "</a>\
+                  </div>";
+    table += "</div>";
+    cabecera =
+      '<div class="INFMP-Table" id="informeTable">\
+                        <div id="informeTitleProyect" class="INFMP-Title">' +
+      $eXeInforme.options.msgs.msgReportTitle +
+      '</div>\
+      <div id="informeUserData" class="INFMP-UserData" style="display:' + ud +';">\
+                  <div id="informeUserNameDiv" class="INFMP-UserName">\
+                    <label for=""informeUserName">' + $eXeInforme.options.msgs.msgName +': </label><input type="text" id="informeUserName">\
+                  </div>\
+                  <div id="informeUserDateDiv" class="INFMP-UserDate">\
+                    <label for="informeUserDate">' +  $eXeInforme.options.msgs.msgDate +': </label><input type="text" id="informeUserDate" disabled>\
+                  </div>\
+                </div>\
+                        <div class="INFMP-Header">\
+                            <div id="informeTotalActivities">' +
+      $eXeInforme.options.msgs.mssActivitiesNumber.replace("%s", number) +
+      '</div>\
+                            <div id="informeCompletedActivities">' +
+      $eXeInforme.options.msgs.msgActivitiesCompleted.replace("%s", z) +
+      '</div>\
+                            <div id="informeTotalScoreA">' +
+      $eXeInforme.options.msgs.msgAverageScoreCompleted.replace(
+        "%s",
+        scorepartial
+      ) +
+      '</div>\
+                            <div id="informeTotalScore">' +
+      $eXeInforme.options.msgs.msgAverageScore1.replace("%s", scoretotal) +
+      '</div>\
+                        </div>\
+                        <div id="informePlusDiv" class="INFMP-Plus">';
+    table = cabecera + table;
+    $("#informeData").empty();
+    $("#informeData").append(table);
+    $("#informeUserDate").val( $eXeInforme.getDateNow());
+    if (!$eXeInforme.isInExe) {
+      var headerContent = $("#headerContent").text();
+      if (headerContent && headerContent.length > 0) {
+        $("#informeTitleProyect").text(headerContent);
+      }
+    }
+  },
+
+  getDataStorage: function (id) {
+    var id = "dataEvaluation-" + id,
+      data = $eXeInforme.isJsonString(localStorage.getItem(id));
+    return data.activities;
+  },
+
+  addEvents: function () {
+    $("#informeGameContainer").on("click", "#informeLinkPlus", function (e) {
+      e.preventDefault();
+      $("#informePlusDiv").slideToggle();
+    });
+
+    $("#informeGameContainer").on("click", "#informeReboot", function (e) {
+      e.preventDefault();
+      if (confirm($eXeInforme.options.msgs.msgDelete)) {
+        localStorage.removeItem(
+          "dataEvaluation-" + $eXeInforme.options.evaluationID
+        );
+        $eXeInforme.generateTable1(false, $eXeInforme.options.number);
+      }
+    });
+    $("#informeGameContainer").on("click", "#informeCapture", function (e) {
+      e.preventDefault();
+      $eXeInforme.saveReport();
+    });
+  },
+  saveReport: function () {
+    if($eXeInforme.options.userData){
+      if($('#informeUserName').val().trim() === ""){
+        alert("Antes de guardar este informe, debes indicar tu nombre y apellidos antes de guardar el informe.");
+
+        return;
+
+      }
+    }
+    var divElement = document.getElementById("informeTable");
+    if (!divElement) {
+      console.error("No se encontró el elemento con el ID proporcionado.");
+      return;
+    }
+    $("#informeButtons").hide();
+    html2canvas(divElement)
+      .then(function (canvas) {
+        var imageData = canvas.toDataURL("image/png");
+        var link = document.createElement("a");
+        link.href = imageData;
+        link.download = $eXeInforme.options.msgs.msgReport + ".png";
+        link.click();
+      })
+      .catch(function (error) {
+        $("#informeButtons").show();
+        console.error("Error al generar la captura: ", error);
+      });
+    $("#informeButtons").show();
+  },
+
+  showMessage: function (type, message) {
+    var colors = [
+        "#555555",
+        $eXeInforme.borderColors.red,
+        $eXeInforme.borderColors.green,
+        $eXeInforme.borderColors.blue,
+        $eXeInforme.borderColors.yellow,
+      ],
+      color = colors[type];
+    $("#informePAuthor-" + instance).text(message);
+    $("#informePAuthor-" + instance).css({
+      color: color,
+    });
+  },
+  supportedBrowser: function (idevice) {
+    var ua = window.navigator.userAgent,
+      msie = ua.indexOf("MSIE "),
+      sp = true;
+    if (msie > 0) {
+      var ie = parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+      if (ie < 10) {
+        var bns =
+          $("." + idevice + "-bns")
+            .eq(0)
+            .text() || "Your browser is not compatible with this tool.";
+        $("." + idevice + "-instructions").text(bns);
+        sp = false;
+      }
+    }
+    return sp;
+  },
+   getDateNow: function() {
+    var dateNow = new Date();
+    var dia = $eXeInforme.addZero(dateNow.getDate());
+    var mes = $eXeInforme.addZero(dateNow.getMonth() + 1); // Los meses van de 0 a 11
+    var anio = dateNow.getFullYear();
+    var hora = $eXeInforme.addZero(dateNow.getHours());
+    var minutos = $eXeInforme.addZero(dateNow.getMinutes());
+    var segundos = $eXeInforme.addZero(dateNow.getSeconds());
+
+    return dia + "/" + mes + "/" + anio + " " + hora + ":" + minutos + ":" + segundos;
+  },
+  addZero: function(number) {
+     return number < 10 ? "0" + number : number;
+  }
+};
+$(function () {
+  $eXeInforme.init();
+});
