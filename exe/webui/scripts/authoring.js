7fa6b9ee7eaf7d30f53c8170ae96e01ad12263a2
ignaciogros <ignacio@gros.es>
2024-02-02 14:29:23 +0100

    Tmp string to be removed when the "Full Inclusion" style is ready.

--- exe/webui/scripts/authoring.js
+++ exe/webui/scripts/authoring.js
@@ -950,7 +950,8 @@ var $exeAuthoring = {
     iDevice : {
         init : function() {
 
-            var errorMsg = "";
+			var toReview : _("Full Inclusion"); // Just to complete the translations before the new style is in eXe
+			var errorMsg = "";
 
             // Check if the object and the required methods are defined
             if (typeof($exeDevice)=='undefined') errorMsg += "$exeDevice";
