0a9db43d28dac1915b09d60fa3ec8f2a796c299c
ignaciogros <ignacio@gros.es>
2024-03-01 15:49:39 +0100

    i18n. JS and MO files with complete Valencian translation.

--- exe/jsui/scripts/i18n/ig.js
+++ exe/jsui/scripts/i18n/ig.js
@@ -1,4 +1,4 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2024-03-01 13:16+0100\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n", 
+ "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2024-03-01 15:37+0100\nPO-Revision-Date: 2006-09-22 10:21+1200\nLast-Translator: EXE Team <exe@exelearning.org>\nLanguage: ig\nLanguage-Team: EXE Team <exe@exelearning.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n", 
  "?????": "?????"
 };
