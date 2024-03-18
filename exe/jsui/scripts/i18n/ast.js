bd5a2af4e4c04da6d9e6119fbbe58a94b296f7b6
ignaciogros <ignacio@gros.es>
2024-03-18 12:43:15 +0100

    Complete translations. JS and MO files.

--- exe/jsui/scripts/i18n/ast.js
+++ exe/jsui/scripts/i18n/ast.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2024-03-01 15:51+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: ast\nLanguage-Team: ast <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n"
+ "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: EMAIL@ADDRESS\nPOT-Creation-Date: 2024-03-18 12:09+0100\nPO-Revision-Date: 2013-03-09 22:04+0100\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage: ast\nLanguage-Team: ast <LL@li.org>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n"
 };
--- exe/jsui/scripts/i18n/en.js
+++ exe/jsui/scripts/i18n/en.js
@@ -1,3 +1,3 @@
 translations = {
- "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2024-03-01 15:51+0100\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n"
+ "": "Project-Id-Version: eXeLearning 2.9\nReport-Msgid-Bugs-To: eXe Translation Teams <translations@exelearning.net>\nPOT-Creation-Date: 2024-03-18 12:09+0100\nPO-Revision-Date: 2019-12-07 12:29+0100\nLast-Translator: \nLanguage: en\nLanguage-Team: eXe Translation Teams <translations@exelearning.net>\nPlural-Forms: nplurals=2; plural=(n != 1)\nMIME-Version: 1.0\nContent-Type: text\/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\nGenerated-By: Babel 2.9.1\n"
 };
