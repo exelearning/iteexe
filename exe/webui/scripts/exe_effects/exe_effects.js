67184e95e9f2c849c5ca5232c4ac13d9caededdf
ignaciogros <ignacio@gros.es>
2024-09-12 10:04:47 +0200

    Fix #763 - Carousel FX: The "Next" link skipped one slide.

--- exe/webui/scripts/exe_effects/exe_effects.js
+++ exe/webui/scripts/exe_effects/exe_effects.js
@@ -552,10 +552,7 @@ $exeFX = {
 				prevA.className = "fx-disabled-link";
 				// Next
 				nextLi.removeClass("fx-disabled");
-				nextA.onclick = function(){
-					$exeFX.carousel.show(gID,gID+"-1",1);
-					return false;
-				}
+				nextA.className = "exeFXSlideLink"+gID+"_"+gID+"-"+(n+1)+"_"+(n+1);
 			} else {
 				// Prev
 				prevLi.removeClass("fx-disabled");
